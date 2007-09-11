7e701a864ba1d8bac34a42588057e19d9e1833d5
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2007-09-11 14:50:18 +0000

    renamed from readwrite

---
+++ jsaga-adaptoritf/src/fr/in2p3/jsaga/adaptor/data/optimise/DataCopy.java
@@ -0,0 +1,46 @@
+package fr.in2p3.jsaga.adaptor.data.optimise;
+
+import fr.in2p3.jsaga.adaptor.data.DataAdaptor;
+import org.ogf.saga.error.*;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   DataCopy
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   13 juin 2007
+* ***************************************************
+* Description:                                      */
+/**
+ * Do not implement this interface unless it improve efficiency (e.g. third-party transfer)
+ */
+public interface DataCopy extends DataAdaptor {
+    /**
+     * Copy this entry to another part of the namespace.
+     * @param sourceAbsolutePath the path of the file to be copied.
+     * @param targetHost the host of the target server.
+     * @param targetPort the post of the target server.
+     * @param targetAbsolutePath the path of the file to copy to.
+     * @param overwrite if true, then target is overwrited if it exists.
+     * @throws BadParameter if <code>sourceAbsolutePath</code> is not a file.
+     * @throws AlreadyExists if <code>targetAbsolutePath</code> already exists and <code>overwrite</code> is false.
+     * @throws DoesNotExist if <code>sourceAbsolutePath</code> does not exist.
+     */
+    public void copy(String sourceAbsolutePath, String targetHost, int targetPort, String targetAbsolutePath, boolean overwrite)
+        throws AuthenticationFailed, AuthorizationFailed, PermissionDenied, BadParameter, DoesNotExist, AlreadyExists, Timeout, NoSuccess;
+
+    /**
+     * Copy this entry to another part of the namespace.
+     * @param sourceHost the host of the source server.
+     * @param sourcePort the post of the source server.
+     * @param sourceAbsolutePath the path of the file to be copied.
+     * @param targetAbsolutePath the path of the file to copy to.
+     * @param overwrite if true, then target is overwrited if it exists.
+     * @throws BadParameter if <code>sourceAbsolutePath</code> is not a file.
+     * @throws AlreadyExists if <code>targetAbsolutePath</code> already exists and <code>overwrite</code> is false.
+     * @throws DoesNotExist if <code>sourceAbsolutePath</code> does not exist.
+     */
+    public void copyFrom(String sourceHost, int sourcePort, String sourceAbsolutePath, String targetAbsolutePath, boolean overwrite)
+        throws AuthenticationFailed, AuthorizationFailed, PermissionDenied, BadParameter, DoesNotExist, AlreadyExists, Timeout, NoSuccess;
+}
---
+++ jsaga-adaptoritf/src/fr/in2p3/jsaga/adaptor/data/optimise/DataRename.java
@@ -0,0 +1,30 @@
+package fr.in2p3.jsaga.adaptor.data.optimise;
+
+import fr.in2p3.jsaga.adaptor.data.DataAdaptor;
+import org.ogf.saga.error.*;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   DataRename
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   21 août 2007
+* ***************************************************
+* Description:                                      */
+/**
+ * Do not implement this interface unless your implementation improves efficiency
+ */
+public interface DataRename extends DataAdaptor {
+    /**
+     * Rename entry <code>sourceAbsolutePath</code> to <code>targetAbsolutePath</code>.
+     * @param sourceAbsolutePath the file to rename.
+     * @param targetAbsolutePath the new path of the file.
+     * @param overwrite if true, the target is overwrited if it exists.
+     * @throws BadParameter if <code>sourceAbsolutePath</code> is not a file.
+     * @throws AlreadyExists if <code>targetAbsolutePath</code> already exists and <code>overwrite</code> is false.
+     * @throws DoesNotExist if <code>sourceAbsolutePath</code> does not exist.
+     */
+    public void rename(String sourceAbsolutePath, String targetAbsolutePath, boolean overwrite)
+        throws PermissionDenied, BadParameter, DoesNotExist, AlreadyExists, Timeout, NoSuccess;
+}
