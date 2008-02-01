d95f482e955eea041dd01c117948d65a393c528d
Nicolas Demesy <nicolas.demesy@bt.com>
2008-02-01 13:44:49 +0000

    Test suite with prerequisite

---
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuiteWithPrerequisite.java
@@ -0,0 +1,36 @@
+package integration;
+
+import org.ogf.saga.job.JobRunWithPrequisiteTest;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   GlobusExecutionTestSuite
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   9 nov. 2007
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class GlobusExecutionTestSuiteWithPrerequisite extends TestSuite {
+    // test cases
+    public static class GlobusJobRunWithPrequisiteTest extends JobRunWithPrequisiteTest {
+        public GlobusJobRunWithPrequisiteTest() throws Exception {super("gatekeeper");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+
+    public GlobusExecutionTestSuiteWithPrerequisite() throws Exception {
+        super();
+        // test cases
+        this.addTestSuite(GlobusJobRunWithPrequisiteTest.class);
+    }
+
+    public static Test suite() throws Exception {
+        return new GlobusExecutionTestSuiteWithPrerequisite();
+    }
+}
