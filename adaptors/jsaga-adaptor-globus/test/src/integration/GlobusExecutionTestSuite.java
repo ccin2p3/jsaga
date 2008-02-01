fda5b1df0ee40ff19022976aada35b22658524f4
Nicolas Demesy <nicolas.demesy@bt.com>
2008-02-01 13:44:19 +0000

    Refactor the test suite

--- adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuite.java
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuite.java
@@ -2,7 +2,10 @@ package integration;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.ogf.saga.job.JobRunTest;
+
+import org.ogf.saga.job.JobRunBasicTest;
+import org.ogf.saga.job.JobRunDescriptionTest;
+import org.ogf.saga.job.JobRunOptionalTest;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -17,16 +20,31 @@ import org.ogf.saga.job.JobRunTest;
  *
  */
 public class GlobusExecutionTestSuite extends TestSuite {
+	
     // test cases
-    public static class GlobusJobRunTest extends JobRunTest {
-        public GlobusJobRunTest() throws Exception {super("gatekeeper");}
+    public static class GlobusJobRunBasicTest extends JobRunBasicTest {
+        public GlobusJobRunBasicTest() throws Exception {super("gatekeeper");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    
+    // test cases
+    public static class GlobusJobRunOptionalTest extends JobRunOptionalTest {
+        public GlobusJobRunOptionalTest() throws Exception {super("gatekeeper");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    
+ 	// test cases
+    public static class GlobusJobRunDescriptionTest extends JobRunDescriptionTest {
+        public GlobusJobRunDescriptionTest() throws Exception {super("gatekeeper");}
         public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
     }
 
     public GlobusExecutionTestSuite() throws Exception {
         super();
         // test cases
-        this.addTestSuite(GlobusJobRunTest.class);
+        this.addTestSuite(GlobusJobRunBasicTest.class);
+        this.addTestSuite(GlobusJobRunOptionalTest.class);
+        this.addTestSuite(GlobusJobRunDescriptionTest.class);
     }
 
     public static Test suite() throws Exception {
--- adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuiteForWindows.java
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuiteForWindows.java
@@ -1,8 +1,11 @@
 package integration;
 
+import org.ogf.saga.job.JobRunBasicTest;
+import org.ogf.saga.job.JobRunDescriptionTest;
+import org.ogf.saga.job.JobRunOptionalTest;
+
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.ogf.saga.job.JobRunTest;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -17,16 +20,36 @@ import org.ogf.saga.job.JobRunTest;
  *
  */
 public class GlobusExecutionTestSuiteForWindows extends TestSuite {
+    
+	// test cases
+    public static class GlobusJobRunBasicTest extends JobRunBasicTest {
+        public GlobusJobRunBasicTest() throws Exception {super("gatekeeper-windows");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+        public void test_run_error() { System.out.println("the test_run_error ignored : personal gatekeeper does not support this"); };
+    }
+    
     // test cases
-    public static class GlobusJobRunTest extends JobRunTest {
-        public GlobusJobRunTest() throws Exception {super("gatekeeper-windows");}
+    public static class GlobusJobRunOptionalTest extends JobRunOptionalTest {
+        public GlobusJobRunOptionalTest() throws Exception {super("gatekeeper-windows");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+        public void test_resume_done() { System.out.println("the test_resume_done ignored : personal gatekeeper does not support this"); };
+        public void test_resume_running() { System.out.println("the test_resume_running ignored : personal gatekeeper does not support this"); };
+        public void test_suspend_done() { System.out.println("the test_suspend_done ignored : personal gatekeeper does not support this"); };
+        public void test_suspend_running() { System.out.println("the test_resume_running ignored : personal gatekeeper does not support this"); };
+    }
+    
+ 	// test cases
+    public static class GlobusJobRunDescriptionTest extends JobRunDescriptionTest {
+        public GlobusJobRunDescriptionTest() throws Exception {super("gatekeeper-windows");}
         public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
     }
 
     public GlobusExecutionTestSuiteForWindows() throws Exception {
         super();
         // test cases
-        this.addTestSuite(GlobusJobRunTest.class);
+        this.addTestSuite(GlobusJobRunBasicTest.class);
+        this.addTestSuite(GlobusJobRunOptionalTest.class);
+        this.addTestSuite(GlobusJobRunDescriptionTest.class);
     }
 
     public static Test suite() throws Exception {
