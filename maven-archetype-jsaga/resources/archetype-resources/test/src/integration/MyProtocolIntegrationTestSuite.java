cd6a8436b012fd14fcc14519fbdae3ef31c864db
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-02-04 16:22:33 +0000

    Initial revision

---
+++ maven-archetype-jsaga/resources/archetype-resources/test/src/integration/MyProtocolIntegrationTestSuite.java
@@ -0,0 +1,32 @@
+package integration;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.ogf.saga.namespace.NSEntryTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   MyProtocolIntegrationTestSuite
+* Author:
+* Date:
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class MyProtocolIntegrationTestSuite extends TestSuite {
+    public static class MyProtocolNSEntryTest extends NSEntryTest {
+        public MyProtocolNSEntryTest() throws Exception {super("myprotocol");}
+    }
+
+    public MyProtocolIntegrationTestSuite() throws Exception {
+        super();
+        this.addTestSuite(MyProtocolNSEntryTest.class);
+    }
+
+    public static Test suite() throws Exception {
+        return new MyProtocolIntegrationTestSuite();
+    }
+}
