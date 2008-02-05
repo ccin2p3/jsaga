349f93e7bdaa9761064e31a146e8ba63f1746675
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-02-05 09:31:47 +0000

    refactored for use with UFilePath

--- jsaga-adaptoritf/src/fr/in2p3/jsaga/adaptor/base/usage/UFile.java
+++ jsaga-adaptoritf/src/fr/in2p3/jsaga/adaptor/base/usage/UFile.java
@@ -1,9 +1,7 @@
 package fr.in2p3.jsaga.adaptor.base.usage;
 
-import fr.in2p3.jsaga.Base;
-import org.ogf.saga.error.DoesNotExist;
-
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -17,7 +15,7 @@ import java.io.*;
 /**
  *
  */
-public class UFile extends U {
+public class UFile extends UFilePath {
     public UFile(String name) {
         super(name);
     }
@@ -26,32 +24,8 @@ public class UFile extends U {
         return "<"+m_name+">";
     }
 
-    /** override U.correctValue() */
-    public String correctValue(String attributeName, String attributeValue, int attributeWeight) throws DoesNotExist {
-        if (m_name.equals(attributeName)) {
-            try {
-                File file = (File) this.throwExceptionIfInvalid(attributeValue);
-                m_weight = attributeWeight;
-                // returns corrected path
-                try {
-                    return file.getCanonicalPath();
-                } catch (IOException e) {
-                    return file.getAbsolutePath();
-                }
-            } catch (Exception e) {
-                m_weight = -1;
-                return null;
-            }
-        } else {
-            throw new DoesNotExist("Attribute not found: "+attributeName);
-        }
-    }
-
     protected Object throwExceptionIfInvalid(Object value) throws Exception {
-        File file = new File((String) super.throwExceptionIfInvalid(value));
-        if (!file.isAbsolute()) {
-            file = new File(Base.JSAGA_HOME, file.getPath());
-        }
+        File file = (File) super.throwExceptionIfInvalid(value);
         if (!file.exists()) {
             throw new FileNotFoundException("File not found: "+file.getAbsolutePath());
         }
