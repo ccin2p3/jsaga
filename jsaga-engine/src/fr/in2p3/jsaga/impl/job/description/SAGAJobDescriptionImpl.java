1b7e7c3c85858b6a6aaa73405ad6b96fc05fe4ae
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-02-05 09:38:42 +0000

    bugfix (setAttribute was adding instead of replacing)

--- jsaga-engine/src/fr/in2p3/jsaga/impl/job/description/SAGAJobDescriptionImpl.java
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/job/description/SAGAJobDescriptionImpl.java
@@ -6,8 +6,7 @@ import fr.in2p3.jsaga.helpers.xslt.XSLTransformerFactory;
 import org.ogf.saga.SagaObject;
 import org.ogf.saga.error.*;
 import org.ogf.saga.job.JobDescription;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
+import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.lang.Exception;
@@ -51,10 +50,8 @@ public class SAGAJobDescriptionImpl extends AbstractJobDescriptionImpl implement
     /** override super.setAttribute() */
     public void setAttribute(String key, String value) throws NotImplemented, AuthenticationFailed, AuthorizationFailed, PermissionDenied, IncorrectState, BadParameter, DoesNotExist, Timeout, NoSuccess {
         if (m_adaptor.isProperty(key)) {
-            Element attribute = m_jobDescDOM.createElement("attribute");
-            attribute.setAttribute("name", key);
+            Element attribute = this.createAttribute(key);
             attribute.setAttribute("value", value);
-            m_root.appendChild(attribute);
             super.setAttribute(key, value);
         } else if (m_adaptor.isVectoryProperty(key)) {
             throw new IncorrectState("Attempt to set a vector attribute with method setAttribute: "+key);
@@ -66,14 +63,12 @@ public class SAGAJobDescriptionImpl extends AbstractJobDescriptionImpl implement
     /** override super.setVectorAttribute() */
     public void setVectorAttribute(String key, String[] values) throws NotImplemented, AuthenticationFailed, AuthorizationFailed, PermissionDenied, IncorrectState, BadParameter, DoesNotExist, Timeout, NoSuccess {
         if (m_adaptor.isVectoryProperty(key)) {
-            Element vectorAttribute = m_jobDescDOM.createElement("vectorAttribute");
-            vectorAttribute.setAttribute("name", key);
+            Element vectorAttribute = this.createAttribute(key);
             for (int i=0; i<values.length; i++) {
                 Element item = m_jobDescDOM.createElement("value");
                 item.appendChild(m_jobDescDOM.createTextNode(values[i]));
                 vectorAttribute.appendChild(item);
             }
-            m_root.appendChild(vectorAttribute);
             super.setVectorAttribute(key, values);
         } else if (m_adaptor.isProperty(key)) {
             throw new IncorrectState("Attempt to set a scalar attribute with method setVectorAttribute: "+key);
@@ -93,6 +88,23 @@ public class SAGAJobDescriptionImpl extends AbstractJobDescriptionImpl implement
             return (Element) jsdlDOM.getFirstChild();
         } catch (Exception e) {
             throw new NoSuccess(e);
+        }
+    }
+
+    private Element createAttribute(String key) throws NoSuccess {
+        Element attribute;
+        NodeList list = m_root.getElementsByTagName(key);
+        switch(list.getLength()) {
+            case 0:
+                attribute = m_jobDescDOM.createElement(key);
+                m_root.appendChild(attribute);
+                return attribute;
+            case 1:
+                attribute = m_jobDescDOM.createElement(key);
+                m_root.replaceChild(attribute, list.item(0));
+                return attribute;
+            default:
+                throw new NoSuccess("INTERNAL ERROR: unexpected exception", this);
         }
     }
 }
