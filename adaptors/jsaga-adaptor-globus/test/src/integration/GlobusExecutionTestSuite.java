d0c4d1a1e4fbff46d289c43a96c2401b05e1d4d9
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-02-05 09:45:44 +0000

    add JobDescription test suite

--- adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuite.java
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusExecutionTestSuite.java
@@ -2,10 +2,7 @@ package integration;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-
-import org.ogf.saga.job.JobRunBasicTest;
-import org.ogf.saga.job.JobRunDescriptionTest;
-import org.ogf.saga.job.JobRunOptionalTest;
+import org.ogf.saga.job.*;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -20,7 +17,20 @@ import org.ogf.saga.job.JobRunOptionalTest;
  *
  */
 public class GlobusExecutionTestSuite extends TestSuite {
-	
+
+    // test cases
+    public static class GlobusJobDescriptionTest extends JobDescriptionTest {
+        public GlobusJobDescriptionTest() throws Exception {super("gatekeeper");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+        public void test_totalCPUCount() { System.out.println("the test_totalCPUCount ignored : RSL v1.0 does not support this"); }
+        public void test_threadsPerProcess() { System.out.println("the test_threadsPerProcess ignored : RSL v1.0 does not support this"); }
+        public void test_fileTransfer() { System.out.println("the test_fileTransfer ignored : RSL v1.0 does not support this"); }
+        public void test_cleanup() { System.out.println("the test_cleanup ignored : RSL v1.0 does not support this"); }
+        public void test_cpuArchitecture() { System.out.println("the test_cpuArchitecture ignored : RSL v1.0 does not support this"); }
+        public void test_operatingSystemType() { System.out.println("the test_operatingSystemType ignored : RSL v1.0 does not support this"); }
+        public void test_candidateHosts() { System.out.println("the test_candidateHosts ignored : RSL v1.0 does not support this"); }
+    }
+
     // test cases
     public static class GlobusJobRunBasicTest extends JobRunBasicTest {
         public GlobusJobRunBasicTest() throws Exception {super("gatekeeper");}
@@ -42,6 +52,7 @@ public class GlobusExecutionTestSuite extends TestSuite {
     public GlobusExecutionTestSuite() throws Exception {
         super();
         // test cases
+        this.addTestSuite(GlobusJobDescriptionTest.class);
         this.addTestSuite(GlobusJobRunBasicTest.class);
         this.addTestSuite(GlobusJobRunOptionalTest.class);
         this.addTestSuite(GlobusJobRunDescriptionTest.class);
