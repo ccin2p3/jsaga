4419a79cd62262ec0dab4b2d7cf2d8249ce7f7d8
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-03-26 09:46:49 +0000

    moved from jsaga-engine module

---
+++ jsaga-adaptoritf/src/fr/in2p3/jsaga/adaptor/security/impl/UserPassSecurityAdaptor.java
@@ -0,0 +1,59 @@
+package fr.in2p3.jsaga.adaptor.security.impl;
+
+import fr.in2p3.jsaga.adaptor.security.SecurityAdaptor;
+import org.ogf.saga.context.Context;
+import org.ogf.saga.error.NoSuccess;
+import org.ogf.saga.error.NotImplemented;
+
+import java.io.PrintStream;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   UserPassSecurityAdaptor
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   19 juin 2007
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class UserPassSecurityAdaptor implements SecurityAdaptor {
+    private String m_userId;
+    private String m_userPass;
+
+    public UserPassSecurityAdaptor(String userId, String userPass) {
+        m_userId = userId;
+        m_userPass = userPass;
+    }
+
+    public String getUserID() {
+        return m_userId;
+    }
+
+    public String getAttribute(String key) throws NotImplemented, NoSuccess {
+        if (Context.USERPASS.equals(key)) {
+            return m_userPass;
+        } else {
+            throw new NotImplemented("Attribute not supported: "+key);
+        }
+    }
+
+    public String getUserPass() {
+        return m_userPass;
+    }
+
+    public void close() throws Exception {
+        // do nothing
+    }
+
+    public void dump(PrintStream out) throws Exception {
+        out.println("  UserID   : "+ m_userId);
+        out.print("  UserPass : ");
+        for (int i=0; m_userPass!=null && i<m_userPass.length(); i++) {
+            out.print('*');
+        }
+        out.println();
+    }
+}
