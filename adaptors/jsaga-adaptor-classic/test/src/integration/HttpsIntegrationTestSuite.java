5b4d82f24be640be41d769f86475ad30c882e358
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-03-26 17:41:22 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-classic/test/src/integration/HttpsIntegrationTestSuite.java
@@ -0,0 +1,59 @@
+package integration;
+
+import junit.framework.TestSuite;
+import junit.framework.Test;
+import org.ogf.saga.namespace.*;
+import org.ogf.saga.file.*;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   HttpsIntegrationTestSuite
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   26 mars 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class HttpsIntegrationTestSuite extends TestSuite {
+    public static class HttpsNSEntryTest extends NSEntryTest {
+        public HttpsNSEntryTest() throws Exception {super("https");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    public static class HttpsDirectoryListTest extends DirectoryListTest {
+        public HttpsDirectoryListTest() throws Exception {super("https");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    public static class HttpsDirectoryTest extends DirectoryTest {
+        public HttpsDirectoryTest() throws Exception {super("https");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    public static class HttpsFileReadTest extends FileReadTest {
+        public HttpsFileReadTest() throws Exception {super("https");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    public static class Https_to_EmulatorNSCopyTest extends NSCopyTest {
+        public Https_to_EmulatorNSCopyTest() throws Exception {super("https", "test");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+    public static class Https_to_EmulatorNSCopyRecursiveTest extends NSCopyRecursiveTest {
+        public Https_to_EmulatorNSCopyRecursiveTest() throws Exception {super("https", "test");}
+        public void setUp() throws Exception {System.out.println(this.getClass()); super.setUp();}
+    }
+
+    public HttpsIntegrationTestSuite() throws Exception {
+        super();
+        this.addTestSuite(HttpsNSEntryTest.class);
+        this.addTestSuite(HttpsDirectoryListTest.class);
+        this.addTestSuite(HttpsDirectoryTest.class);
+        this.addTestSuite(HttpsFileReadTest.class);
+        this.addTestSuite(Https_to_EmulatorNSCopyTest.class);
+        this.addTestSuite(Https_to_EmulatorNSCopyRecursiveTest.class);
+    }
+
+    public static Test suite() throws Exception {
+        return new HttpsIntegrationTestSuite();
+    }
+}
