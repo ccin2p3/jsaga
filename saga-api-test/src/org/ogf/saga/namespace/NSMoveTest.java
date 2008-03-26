91f99228978e0913dd1a5a2584e53179b6e0cebd
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-03-26 09:48:11 +0000

    test methods moved from NSCopyTest and NSCopyRecursiveTest

---
+++ saga-api-test/src/org/ogf/saga/namespace/NSMoveTest.java
@@ -0,0 +1,56 @@
+package org.ogf.saga.namespace;
+
+import org.ogf.saga.URL;
+import org.ogf.saga.error.DoesNotExist;
+import org.ogf.saga.namespace.abstracts.AbstractNSCopyTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   NSMoveTest
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   25 mars 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class NSMoveTest extends AbstractNSCopyTest {
+    public NSMoveTest(String protocol, String targetProtocol) throws Exception {
+        super(protocol, targetProtocol);
+    }
+
+    public void test_move() throws Exception {
+        URL target = createURL(m_rootUrl2, DEFAULT_FILENAME);
+        m_file.move(m_rootUrl2, Flags.NONE.getValue());
+        checkCopied(target, DEFAULT_CONTENT);
+        try {
+            NSFactory.createNSEntry(m_session, m_fileUrl, Flags.NONE.getValue());
+            fail("Expected DoesNotExist exception");
+        } catch(DoesNotExist e) {
+        }
+    }
+
+    public void test_rename() throws Exception {
+        URL target = createURL(m_rootUrl, DEFAULT_FILENAME);
+        m_file.move(m_rootUrl, Flags.NONE.getValue());
+        checkCopied(target, DEFAULT_CONTENT);
+        try {
+            NSFactory.createNSEntry(m_session, m_fileUrl, Flags.NONE.getValue());
+            fail("Expected DoesNotExist exception");
+        } catch(DoesNotExist e) {
+        }
+    }
+
+    public void test_move_recurse() throws Exception {
+        URL target = createURL(m_dirUrl2, DEFAULT_DIRNAME+DEFAULT_FILENAME);
+        m_dir.move(m_dirUrl2, Flags.RECURSIVE.getValue());
+        checkCopied(target, DEFAULT_CONTENT);
+        try {
+            NSFactory.createNSDirectory(m_session, m_dirUrl, Flags.NONE.getValue());
+            fail("Expected DoesNotExist exception");
+        } catch(DoesNotExist e) {
+        }
+    }
+}
