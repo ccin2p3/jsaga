6dc6c4404cc7110572435b9dde4bdc624a3ac606
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-03-28 09:54:17 +0000

    Initial revision

---
+++ jsaga-engine/test/src/org/ogf/saga/job/EmulatorJobRunMinimalTest.java
@@ -0,0 +1,19 @@
+package org.ogf.saga.job;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   EmulatorJobRunMinimalTest
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   28 mars 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class EmulatorJobRunMinimalTest extends JobRunMinimalTest {
+    public EmulatorJobRunMinimalTest() throws Exception {
+        super("test");
+    }
+}
