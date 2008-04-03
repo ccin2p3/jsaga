cb6b8d2995af3c32328273a69a7d5f2f0a96642d
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-04-03 08:47:04 +0000

    Initial revision

---
+++ jsaga-engine/test/src/org/ogf/saga/job/EmulatorJobDescriptionTest.java
@@ -0,0 +1,19 @@
+package org.ogf.saga.job;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   EmulatorJobDescriptionTest
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   3 avr. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class EmulatorJobDescriptionTest extends JobDescriptionTest {
+    public EmulatorJobDescriptionTest() throws Exception {
+        super("test");
+    }
+}
