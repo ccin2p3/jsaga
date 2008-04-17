c1adccd2faad260b958340cdd1830050995bec5e
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-04-17 14:33:03 +0000

    initial implementation

--- jsaga-engine/src/fr/in2p3/jsaga/engine/job/monitor/poll/FilteredJobStatusPoller.java
+++ jsaga-engine/src/fr/in2p3/jsaga/engine/job/monitor/poll/FilteredJobStatusPoller.java
@@ -1,7 +1,11 @@
 package fr.in2p3.jsaga.engine.job.monitor.poll;
 
+import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 import fr.in2p3.jsaga.adaptor.job.monitor.QueryFilteredJob;
+import fr.in2p3.jsaga.engine.job.monitor.JobMonitorCallback;
 import org.apache.log4j.Logger;
+
+import java.util.Calendar;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -25,6 +29,26 @@ public class FilteredJobStatusPoller extends AbstractJobStatusPoller {
     }
 
     public void run() {
-        //todo:
+        Object[] filters = new Object[3];
+        filters[QueryFilteredJob.USER_ID] = null;           //todo: set filter value
+        filters[QueryFilteredJob.COLLECTION_NAME] = null;   //todo: set filter value
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_YEAR, -1);
+        filters[QueryFilteredJob.START_DATE] = cal.getTime();
+        try {
+            JobStatus[] statusArray = m_adaptor.getFilteredStatus(filters);
+            for (JobStatus status : statusArray) {
+                String nativeJobId = status.getNativeJobId();
+                JobMonitorCallback callback;
+                synchronized(m_subscribedJobs) {
+                    callback = m_subscribedJobs.get(nativeJobId);
+                }
+                if (callback != null) {
+                    callback.setState(status.getSagaState(), status.getStateDetail(), status.getSubState());
+                }
+            }
+        } catch (Exception e) {
+            s_logger.warn("Failed to get status for filtered jobs", e);
+        }
     }
 }
