8923578be7cd8d960996f4bdb2fb1688b96e1a3b
Nicolas Demesy <nicolas.demesy@bt.com>
2008-04-18 08:17:24 +0000

    Add CleanableJobAdaptor implementation and remove getStdout/Stderr

--- adaptors/jsaga-adaptor-ssh/src/fr/in2p3/jsaga/adaptor/ssh/job/SSHJobControlAdaptor.java
+++ adaptors/jsaga-adaptor-ssh/src/fr/in2p3/jsaga/adaptor/ssh/job/SSHJobControlAdaptor.java
@@ -1,22 +1,17 @@
 package fr.in2p3.jsaga.adaptor.ssh.job;
 
 import fr.in2p3.jsaga.adaptor.job.control.JobControlAdaptor;
+import fr.in2p3.jsaga.adaptor.job.control.advanced.CleanableJobAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobMonitorAdaptor;
 import fr.in2p3.jsaga.adaptor.ssh.SSHAdaptorAbstract;
 
-import org.ggf.schemas.jsdl.posix.Argument;
-import org.ggf.schemas.jsdl.posix.POSIXApplication_Type;
 import org.ogf.saga.error.NoSuccess;
 import org.ogf.saga.error.PermissionDenied;
 import org.ogf.saga.error.Timeout;
 
 import com.jcraft.jsch.ChannelExec;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.StringReader;
 import java.util.Date;
-import java.util.Hashtable;
 import java.util.Map;
 
 /* ***************************************************
@@ -32,7 +27,7 @@ import java.util.Map;
  * TODO : Support of pre-requisite
  */
 public class SSHJobControlAdaptor extends SSHAdaptorAbstract implements
-		JobControlAdaptor {
+		JobControlAdaptor, CleanableJobAdaptor {
 
 	public String getType() {
 		return "ssh";
@@ -58,24 +53,15 @@ public class SSHJobControlAdaptor extends SSHAdaptorAbstract implements
 			throws PermissionDenied, Timeout, NoSuccess {
 		try {
 
-			String jobId = String.valueOf(new Date().getTime());
-
 			ChannelExec channel = (ChannelExec) session.openChannel("exec");
 			channel.setCommand(commandLine);
-
-			// set stderr / stdout
-  			FileOutputStream stdout =new FileOutputStream(System.getProperty("user.home")+ File.separator + jobId+ ".stdout");
-  			channel.setOutputStream(stdout);
-			FileOutputStream stderr =new FileOutputStream(System.getProperty("user.home")+ File.separator + jobId+ ".stderr");
-			channel.setErrStream(stderr);
 			
 			// start job
 			channel.connect();
 			
 			// add channel in sessionMap
-			SSHAdaptorAbstract.sessionMap.put(jobId+"-id", channel);
-			SSHAdaptorAbstract.sessionMap.put(jobId+"-stdout", stdout);
-			SSHAdaptorAbstract.sessionMap.put(jobId+"-stderr", stderr);
+			String jobId = String.valueOf(new Date().getTime());
+			SSHAdaptorAbstract.sessionMap.put(jobId, channel);
 			
 			return jobId;
 		} catch (Exception e) {
@@ -89,6 +75,15 @@ public class SSHJobControlAdaptor extends SSHAdaptorAbstract implements
 
 		} catch (Exception e) {
 			throw new NoSuccess(e);
+		}
+	}
+
+	public void clean(String nativeJobId) throws PermissionDenied, Timeout,
+			NoSuccess {
+		
+		ChannelExec channel = (ChannelExec) SSHAdaptorAbstract.sessionMap.get(nativeJobId);
+		if(channel.isClosed()) {
+			channel.disconnect();
 		}
 	}
 
