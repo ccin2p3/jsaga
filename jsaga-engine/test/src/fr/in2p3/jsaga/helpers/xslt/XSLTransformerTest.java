e7d8ae754a995c00fed9e76518979fb39acd1f0d
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-04-19 11:16:03 +0000

    Initial revision

---
+++ jsaga-engine/test/src/fr/in2p3/jsaga/helpers/xslt/XSLTransformerTest.java
@@ -0,0 +1,61 @@
+package fr.in2p3.jsaga.helpers.xslt;
+
+import junit.framework.TestCase;
+import org.ogf.saga.URL;
+
+import java.io.ByteArrayInputStream;
+import java.util.Properties;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   XSLTransformerTest
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   17 avr. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class XSLTransformerTest extends TestCase {
+    public void test_wrapper_1_generate_URL() throws Exception {
+        final URL[] urls = new URL[] {
+                new URL("gsiftp://host:2811"),
+                new URL("gsiftp://host:2811/absolute/path/to/file.txt"),
+                new URL("gsiftp://host:2811/./relative/path/to/file.txt"),
+                new URL("srb://user@host/absolute/path/to/file.txt"),
+                new URL("srb:///absolute/path/to/file.txt"),
+                new URL("file:///C:/absolute/path/to/dir/"),
+                new URL("file:///absolute/path/to/dir/"),
+                new URL("file://./relative/path/to/file.txt"),
+                new URL("file://$PWD/relative/path/to/file.txt")
+        };
+        XSLTransformer t = XSLTransformerFactory.getInstance().create("xsl/execution/wrapper_1-generate.xsl");
+        for (int i=0; i<urls.length; i++) {
+            byte[] xml = ("<test>"+urls[i].getURL()+"</test>").getBytes();
+            Properties prop = new Properties();
+            prop.load(new ByteArrayInputStream(t.transform(xml)));
+            assertEquals(
+                    "Bad scheme for URL: "+prop.getProperty("URL"),
+                    urls[i].getScheme()!=null ? urls[i].getScheme() : "",
+                    prop.getProperty("SCHEME"));
+            assertEquals(
+                    "Bad userInfo for URL: "+prop.getProperty("URL"),
+                    urls[i].getUserInfo()!=null ? urls[i].getUserInfo() : "",
+                    prop.getProperty("USER"));
+            assertEquals(
+                    "Bad host for URL: "+prop.getProperty("URL"),
+                    urls[i].getHost()!=null ? urls[i].getHost() : "",
+                    prop.getProperty("HOST"));
+            assertEquals(
+                    "Bad port for URL: "+prop.getProperty("URL"),
+                    urls[i].getPort()>-1 ? urls[i].getPort()+"" : "",
+                    prop.getProperty("PORT"));
+            assertEquals(
+                    "Bad path for URL: "+prop.getProperty("URL"),
+                    urls[i].getPath()!=null ? urls[i].getPath() : "",
+                    prop.getProperty("PATH"));
+        }
+    }
+}
