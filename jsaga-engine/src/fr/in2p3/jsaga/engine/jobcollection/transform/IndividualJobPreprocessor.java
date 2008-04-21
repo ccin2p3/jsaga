85bb50fa1e9d0bd511b39c450c33f33507de5652
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-04-21 08:16:00 +0000

    initial implementation

--- jsaga-engine/src/fr/in2p3/jsaga/engine/jobcollection/transform/IndividualJobPreprocessor.java
+++ jsaga-engine/src/fr/in2p3/jsaga/engine/jobcollection/transform/IndividualJobPreprocessor.java
@@ -1,6 +1,19 @@
 package fr.in2p3.jsaga.engine.jobcollection.transform;
 
+import fr.in2p3.jsaga.Base;
+import fr.in2p3.jsaga.engine.jobcollection.preprocess.XMLDocument;
+import fr.in2p3.jsaga.engine.schema.jsdl.extension.Resource;
+import fr.in2p3.jsaga.helpers.xslt.XSLTransformerFactory;
+import fr.in2p3.jsaga.impl.job.description.XJSDLJobDescriptionImpl;
+import org.ogf.saga.error.NoSuccess;
+import org.ogf.saga.error.NotImplemented;
 import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -15,8 +28,59 @@ import org.w3c.dom.Document;
  *
  */
 public class IndividualJobPreprocessor {
-    public static Document preprocess(Document jobDesc) {
-        //todo: insert <jobService> configuration into <ext:Job>
-        return jobDesc;
+    private static final String XML_STAGE_GRAPH = "stage-graph.xml";
+
+    private static final String XSL_1_ADD_FILESYSTEMS = "xsl/execution/job_1-add-filesystems.xsl";
+    private static final String XSL_2_SIMPLIFY = "xsl/execution/job_2-simplify.xsl";
+    private static final String XSL_3_GENERATE_STAGE = "xsl/execution/job_3-generate-stage.xsl";
+    private static final String XSL_UPDATE_GRAPH = "xsl/execution/graph_1-update.xsl";
+
+    private Document m_effectiveJob;
+    private byte[] m_stageGraph;
+
+    public IndividualJobPreprocessor(XJSDLJobDescriptionImpl jobDesc, Resource rm) throws NotImplemented, NoSuccess {
+        // Set stylesheet parameters
+        Map<String,String> parameters = new HashMap<String,String>();
+        parameters.put("resourceId", rm.getId());
+        parameters.put("gridName", rm.getGrid());
+        if(rm.getIntermediary()!=null) parameters.put("Intermediary", rm.getIntermediary());
+        if(rm.hasStandaloneWorker()) parameters.put("StandaloneWorker", ""+rm.getStandaloneWorker());
+        if(rm.getTagCount()>0) parameters.put("Tag", rm.getTag(0));
+        parameters.put("collectionName", jobDesc.getCollectionName());
+
+        // Set base directory
+        File baseDir = new File(Base.JSAGA_VAR, "jobs");
+        if(!baseDir.exists()) baseDir.mkdir();
+        baseDir = new File(baseDir, jobDesc.getCollectionName());
+        if(!baseDir.exists()) baseDir.mkdir();
+
+        // Transform
+        XSLTransformerFactory t = XSLTransformerFactory.getInstance();
+        XMLDocument jobContainer = new XMLDocument(new File(baseDir, jobDesc.getJobName()+".xml"));
+        XMLDocument stageContainer = new XMLDocument(new File(baseDir, XML_STAGE_GRAPH));
+        try {
+            jobContainer.set(t.getCached(XSL_1_ADD_FILESYSTEMS, parameters).transform(jobDesc.getAsDocument().getDocumentElement()));
+            jobContainer.set(t.getCached(XSL_2_SIMPLIFY).transform(jobContainer.get()));
+            jobContainer.set(t.getCached(XSL_3_GENERATE_STAGE).transform(jobContainer.get()));
+            jobContainer.save();
+
+            stageContainer.set(t.getCached(XSL_UPDATE_GRAPH).transform(jobContainer.get()));
+            stageContainer.save();
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            m_effectiveJob = factory.newDocumentBuilder().parse(new ByteArrayInputStream(jobContainer.get()));
+            m_stageGraph = stageContainer.get();
+        } catch (Exception e) {
+            throw new NoSuccess(e);
+        }
+    }
+
+    public Document getEffectiveJobDescription() {
+        return m_effectiveJob;
+    }
+
+    public byte[] getStageGraph() {
+        return m_stageGraph;
     }
 }
