7224b7d123ea8f3cc531d91286e305788406f2ec
Nicolas Demesy <nicolas.demesy@bt.com>
2008-04-30 13:03:15 +0000

    Add test_simultaneousStdin

--- saga-api-test/src/org/ogf/saga/job/JobRunInteractiveTest.java
+++ saga-api-test/src/org/ogf/saga/job/JobRunInteractiveTest.java
@@ -2,11 +2,15 @@ package org.ogf.saga.job;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 
-import org.ogf.saga.error.NotImplemented;
+import org.ogf.saga.error.NoSuccess;
 import org.ogf.saga.job.abstracts.AbstractJobTest;
 import org.ogf.saga.job.abstracts.AttributeVector;
 import org.ogf.saga.task.State;
+import org.ogf.saga.task.TaskContainer;
+import org.ogf.saga.task.TaskFactory;
+import org.ogf.saga.task.WaitMode;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -17,8 +21,8 @@ import org.ogf.saga.task.State;
 * Date:   30 janv. 2008
 * ***************************************************
 * Description: 
-* This test suite is made to be test advanced functions
-* 											          */
+* This test suite is made to be test "pseudo" 
+* interactive jobs									 */
 
 public class JobRunInteractiveTest extends AbstractJobTest {
 	
@@ -26,6 +30,50 @@ public class JobRunInteractiveTest extends AbstractJobTest {
         super(jobprotocol);
     }
 
+	/**
+     * Runs a job,write in stdin, wait for done and check stdout
+     */
+    public void test_setStdin() throws Exception {
+        
+    	// prepare interactive job
+    	String stringToTestInStdout = "     1\tTest 1";
+    	JobDescription desc = createJob("cat", null, null);
+    	desc.setAttribute(JobDescription.INTERACTIVE, JobDescription.TRUE);
+    	desc.setVectorAttribute(JobDescription.ARGUMENTS, new String[]{"-n"});
+    	
+        // create
+        Job job = createJob(desc);
+        
+        // get stdin
+        OutputStream stdin = job.getStdin();
+        stdin.write("Test 1".getBytes());
+        stdin.close();
+        
+        // run
+        job.run();
+        
+        // wait for the end
+        job.waitFor();
+
+        // check job for DONE status
+        checkStatus(job.getState(), State.DONE);
+
+        // check stdout
+    	String stdout = "";
+    	BufferedReader jobStdoutReader = new BufferedReader(new InputStreamReader(job.getStdout()));
+        String input;
+        if(jobStdoutReader != null) {
+	        while ((input = jobStdoutReader.readLine()) !=null ){
+	        	stdout += input;
+	        }
+        }
+        
+    	// check
+        assertEquals(
+        		stringToTestInStdout,
+        		stdout);
+    }
+    
     /**
      * Runs a job, waits for done and check stdout
      */
@@ -38,36 +86,30 @@ public class JobRunInteractiveTest extends AbstractJobTest {
     	
         // submit
         Job job = runJob(desc);
-
+        
         // wait for the end
         job.waitFor();
 
         // check job for DONE status
         checkStatus(job.getState(), State.DONE);
 
-        // get stdout
-        try {
-        	
-        	// check stdout
-        	boolean containsTest = false;
-        	BufferedReader jobStdoutReader = new BufferedReader( new InputStreamReader(job.getStdout()));
-            String input;
-            System.out.println("Get stdout:");
-            while ((input = jobStdoutReader.readLine()) !=null ){
-            	System.out.println(input);
-            	if(input.indexOf(stringToTestInStdout) > -1) {
-            		containsTest = true;
-            		break;
-            	}
-            }
-        	// check
-	        assertEquals(
-	        		containsTest,
-	                true);
+        // check stdout
+    	String stdout = "";
+    	BufferedReader jobStdoutReader = new BufferedReader(new InputStreamReader(job.getStdout()));
+        String input;
+        if(jobStdoutReader != null) {
+	        while ((input = jobStdoutReader.readLine()) !=null ){
+	        	if(input.indexOf(stringToTestInStdout) > -1) {
+	        		stdout = input;
+	        		break;
+	        	}
+	        }
         }
-        catch (NotImplemented notImplemented) {
-        	System.err.println("WARNING : "+this.getName()+" not implemented in plugin");
-        }
+        
+    	// check
+        assertEquals(
+        		stringToTestInStdout,
+        		stdout);
     }
     
     /**
@@ -88,41 +130,36 @@ public class JobRunInteractiveTest extends AbstractJobTest {
         // check job for FAILED status
         checkStatus(job.getState(), State.FAILED);
 
-        // get stderr
-        try {
-	        boolean stderrNotEmpty = false;
-	        //test inputstream
-	        BufferedReader jobStdoutReader = new BufferedReader( new InputStreamReader( job.getStderr()));
-	        String firstLine = jobStdoutReader.readLine();
-	        if(firstLine != null) {
-	        	System.out.println("Stderr "+firstLine);
-	        	stderrNotEmpty = true;
-	        }
-	        	
-	        // check
-	        assertEquals(
-	                true,
-	                stderrNotEmpty);
+      	String stderrEmpty = "";
+        //test stderr
+        BufferedReader jobStdoutReader = new BufferedReader( new InputStreamReader( job.getStderr()));
+        String firstLine = jobStdoutReader.readLine();
+        if(firstLine != null) {
+        	stderrEmpty = firstLine;
         }
-        catch (NotImplemented notImplemented) {
-        	System.err.println("WARNING : "+this.getName()+" not implemented in plugin");
-        }
+        	
+        // check
+        assertNotSame(
+                "",
+                stderrEmpty);
     }
 	
 	
 	/**
 	 * check the environment in job stdout 
-	 * Runs job with requested environment variable
+	 * Runs job with requested environment variables
 	 */
 	public void test_run_environnement() throws Exception {
 	    
 		// prepare
+		String env0 = "MYVAR3=Testing";
 		String env1 = "MYVAR1=Testing 1";
 		String env2 = "MYVAR2=Testing two";
 		AttributeVector[] attributesV = new AttributeVector[1];
-		attributesV[0] = new AttributeVector(JobDescription.ENVIRONMENT, new String[]{env1, env2});
+		attributesV[0] = new AttributeVector(JobDescription.ENVIRONMENT, new String[]{env0, env1, env2});
 		JobDescription desc =  createJob("/bin/env", null, attributesV);
-		
+		desc.setAttribute(JobDescription.INTERACTIVE, JobDescription.TRUE);
+    	
 		// submit
 	    Job job = runJob(desc);
 	    
@@ -130,11 +167,13 @@ public class JobRunInteractiveTest extends AbstractJobTest {
 	    job.waitFor();  
 	    
 	    // check stdout
-	    boolean containsEnv1 = false, containsEnv2 = false;
+	    boolean containsEnv0 = false, containsEnv1 = false, containsEnv2 = false;
     	BufferedReader jobStdoutReader = new BufferedReader( new InputStreamReader(job.getStdout()));       
 	    String input;
 	    while ((input = jobStdoutReader.readLine()) !=null ){
-        	System.out.println(input);
+        	if(input.indexOf(env0) > -1) {
+        		containsEnv0 = true;
+        	}
         	if(input.indexOf(env1) > -1) {
         		containsEnv1 = true;
         	}
@@ -142,9 +181,79 @@ public class JobRunInteractiveTest extends AbstractJobTest {
         		containsEnv2 = true;
         	}
         }
+	    
     	// check
         assertEquals(
-        		containsEnv1 && containsEnv2,
-                true);
+        		true,
+        		containsEnv0 && containsEnv1 && containsEnv2);
 	}
+	
+	/**
+     * Runs short jobs on the same time with one task container
+     */
+    public void test_simultaneousStdin() throws Exception {
+        
+    	int numberOfJobs = Integer.parseInt(SIMULTANEOUS_JOB_NUMBER);
+
+    	TaskContainer taskContainer = TaskFactory.createTaskContainer();
+    	    	
+    	// create and start jobs
+    	JobService m_service = JobFactory.createJobService(m_session, m_jobservice);
+    	Job[] jobs = new Job[numberOfJobs];
+		for (int index = 0; index < numberOfJobs; index++) {
+			// create description
+	    	JobDescription desc = JobFactory.createJobDescription();
+    		desc.setAttribute(JobDescription.EXECUTABLE, "/bin/echo");
+	        desc.setAttribute(JobDescription.INTERACTIVE, JobDescription.TRUE);
+	    	
+	        jobs[index] = m_service.createJob(desc);
+	        // get stdin
+	        OutputStream stdin = jobs[index].getStdin();
+	        stdin.write(new String("Test "+index).getBytes());
+	        stdin.close();
+	        
+	        // add job to task			
+			taskContainer.add(jobs[index]);
+		}
+    
+		// run
+		taskContainer.run();
+		
+		// wait the end 
+		taskContainer.waitFor(WaitMode.ALL);
+    	
+		// get failed jobs
+		int numberOfFailed = 0;
+		for (int j = 0; j < jobs.length; j++) {
+			if(jobs[j].getState().getValue() == State.FAILED.getValue()) {
+				numberOfFailed ++;
+			}
+		}
+		
+		if(numberOfFailed > 1) 
+			throw new NoSuccess(numberOfFailed + "jobs of "+numberOfJobs+" are failed.");
+		if(numberOfFailed > 0) 
+			throw new NoSuccess(numberOfFailed + "job of "+numberOfJobs+" is failed.");
+		
+		int numberOfWrongStdout = 0;
+		for (int j = 0; j < jobs.length; j++) {
+			// check stdout
+			String stdout = "";
+	    	BufferedReader jobStdoutReader = new BufferedReader(new InputStreamReader(jobs[j].getStdout()));
+	        String input;
+	        if(jobStdoutReader != null) {
+		        while ((input = jobStdoutReader.readLine()) !=null ){
+		        	stdout += input;		        	
+		        }
+	        }
+	        
+	        if(!stdout.equals("Test "+j)) {
+	        	numberOfWrongStdout ++;
+	        }
+		}
+		
+    	assertEquals(
+	                0,
+	                numberOfWrongStdout);
+    }
 }
