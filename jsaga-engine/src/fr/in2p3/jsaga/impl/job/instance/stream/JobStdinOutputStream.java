a49754710e23f1f5b9c41c65ee4b89a4319ff6ac
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-04-30 13:21:23 +0000

    Initial revision

---
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/job/instance/stream/JobStdinOutputStream.java
@@ -0,0 +1,106 @@
+package fr.in2p3.jsaga.impl.job.instance.stream;
+
+import fr.in2p3.jsaga.adaptor.job.control.interactive.*;
+import org.ogf.saga.error.*;
+import org.ogf.saga.job.Job;
+
+import java.io.*;
+import java.lang.Exception;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   JobStdinOutputStream
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   30 avr. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class JobStdinOutputStream extends OutputStream {
+    private Job m_job;
+    private JobIOHandler m_ioHandler;
+
+    public JobStdinOutputStream(Job job) throws NotImplemented, DoesNotExist, Timeout, NoSuccess {
+        m_job = job;
+        switch(m_job.getState()) {
+            case NEW:
+            case RUNNING:
+                // OK
+                break;
+            default:
+                throw new DoesNotExist("Stdin is not available because job neither unsubmitted nor running");
+        }
+    }
+
+    public byte[] getBuffer() {
+        if (m_buffer != null) {
+            return m_buffer.toByteArray();
+        } else {
+            return new byte[0];
+        }
+    }
+    
+    public void openJobIOHandler(JobIOHandler ioHandler) throws PermissionDenied, Timeout, NoSuccess {
+        m_ioHandler = ioHandler;
+
+        // get stream
+        if (m_stream == null) {
+            if (m_ioHandler instanceof JobIOGetter) {
+                m_stream = ((JobIOGetter)m_ioHandler).getStdin();
+            } else if (m_ioHandler instanceof JobIOSetter) {
+                m_stream = new PipedStdin((JobIOSetter) m_ioHandler);
+            }
+        }
+
+        // dump buffer to stream
+        if (m_buffer!=null && m_buffer.size()>0) {
+            try {
+                m_stream.write(m_buffer.toByteArray());
+            } catch (IOException e) {
+                throw new NoSuccess(e);
+            }
+        }
+    }
+
+    /////////////////////////////////// interface OutputStream ///////////////////////////////////
+
+    public void write(int b) throws IOException {this.getStream().write(b);}
+    public void write(byte[] b) throws IOException {this.getStream().write(b);}
+    public void write(byte[] b, int off, int len) throws IOException {this.getStream().write(b, off, len);}
+    public void flush() throws IOException {this.getStream().flush();}
+    public void close() throws IOException {this.getStream().close();}
+
+    /////////////////////////////////////// private method ///////////////////////////////////////
+
+    private ByteArrayOutputStream m_buffer;
+    private OutputStream m_stream;
+    private OutputStream getStream() throws IOException {
+        try {
+            switch(m_job.getState()) {
+                case NEW:
+                    if (m_buffer == null) {
+                        m_buffer = new ByteArrayOutputStream();
+                    }
+                    return m_buffer;
+                case RUNNING:
+                    if (m_stream == null) {
+                        if (m_ioHandler instanceof JobIOGetterPseudo || m_ioHandler instanceof JobIOSetterPseudo) {
+                            throw new NotImplemented("Can not write to stdin because job is running and adaptor does not support job interactivity");
+                        } else if (m_ioHandler instanceof JobIOGetter) {
+                            m_stream = ((JobIOGetter)m_ioHandler).getStdin();
+                        } else if (m_ioHandler instanceof JobIOSetter) {
+                            m_stream = new PipedStdin((JobIOSetter) m_ioHandler);
+                        }
+                    }
+                    return m_stream;
+                default:
+                    throw new DoesNotExist("Stdin is not available because job neither unsubmitted nor running");
+            }
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+}
---
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/job/instance/stream/JobStdoutInputStream.java
@@ -0,0 +1,102 @@
+package fr.in2p3.jsaga.impl.job.instance.stream;
+
+import fr.in2p3.jsaga.adaptor.job.control.interactive.*;
+import org.ogf.saga.error.*;
+import org.ogf.saga.job.Job;
+
+import java.io.*;
+import java.lang.Exception;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   JobStdoutInputStream
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   30 avr. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class JobStdoutInputStream extends InputStream {
+    private Job m_job;
+    private JobIOHandler m_ioHandler;
+
+    public JobStdoutInputStream(Job job) throws NotImplemented, DoesNotExist, Timeout, NoSuccess {
+        m_job = job;
+        switch(m_job.getState()) {
+            case DONE:
+            case RUNNING:
+                // OK
+                break;
+            default:
+                throw new DoesNotExist("Stdout is not available because job is neither done nor running");
+        }
+    }
+
+    public void closeJobIOHandler(JobIOHandler ioHandler) throws PermissionDenied, Timeout, NoSuccess {
+        m_ioHandler = ioHandler;
+
+        // get stream
+        if (m_stream == null) {
+            if (m_ioHandler instanceof JobIOGetter) {
+                m_stream = ((JobIOGetter)m_ioHandler).getStdout();
+            } else if (m_ioHandler instanceof JobIOSetter) {
+                m_stream = new PipedStdout((JobIOSetter) m_ioHandler);
+            }
+        }
+
+        // dump stream to buffer
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        try {
+            int len;
+            byte[] bytes = new byte[1024];
+            while ( (len=m_stream.read(bytes)) > -1 ) {
+                buffer.write(bytes, 0, len);
+            }
+        } catch (IOException e) {
+            throw new NoSuccess(e);
+        }
+        m_buffer = new ByteArrayInputStream(buffer.toByteArray());
+    }
+
+    /////////////////////////////////// interface InputStream ///////////////////////////////////
+
+    public int read() throws IOException {return this.getStream().read();}
+    public int read(byte[] b) throws IOException {return this.getStream().read(b);}
+    public int read(byte[] b, int off, int len) throws IOException {return this.getStream().read(b, off, len);}
+    public long skip(long n) throws IOException {return this.getStream().skip(n);}
+    public void close() throws IOException {this.getStream().close();}
+
+    /////////////////////////////////////// private method ///////////////////////////////////////
+
+    private ByteArrayInputStream m_buffer;
+    private InputStream m_stream;
+    private InputStream getStream() throws IOException {
+        try {
+            switch(m_job.getState()) {
+                case DONE:
+                    if (m_buffer == null) {
+                        throw new NoSuccess("INTERNAL ERROR: JobIOHandler has not been closed");
+                    }
+                    return m_buffer;
+                case RUNNING:
+                    if (m_stream == null) {
+                        if (m_ioHandler instanceof JobIOGetter) {
+                            m_stream = ((JobIOGetter)m_ioHandler).getStdout();
+                        } else if (m_ioHandler instanceof JobIOSetter) {
+                            m_stream = new PipedStdout((JobIOSetter) m_ioHandler);
+                        } else {
+                            throw new NoSuccess("Can not read from stdout because job is running and adaptor does not support job interactivity");
+                        }
+                    }
+                    return m_stream;
+                default:
+                    throw new DoesNotExist("Stdout is not available because job is neither done nor running");
+            }
+        } catch (Exception e) {
+            throw new IOException(e.getMessage());
+        }
+    }
+}
