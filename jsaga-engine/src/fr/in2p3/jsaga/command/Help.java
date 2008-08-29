681b6673e911ec15a9dd87ced05cb51203f793bc
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-08-29 14:13:39 +0000

    refactored with introspectors

--- jsaga-engine/src/fr/in2p3/jsaga/command/Help.java
+++ jsaga-engine/src/fr/in2p3/jsaga/command/Help.java
@@ -4,14 +4,16 @@ import fr.in2p3.jsaga.adaptor.base.usage.Usage;
 import fr.in2p3.jsaga.adaptor.security.SecurityAdaptorBuilder;
 import fr.in2p3.jsaga.engine.config.Configuration;
 import fr.in2p3.jsaga.engine.config.bean.EngineConfiguration;
-import fr.in2p3.jsaga.engine.config.bean.ServiceEngineConfigurationAbstract;
 import fr.in2p3.jsaga.engine.factories.SecurityAdaptorBuilderFactory;
-import fr.in2p3.jsaga.engine.schema.config.*;
+import fr.in2p3.jsaga.engine.schema.config.Context;
 import fr.in2p3.jsaga.helpers.ASCIITableFormatter;
 import fr.in2p3.jsaga.helpers.StringArray;
+import fr.in2p3.jsaga.introspector.Introspector;
+import fr.in2p3.jsaga.introspector.IntrospectorFactory;
 import org.apache.commons.cli.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /* ***************************************************
  * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -31,8 +33,12 @@ public class Help extends AbstractCommand {
     private static final String OPT_SECURITY = "s", LONGOPT_SECURITY = "security",
             ARG_SECURITY_USAGE = "usage", ARG_SECURITY_DEFAULT = "default", ARG_SECURITY_MISSING = "missing",
             USAGE_OPT_SECURITY = "<mode> = "+ARG_SECURITY_USAGE+" | "+ ARG_SECURITY_DEFAULT +" | "+ARG_SECURITY_MISSING;
-    private static final String OPT_DATA = "d", LONGOPT_DATA = "data";
-    private static final String OPT_JOB = "j", LONGOPT_JOB = "job";
+    private static final String OPT_DATA = "d", LONGOPT_DATA = "data",
+            ARG_DATA_SERVICE = "service", ARG_DATA_CONTEXT = "context",
+            USAGE_OPT_DATA = "<mode> = "+ARG_DATA_SERVICE+" | "+ARG_DATA_CONTEXT;
+    private static final String OPT_JOB = "j", LONGOPT_JOB = "job",
+            ARG_JOB_SERVICE = "service", ARG_JOB_CONTEXT = "context",
+            USAGE_OPT_JOB = "<mode> = "+ARG_JOB_SERVICE+" | "+ARG_JOB_CONTEXT;
     private static final String LONGOPT_EFFECTIVE_CONFIG = "config";
 
     public Help() {
@@ -111,35 +117,25 @@ public class Help extends AbstractCommand {
         }
         else if (line.hasOption(OPT_DATA))
         {
-            Protocol[] protocolArray = config.getProtocolCfg().toXMLArray();
-            ASCIITableFormatter formatter = new ASCIITableFormatter(new String[] {
-                    "Protocol", "Aliases / Host pattern", "Service name"});
-            for (int p=0; p<protocolArray.length; p++) {
-                Protocol protocol = protocolArray[p];
-                printHostnames(
-                        formatter,
-                        protocol.getScheme(),
-                        StringArray.arrayToString(protocol.getSchemeAlias(), ","),
-                        config.getProtocolCfg(),
-                        protocol.getMapping());
+            String arg = line.getOptionValue(OPT_DATA);
+            if (arg.equals(ARG_DATA_SERVICE)) {
+                dumpServices(IntrospectorFactory.createNSIntrospector());
+            } else if (arg.equals(ARG_DATA_CONTEXT)) {
+                dumpContexts(IntrospectorFactory.createNSIntrospector());
+            } else {
+                command.printHelpAndExit("Missing required argument: "+USAGE_OPT_DATA);
             }
-            formatter.dump(System.out);
         }
         else if (line.hasOption(OPT_JOB))
         {
-            Execution[] jobArray = config.getJobserviceCfg().toXMLArray();
-            ASCIITableFormatter formatter = new ASCIITableFormatter(new String[] {
-                    "Execution", "Aliases / Host pattern", "Service name"});
-            for (int i=0; i<jobArray.length; i++) {
-                Execution job = jobArray[i];
-                printHostnames(
-                        formatter,
-                        job.getScheme(),
-                        StringArray.arrayToString(job.getSchemeAlias(), ","),
-                        config.getJobserviceCfg(),
-                        job.getMapping());
+            String arg = line.getOptionValue(OPT_JOB);
+            if (arg.equals(ARG_JOB_SERVICE)) {
+                dumpServices(IntrospectorFactory.createJobIntrospector());
+            } else if (arg.equals(ARG_JOB_CONTEXT)) {
+                dumpContexts(IntrospectorFactory.createJobIntrospector());
+            } else {
+                command.printHelpAndExit("Missing required argument: "+USAGE_OPT_JOB);
             }
-            formatter.dump(System.out);
         }
         else if (line.hasOption(LONGOPT_EFFECTIVE_CONFIG))
         {
@@ -147,58 +143,34 @@ public class Help extends AbstractCommand {
         }
     }
 
-    private static void printHostnames(ASCIITableFormatter formatter, String column1, String column2, ServiceEngineConfigurationAbstract config, Mapping mapping) throws Exception {
-        Set hostnameSet = getHostnames(mapping);
-        if (hostnameSet.size() == 1) {
-            ServiceRef[] serviceRefs = config.listServiceRefByHostname(mapping, "*.*");
-            formatter.append(new String[] {
-                    column1,
-                    column2,
-                    serviceRefs.length>0 ? serviceRefArrayToString(serviceRefs) : null});
-        } else {
-            formatter.append(new String[] {
-                    column1,
-                    column2,
-                    null});
-            for (Iterator it= hostnameSet.iterator(); it.hasNext(); ) {
-                String hostname = (String) it.next();
-                ServiceRef[] serviceRefs = config.listServiceRefByHostname(mapping, hostname);
-                if (serviceRefs.length > 0) {
-                    formatter.append(new String[] {
-                            null,
-                            "    "+hostname,
-                            serviceRefArrayToString(serviceRefs)});
-                }
+    private static void dumpServices(Introspector introspector) throws Exception {
+        ASCIITableFormatter formatter = new ASCIITableFormatter(new String[] {
+                "Scheme", "Host pattern", "Service name"});
+        for (String scheme : introspector.getVectorAttribute(Introspector.SCHEME)) {
+            Introspector nsScheme = introspector.getChildIntrospector(scheme);
+            boolean isFirst = true;
+            for (String hostPattern : nsScheme.getVectorAttribute(Introspector.HOST_PATTERN)) {
+                Introspector nsHostPattern = nsScheme.getChildIntrospector(hostPattern);
+                formatter.append(new String[] {
+                        isFirst ? scheme : null,
+                        hostPattern,
+                        StringArray.arrayToString(nsHostPattern.getVectorAttribute(Introspector.SERVICE), ",")});
+                isFirst = false;
             }
         }
+        formatter.dump(System.out);
     }
 
-    private static Set getHostnames(Mapping mapping) {
-        Set hostnameSet = new HashSet();
-        for (int d=0; d<mapping.getDomainCount(); d++) {
-            Domain domain = mapping.getDomain(d);
-            for (int h=0; h<domain.getHostCount(); h++) {
-                Host host = domain.getHost(h);
-                hostnameSet.add(host.getPrefix()+"*"+getDomain(domain.getName()));
-            }
-            hostnameSet.add("*"+getDomain(domain.getName()));
+    private static void dumpContexts(Introspector introspector) throws Exception {
+        ASCIITableFormatter formatter = new ASCIITableFormatter(new String[] {
+                "Scheme", "Context name"});
+        for (String scheme : introspector.getVectorAttribute(Introspector.SCHEME)) {
+            Introspector nsScheme = introspector.getChildIntrospector(scheme);
+            formatter.append(new String[] {
+                    scheme,
+                    StringArray.arrayToString(nsScheme.getVectorAttribute(Introspector.CONTEXT), ",")});
         }
-        hostnameSet.add("*.*");
-        return hostnameSet;
-    }
-    private static String getDomain(String domainName) {
-        return (domainName!=null ? "."+domainName : "");
-    }
-
-    private static String serviceRefArrayToString(ServiceRef ctxArray[]) {
-        // convert to string array
-        String[] strArray = new String[ctxArray.length];
-        for (int c=0; c<ctxArray.length; c++) {
-            ServiceRef ctx = ctxArray[c];
-            strArray[c] = ctx.getName();
-        }
-        // string array to comma-separated string
-        return StringArray.arrayToString(strArray, ", ");
+        formatter.dump(System.out);
     }
 
     protected Options createOptions() {
@@ -219,11 +191,15 @@ public class Help extends AbstractCommand {
                     .withArgName("mode")
                     .hasArg()
                     .create(OPT_SECURITY));
-            group.addOption(OptionBuilder.withDescription("Information about data protocols")
+            group.addOption(OptionBuilder.withDescription("Information about data protocols.\n"+ USAGE_OPT_DATA)
                     .withLongOpt(LONGOPT_DATA)
+                    .withArgName("mode")
+                    .hasArg()
                     .create(OPT_DATA));
-            group.addOption(OptionBuilder.withDescription("Information about job services")
+            group.addOption(OptionBuilder.withDescription("Information about job services.\n"+ USAGE_OPT_JOB)
                     .withLongOpt(LONGOPT_JOB)
+                    .withArgName("mode")
+                    .hasArg()
                     .create(OPT_JOB));
             group.addOption(OptionBuilder.withDescription("Output the effective configuration")
                     .withLongOpt(LONGOPT_EFFECTIVE_CONFIG)
