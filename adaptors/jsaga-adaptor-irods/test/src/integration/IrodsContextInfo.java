baae79da4373f338138dca235fee23f551f03bfb
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-10-15 15:39:14 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-irods/test/src/integration/IrodsContextInfo.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInfoTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   IrodsContextInfo
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   15 oct. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class IrodsContextInfo extends ContextInfoTest {
+    public IrodsContextInfo() throws Exception {
+        super();
+    }
+}
---
+++ adaptors/jsaga-adaptor-irods/test/src/integration/IrodsContextInit.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInitTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   IrodsContextInit
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   15 oct. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class IrodsContextInit extends ContextInitTest {
+    public IrodsContextInit() throws Exception {
+        super("IRODS_SRB");
+    }
+}
