e4fd5bdf51efadcca104c24ddae1dab3485c63ff
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-11-05 15:38:22 +0000

    add integration tests

--- adaptors/jsaga-adaptor-irods/test/src/integration/IrodsIntegrationTestSuite.java
+++ adaptors/jsaga-adaptor-irods/test/src/integration/IrodsIntegrationTestSuite.java
@@ -2,7 +2,8 @@ package integration;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.ogf.saga.namespace.NSEntryTest;
+import org.ogf.saga.file.*;
+import org.ogf.saga.namespace.*;
 
 /* ***************************************************
  * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -17,22 +18,83 @@ import org.ogf.saga.namespace.NSEntryTest;
  *
  */
 public class IrodsIntegrationTestSuite extends TestSuite {
+    // test IRODS as a protocol for physical entries
     public static class IrodsNSEntryTest extends NSEntryTest {
         public IrodsNSEntryTest() throws Exception {super("irods");}
-        public void test_unexisting() { }
     }
-	/*
 	public static class IrodsDirectoryListTest extends DirectoryListTest {
-        public IrodsDirectoryListTest() throws Exception {
-			super("irods");
-		}
-        public void test_unexisting() { super.ignore("not yet implemented"); }
+        public IrodsDirectoryListTest() throws Exception {super("irods");}
+    }
+    public static class IrodsDirectoryMakeTest extends DirectoryMakeTest {
+        public IrodsDirectoryMakeTest() throws Exception {super("irods");}
+    }
+    public static class IrodsDirectoryTest extends DirectoryTest {
+        public IrodsDirectoryTest() throws Exception {super("irods");}
+    }
+    public static class IrodsFileReadTest extends FileReadTest {
+        public IrodsFileReadTest() throws Exception {super("irods");}
+    }
+    public static class IrodsFileWriteTest extends FileWriteTest {
+        public IrodsFileWriteTest() throws Exception {super("irods");}
+    }
+    public static class Irods_to_EmulatorNSCopyTest extends NSCopyTest {
+        public Irods_to_EmulatorNSCopyTest() throws Exception {super("irods", "test");}
+    }
+    public static class Irods_to_EmulatorNSCopyFromTest extends NSCopyFromTest {
+        public Irods_to_EmulatorNSCopyFromTest() throws Exception {super("irods", "test");}
+    }
+    public static class Irods_to_EmulatorNSCopyRecursiveTest extends NSCopyRecursiveTest {
+        public Irods_to_EmulatorNSCopyRecursiveTest() throws Exception {super("irods", "test");}
+    }
+    public static class Irods_to_EmulatorNSMoveTest extends NSMoveTest {
+        public Irods_to_EmulatorNSMoveTest() throws Exception {super("irods", "test");}
+    }
+
+/*
+    // test IRODS as a protocol for logical entries
+    public static class IrodsLogicalDirectoryListTest extends LogicalDirectoryListTest {
+        public IrodsLogicalDirectoryListTest() throws Exception {super("irods");}
+    }
+    public static class IrodsLogicalDirectoryMakeTest extends LogicalDirectoryMakeTest {
+        public IrodsLogicalDirectoryMakeTest() throws Exception {super("irods");}
+    }
+    public static class IrodsLogicalDirectoryMetaDataTest extends LogicalDirectoryMetaDataTest {
+        public IrodsLogicalDirectoryMetaDataTest() throws Exception {super("irods");}
+    }
+    public static class IrodsLogicalDirectoryTest extends LogicalDirectoryTest {
+        public IrodsLogicalDirectoryTest() throws Exception {super("irods");}
+    }
+    public static class IrodsLogicalFileReadTest extends LogicalFileReadTest {
+        public IrodsLogicalFileReadTest() throws Exception {super("irods");}
+    }
+    public static class IrodsLogicalFileWriteTest extends LogicalFileWriteTest {
+        public IrodsLogicalFileWriteTest() throws Exception {super("irods");}
     }
 */
+
     public IrodsIntegrationTestSuite() throws Exception {
         super();
+        // test SRB as a protocol for physical entries
         this.addTestSuite(IrodsNSEntryTest.class);
-		//this.addTestSuite(IrodsDirectoryListTest.class);
+		this.addTestSuite(IrodsDirectoryListTest.class);
+        this.addTestSuite(IrodsDirectoryMakeTest.class);
+        this.addTestSuite(IrodsDirectoryTest.class);
+        this.addTestSuite(IrodsFileReadTest.class);
+        this.addTestSuite(IrodsFileWriteTest.class);
+        this.addTestSuite(Irods_to_EmulatorNSCopyTest.class);
+        this.addTestSuite(Irods_to_EmulatorNSCopyFromTest.class);
+        this.addTestSuite(Irods_to_EmulatorNSCopyRecursiveTest.class);
+        this.addTestSuite(Irods_to_EmulatorNSMoveTest.class);
+
+/*
+        // test SRB as a protocol for logical entries
+        this.addTestSuite(IrodsLogicalDirectoryListTest.class);
+        this.addTestSuite(IrodsLogicalDirectoryMakeTest.class);
+        this.addTestSuite(IrodsLogicalDirectoryMetaDataTest.class);
+        this.addTestSuite(IrodsLogicalDirectoryTest.class);
+        this.addTestSuite(IrodsLogicalFileReadTest.class);
+        this.addTestSuite(IrodsLogicalFileWriteTest.class);
+*/
     }
 
     public static Test suite() throws Exception {
--- adaptors/jsaga-adaptor-irods/test/src/integration/SrbIntegrationTestSuite.java
+++ adaptors/jsaga-adaptor-irods/test/src/integration/SrbIntegrationTestSuite.java
@@ -2,7 +2,8 @@ package integration;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import org.ogf.saga.namespace.NSEntryTest;
+import org.ogf.saga.file.*;
+import org.ogf.saga.namespace.*;
 
 /* ***************************************************
  * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -17,22 +18,83 @@ import org.ogf.saga.namespace.NSEntryTest;
  *
  */
 public class SrbIntegrationTestSuite extends TestSuite {
+    // test SRB as a protocol for physical entries
     public static class SrbNSEntryTest extends NSEntryTest {
         public SrbNSEntryTest() throws Exception {super("srb");}
-        public void test_unexisting() { }
     }
-	/*
 	public static class SrbDirectoryListTest extends DirectoryListTest {
-        public SrbDirectoryListTest() throws Exception {
-			super("srb");
-		}
-        public void test_unexisting() { super.ignore("not yet implemented"); }
+        public SrbDirectoryListTest() throws Exception {super("srb");}
+    }
+    public static class SrbDirectoryMakeTest extends DirectoryMakeTest {
+        public SrbDirectoryMakeTest() throws Exception {super("srb");}
+    }
+    public static class SrbDirectoryTest extends DirectoryTest {
+        public SrbDirectoryTest() throws Exception {super("srb");}
+    }
+    public static class SrbFileReadTest extends FileReadTest {
+        public SrbFileReadTest() throws Exception {super("srb");}
+    }
+    public static class SrbFileWriteTest extends FileWriteTest {
+        public SrbFileWriteTest() throws Exception {super("srb");}
+    }
+    public static class Srb_to_EmulatorNSCopyTest extends NSCopyTest {
+        public Srb_to_EmulatorNSCopyTest() throws Exception {super("srb", "test");}
+    }
+    public static class Srb_to_EmulatorNSCopyFromTest extends NSCopyFromTest {
+        public Srb_to_EmulatorNSCopyFromTest() throws Exception {super("srb", "test");}
+    }
+    public static class Srb_to_EmulatorNSCopyRecursiveTest extends NSCopyRecursiveTest {
+        public Srb_to_EmulatorNSCopyRecursiveTest() throws Exception {super("srb", "test");}
+    }
+    public static class Srb_to_EmulatorNSMoveTest extends NSMoveTest {
+        public Srb_to_EmulatorNSMoveTest() throws Exception {super("srb", "test");}
+    }
+
+/*
+    // test SRB as a protocol for logical entries
+    public static class SrbLogicalDirectoryListTest extends LogicalDirectoryListTest {
+        public SrbLogicalDirectoryListTest() throws Exception {super("srb");}
+    }
+    public static class SrbLogicalDirectoryMakeTest extends LogicalDirectoryMakeTest {
+        public SrbLogicalDirectoryMakeTest() throws Exception {super("srb");}
+    }
+    public static class SrbLogicalDirectoryMetaDataTest extends LogicalDirectoryMetaDataTest {
+        public SrbLogicalDirectoryMetaDataTest() throws Exception {super("srb");}
+    }
+    public static class SrbLogicalDirectoryTest extends LogicalDirectoryTest {
+        public SrbLogicalDirectoryTest() throws Exception {super("srb");}
+    }
+    public static class SrbLogicalFileReadTest extends LogicalFileReadTest {
+        public SrbLogicalFileReadTest() throws Exception {super("srb");}
+    }
+    public static class SrbLogicalFileWriteTest extends LogicalFileWriteTest {
+        public SrbLogicalFileWriteTest() throws Exception {super("srb");}
     }
 */
+
     public SrbIntegrationTestSuite() throws Exception {
         super();
+        // test SRB as a protocol for physical entries
         this.addTestSuite(SrbNSEntryTest.class);
-		//this.addTestSuite(SrbDirectoryListTest.class);
+		this.addTestSuite(SrbDirectoryListTest.class);
+        this.addTestSuite(SrbDirectoryMakeTest.class);
+        this.addTestSuite(SrbDirectoryTest.class);
+        this.addTestSuite(SrbFileReadTest.class);
+        this.addTestSuite(SrbFileWriteTest.class);
+        this.addTestSuite(Srb_to_EmulatorNSCopyTest.class);
+        this.addTestSuite(Srb_to_EmulatorNSCopyFromTest.class);
+        this.addTestSuite(Srb_to_EmulatorNSCopyRecursiveTest.class);
+        this.addTestSuite(Srb_to_EmulatorNSMoveTest.class);
+
+/*
+        // test SRB as a protocol for logical entries
+        this.addTestSuite(SrbLogicalDirectoryListTest.class);
+        this.addTestSuite(SrbLogicalDirectoryMakeTest.class);
+        this.addTestSuite(SrbLogicalDirectoryMetaDataTest.class);
+        this.addTestSuite(SrbLogicalDirectoryTest.class);
+        this.addTestSuite(SrbLogicalFileReadTest.class);
+        this.addTestSuite(SrbLogicalFileWriteTest.class);
+*/
     }
 
     public static Test suite() throws Exception {
