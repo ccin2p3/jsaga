2d2a2f9d2718a21b45e0a38864766334eb8acd54
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-11-05 15:36:27 +0000

    Initial revision

---
+++ jsaga-engine/test/src/org/ogf/saga/logicalfile/CatalogLogicalDirectoryMetaDataTest.java
@@ -0,0 +1,19 @@
+package org.ogf.saga.logicalfile;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   CatalogLogicalDirectoryMetaDataTest
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   4 nov. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class CatalogLogicalDirectoryMetaDataTest extends LogicalDirectoryMetaDataTest {
+    public CatalogLogicalDirectoryMetaDataTest() throws Exception {
+        super("catalog");
+    }
+}
