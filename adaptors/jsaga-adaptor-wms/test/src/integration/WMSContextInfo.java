0396c237cc45e044ca96e2303d0ab233c4947248
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2008-12-10 16:11:04 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-wms/test/src/integration/WMSContextInfo.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInfoTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   WMSContextInfo
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   10 déc. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class WMSContextInfo extends ContextInfoTest {
+    public WMSContextInfo() throws Exception {
+        super();
+    }
+}
---
+++ adaptors/jsaga-adaptor-wms/test/src/integration/WMSContextInit.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInitTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   WMSContextInit
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   10 déc. 2008
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class WMSContextInit extends ContextInitTest {
+    public WMSContextInit() throws Exception {
+        super("EGEE");
+    }
+}
