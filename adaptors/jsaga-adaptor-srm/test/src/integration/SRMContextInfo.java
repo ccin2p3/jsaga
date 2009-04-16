edcbbc647794d43ec45be7e02bec99808c38c37d
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2009-04-16 14:09:49 +0000

    no message

---
+++ adaptors/jsaga-adaptor-srm/test/src/integration/SRMContextInfo.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInfoTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   SRMContextInfo
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   16 avr. 2009
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class SRMContextInfo extends ContextInfoTest {
+    public SRMContextInfo() throws Exception {
+        super();
+    }
+}
---
+++ adaptors/jsaga-adaptor-srm/test/src/integration/SRMContextInit.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInitTest;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   SRMContextInit
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   16 avr. 2009
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class SRMContextInit extends ContextInitTest {
+    public SRMContextInit() throws Exception {
+        super("EGEE");
+    }
+}
