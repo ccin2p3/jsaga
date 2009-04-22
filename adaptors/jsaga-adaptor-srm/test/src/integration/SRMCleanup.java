aa90432bff3d1dcc9986ebaec214c577eea9d556
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2009-04-22 08:43:23 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-srm/test/src/integration/SRMCleanup.java
@@ -0,0 +1,23 @@
+package integration;
+
+import org.ogf.saga.namespace.IntegrationClean;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   SRMCleanup
+* Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+* Date:   20 avr. 2009
+* ***************************************************
+* Description:                                      */
+/**
+ *
+ */
+public class SRMCleanup extends IntegrationClean {
+    public SRMCleanup() throws Exception {
+        super("srm", "srm");
+    }
+
+    public void test_dummy() {}
+}
