9fcbde9f7aa835ed02ac15383aae14e8a05fe59e
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2009-05-27 13:29:03 +0000

    add cleanup

--- saga-api-test/src/org/ogf/saga/job/JobRunSandboxTest.java
+++ saga-api-test/src/org/ogf/saga/job/JobRunSandboxTest.java
@@ -25,7 +25,7 @@ import java.util.UUID;
  *
  */
 public abstract class JobRunSandboxTest extends AbstractJobTest {
-    private static final String SCRIPT_CONTENT = "#!/bin/sh\n/usr/bin/cat $1 | /usr/bin/tr 'ou' 'ui' $2";
+    private static final String SCRIPT_CONTENT = "#!/bin/sh\n/usr/bin/cat ${1##file:/} | /usr/bin/tr 'ou' 'ui' > ${2##file:/}";
     private static final String INPUT_CONTENT = "coucou";
     private static final String OUTPUT_CONTENT = "cuicui";
     private static final File TMP = new File(System.getProperty("java.io.tmpdir"));
@@ -67,7 +67,7 @@ public abstract class JobRunSandboxTest extends AbstractJobTest {
         this.runJobOutput(getLocal("output"), getRemote("output"));
     }
     public void test_output_remote_from_remote() throws Exception {
-        this.runJobOutput(getRemote("output"), getRemote("output"));
+        this.runJobOutput(getRemote("output"), getRemote("output_target"));
     }
 
     //////////////////////////////////////////// private methods ////////////////////////////////////////////
@@ -79,7 +79,7 @@ public abstract class JobRunSandboxTest extends AbstractJobTest {
         return new File(TMP, "remote-"+m_uuid+"."+suffix).toURI();
     }
     private String getWorker(String suffix) {
-        return new File(TMP, "worker-"+m_uuid+"."+suffix).toURI().getPath();
+        return "/tmp/worker-"+m_uuid+"."+suffix;
     }
 
     private void runJobInput(Object localInput, Object workerInput) throws Exception {
@@ -92,14 +92,11 @@ public abstract class JobRunSandboxTest extends AbstractJobTest {
         // prepare
         this.put(localScript, SCRIPT_CONTENT.getBytes());
         this.put(localInput, INPUT_CONTENT.getBytes());
-        String scriptPath = toWorkerPath(workerScript);
-        String inputPath = toWorkerPath(workerInput);
-        String outputPath = toWorkerPath(workerOutput);
 
         // create job
         JobDescription desc = JobFactory.createJobDescription();
-        desc.setAttribute(JobDescription.EXECUTABLE, scriptPath);
-        desc.setVectorAttribute(JobDescription.ARGUMENTS, new String[]{inputPath, outputPath});
+        desc.setAttribute(JobDescription.EXECUTABLE, workerScript);
+        desc.setVectorAttribute(JobDescription.ARGUMENTS, new String[]{workerInput.toString(), workerOutput.toString()});
         desc.setVectorAttribute(JobDescription.FILETRANSFER, new String[]{
                 localScript+" > "+workerScript,
                 localInput+" > "+workerInput,
@@ -114,12 +111,31 @@ public abstract class JobRunSandboxTest extends AbstractJobTest {
         // wait
         job.waitFor();
 
+        // for debugging
+        if (State.FAILED.equals(job.getState())) {
+            // print stderr
+            byte[] buffer = new byte[1024];
+            InputStream stderr = job.getStderr();
+            for (int len; (len=stderr.read(buffer))>-1; ) {
+                System.err.write(buffer, 0, len);
+            }
+            stderr.close();
+
+            // rethrow exception
+            job.rethrow();
+        }
+
         // check job status
         assertEquals(State.DONE.getValue(), job.getState().getValue());
 
         // check job output
         String outputContent = this.get(localOutput);
         assertEquals(OUTPUT_CONTENT, outputContent);
+
+        // cleanup
+        this.cleanup(localScript);
+        this.cleanup(localInput);
+        this.cleanup(localOutput);
     }
 
     private void put(Object location, byte[] content) throws Exception {
@@ -146,26 +162,38 @@ public abstract class JobRunSandboxTest extends AbstractJobTest {
             InputStream in = new FileInputStream(file);
             int len = in.read(buffer);
             in.close();
-            return new String(buffer, 0, len);
-        } else if (location instanceof  URI) {
+            if (len > -1) {
+                return new String(buffer, 0, len);
+            } else {
+                throw new Exception("File is empty: "+file);
+            }
+        } else if (location instanceof URI) {
             URI uri = (URI) location;
             Buffer buffer = BufferFactory.createBuffer(BUFFER_SIZE);
             org.ogf.saga.file.File file = FileFactory.createFile(m_session, URLFactory.createURL(uri.toString()), Flags.READ.getValue());
             int len = file.read(buffer);
             file.close();
-            return new String(buffer.getData(), 0, len);
+            if (len > -1) {
+                return new String(buffer.getData(), 0, len);
+            } else {
+                throw new Exception("File is empty: "+uri);
+            }
         } else {
             throw new Exception("Unexpected class: "+location.getClass());
         }
     }
 
-    private static String toWorkerPath(Object location) throws Exception {
-        if (location instanceof String) {
-            String path = (String) location;
-            return path;
+    private void cleanup(Object location) throws Exception {
+        if (location instanceof File) {
+            File file = (File) location;
+            if (! file.delete()) {
+                throw new Exception("Failed to remove file: "+file);
+            }
         } else if (location instanceof URI) {
             URI uri = (URI) location;
-            return uri.getPath();
+            org.ogf.saga.file.File file = FileFactory.createFile(m_session, URLFactory.createURL(uri.toString()), Flags.NONE.getValue());
+            file.remove();
+            file.close();
         } else {
             throw new Exception("Unexpected class: "+location.getClass());
         }
