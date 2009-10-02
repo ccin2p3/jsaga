4b34f6a6e7e005aed295e93d9420f8ea96b7aaf1
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2009-10-02 12:34:35 +0000

    Initial revision

---
+++ jsaga-engine/src/fr/in2p3/jsaga/command/JobInfo.java
@@ -0,0 +1,89 @@
+package fr.in2p3.jsaga.command;
+
+import org.apache.commons.cli.*;
+import org.ogf.saga.error.BadParameterException;
+import org.ogf.saga.error.SagaException;
+import org.ogf.saga.job.*;
+import org.ogf.saga.session.Session;
+import org.ogf.saga.session.SessionFactory;
+import org.ogf.saga.url.URL;
+import org.ogf.saga.url.URLFactory;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   JobInfo
+ * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+ * Date:   2 oct. 2009
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class JobInfo extends AbstractCommand {
+    private static final String OPT_HELP = "h", LONGOPT_HELP = "help";
+
+    protected JobInfo() {
+        super("jsaga-job-info", new String[]{"jobId"}, new String[]{OPT_HELP, LONGOPT_HELP});
+    }
+
+    public static void main(String[] args) throws Exception {
+        JobInfo command = new JobInfo();
+        CommandLine line = command.parse(args);
+        if (line.hasOption(OPT_HELP))
+        {
+            command.printHelpAndExit(null);
+        }
+        else
+        {
+            // get arguments
+            URL serviceURL;
+            String nativeJobId;
+            Pattern pattern = Pattern.compile("\\[(.*)\\]-\\[(.*)\\]");
+            Matcher matcher = pattern.matcher(command.m_nonOptionValues[0]);
+            if (matcher.find()) {
+                serviceURL = URLFactory.createURL(matcher.group(1));
+                nativeJobId = matcher.group(2);
+            } else {
+                throw new BadParameterException("Job ID does not match regular expression: "+pattern.pattern());
+            }
+
+            // get status
+            Session session = SessionFactory.createSession(true);
+            JobService service = JobFactory.createJobService(session, serviceURL);
+            Job job = service.getJob(nativeJobId);
+
+            // dump info
+            System.out.println("State:         "+job.getState().toString());
+            System.out.println("Exit code:     "+job.getAttribute(Job.EXITCODE));
+            System.out.println("State reason:  "+getCause(job));
+            System.out.println("Created time:  "+job.getAttribute(Job.CREATED));
+            System.out.println("Started time:  "+job.getAttribute(Job.STARTED));
+            System.out.println("Finished time: "+job.getAttribute(Job.FINISHED));
+        }
+    }
+    private static String getCause(Job job) {
+        try {
+            job.rethrow();
+            return null;
+        } catch (SagaException e) {
+            return e.getMessage();
+        }
+    }
+
+    protected Options createOptions() {
+        Options opt = new Options();
+
+        // command
+        opt.addOption(OptionBuilder.withDescription("Display this help and exit")
+                .withLongOpt(LONGOPT_HELP)
+                .create(OPT_HELP));
+
+        // returns
+        return opt;
+    }
+}
---
+++ jsaga-engine/src/fr/in2p3/jsaga/command/JobList.java
@@ -0,0 +1,67 @@
+package fr.in2p3.jsaga.command;
+
+import org.apache.commons.cli.*;
+import org.ogf.saga.job.JobFactory;
+import org.ogf.saga.job.JobService;
+import org.ogf.saga.session.Session;
+import org.ogf.saga.session.SessionFactory;
+import org.ogf.saga.url.URL;
+import org.ogf.saga.url.URLFactory;
+
+import java.util.List;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   JobList
+ * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+ * Date:   1 oct. 2009
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class JobList extends AbstractCommand {
+    private static final String OPT_HELP = "h", LONGOPT_HELP = "help";
+
+    protected JobList() {
+        super("jsaga-job-list", new String[]{"resource"}, new String[]{OPT_HELP, LONGOPT_HELP});
+    }
+
+    public static void main(String[] args) throws Exception {
+        JobList command = new JobList();
+        CommandLine line = command.parse(args);
+        if (line.hasOption(OPT_HELP))
+        {
+            command.printHelpAndExit(null);
+        }
+        else
+        {
+            // get arguments
+            URL serviceURL = URLFactory.createURL(command.m_nonOptionValues[0]);
+
+            // get status
+            Session session = SessionFactory.createSession(true);
+            JobService service = JobFactory.createJobService(session, serviceURL);
+
+            // dump list
+            List<String> list = service.list();
+            for (String jobid : list) {
+                System.out.println(jobid);
+            }
+        }
+    }
+
+    protected Options createOptions() {
+        Options opt = new Options();
+
+        // command
+        opt.addOption(OptionBuilder.withDescription("Display this help and exit")
+                .withLongOpt(LONGOPT_HELP)
+                .create(OPT_HELP));
+
+        // returns
+        return opt;
+    }
+}
