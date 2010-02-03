b8f6c4a1c044a3111960e22584647866b9f1eb5b
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2010-02-03 11:31:07 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-unicore6/test/src/integration/U6ContextInfo.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInfoTest;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   U6ContextInfo
+ * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+ * Date:   3 févr. 2010
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class U6ContextInfo extends ContextInfoTest {
+    public U6ContextInfo() throws Exception {
+        super();
+    }
+}
