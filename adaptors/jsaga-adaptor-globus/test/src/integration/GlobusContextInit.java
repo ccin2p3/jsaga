97b2d028317bb588fee169dcb04a8e3011966109
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2010-03-02 16:30:35 +0000

    use D-Grid instead of local

--- adaptors/jsaga-adaptor-globus/test/src/integration/GlobusContextInit.java
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusContextInit.java
@@ -16,6 +16,6 @@ import org.ogf.saga.context.ContextInitTest;
  */
 public class GlobusContextInit extends ContextInitTest {
     public GlobusContextInit() throws Exception {
-        super("Test_Globus");
+        super("DGrid");
     }
 }
