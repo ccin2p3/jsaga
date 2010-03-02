647343a11cf1c000296fd57e4d1bc86eb6337cb0
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2010-03-02 17:40:01 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-wsgram/test/src/integration/WSGramContextInit.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInitTest;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   WSGramContextInit
+ * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+ * Date:   2 mars 2010
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class WSGramContextInit extends ContextInitTest {
+    public WSGramContextInit() throws Exception {
+        super("DGrid");
+    }
+}
