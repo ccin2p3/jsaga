d664a24ae7a4fc8193cae46b0e71a0d23422b690
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2010-03-05 14:17:07 +0000

    Initial revision

---
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusContextInitForWindows.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInitTest;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   GlobusContextInitForWindows
+ * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+ * Date:   5 mars 2010
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class GlobusContextInitForWindows extends ContextInitTest {
+    public GlobusContextInitForWindows() throws Exception {
+        super("Test_Globus");
+    }
+}
