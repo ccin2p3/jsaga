837c1f57d5a1c68473f99916b62ffdce582d98e8
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2010-03-16 17:36:16 +0000

    Initial revision

---
+++ jsaga-installer/test/resources/demo.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+./bin/jsaga-help.bat -s default
+
+# copier jsaga-universe.xml
+
+./bin/jsaga-context-info.bat
+./bin/jsaga-context-init.bat
+./bin/jsaga-context-info.bat
+
+./bin/jsaga-job-run.bat
+./bin/jsaga-job-run.bat -Executable /bin/hostname -r local://localhost
+cat job-resources.txt | while read url ; do echo $url ; ./bin/jsaga-job-run.bat -Executable /bin/hostname -r $url ; done
+
+./bin/jsaga-ls.bat -l file://c:/
+cat data-resources.txt | while read url ; do echo $url ; ./bin/jsaga-ls.bat -l $url ; done
+./bin/jsaga-cat.bat
