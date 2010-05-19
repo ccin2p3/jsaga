9bb4ee08182f8530dade2d21086701c649e98ae2
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2010-05-19 12:15:42 +0000

    refactored

--- maven-archetype-jsaga/resources/archetype-resources/test/src/integration/MyProtocolIntegrationTestSuite.java
+++ maven-archetype-jsaga/resources/archetype-resources/test/src/integration/MyProtocolIntegrationTestSuite.java
@@ -17,17 +17,14 @@ import org.ogf.saga.namespace.NSEntryTest;
  *
  */
 public class MyProtocolIntegrationTestSuite extends TestSuite {
+    /** create test suite */
+    public static Test suite() throws Exception {return new MyProtocolIntegrationTestSuite();}
+    /** index of test cases */
+    public static class index extends IndexTest {public index(){super(MyProtocolIntegrationTestSuite.class);}}
+
+    /** test cases */
     public static class MyProtocolNSEntryTest extends NSEntryTest {
         public MyProtocolNSEntryTest() throws Exception {super("myprotocol");}
         public void test_unexisting() { super.ignore("not yet implemented"); }
-    }
-
-    public MyProtocolIntegrationTestSuite() throws Exception {
-        super();
-        this.addTestSuite(MyProtocolNSEntryTest.class);
-    }
-
-    public static Test suite() throws Exception {
-        return new MyProtocolIntegrationTestSuite();
     }
 }
