7f9d52783415861ffc5ac87c21323cac2a4bbece
Sylvain Reynaud <sreynaud@in2p3.fr>
2010-10-18 18:32:52 +0200

    update

--- jsaga-installer/test/resources/demo.sh
+++ jsaga-installer/test/resources/demo.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
+cp ./doc/etc/jsaga-default-contexts.xml ./etc/
 ./bin/jsaga-help.bat -s default
-
-# copier jsaga-default-contexts.xml
 
 ./bin/jsaga-context-info.bat
 ./bin/jsaga-context-init.bat
 ./bin/jsaga-context-info.bat
 
 ./bin/jsaga-job-run.bat
-./bin/jsaga-job-run.bat -Executable /bin/hostname -r local://localhost
+./bin/jsaga-job-run.bat -Executable /bin/hostname -r fork://localhost
 cat doc/job-resources.txt | while read url ; do echo $url ; ./bin/jsaga-job-run.bat -Executable /bin/hostname -r "$url" ; done
 
 ./bin/jsaga-ls.bat -l file://c:/
