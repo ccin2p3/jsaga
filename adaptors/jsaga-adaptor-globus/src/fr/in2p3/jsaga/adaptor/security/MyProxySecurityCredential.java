c9ac85b5062be102e31d522ba26c26f4d99f0182
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2010-11-09 18:26:11 +0100

    Use info(m_proxy, proxyParameters) instead of info(m_proxy, m_userId, m_myProxyPass)

--- adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/security/MyProxySecurityCredential.java
+++ adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/security/MyProxySecurityCredential.java
@@ -2,6 +2,7 @@ package fr.in2p3.jsaga.adaptor.security;
 
 import fr.in2p3.jsaga.adaptor.security.impl.GSSCredentialSecurityCredential;
 import org.globus.myproxy.CredentialInfo;
+import org.globus.myproxy.InfoParams;
 import org.globus.myproxy.MyProxy;
 import org.globus.util.Util;
 import org.ietf.jgss.GSSCredential;
@@ -15,7 +16,7 @@ import java.io.PrintStream;
 * ***************************************************
 * File:   MyProxySecurityCredential
 * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
-* Date:   13 août 2007
+* Date:   13 aoï¿½t 2007
 * ***************************************************
 * Description:                                      */
 /**
@@ -38,29 +39,34 @@ public class MyProxySecurityCredential extends GSSCredentialSecurityCredential i
         MyProxy server = MyProxySecurityAdaptor.getServer(m_server);
 
         // server info
-        CredentialInfo info = server.info(m_proxy, m_userId, m_myProxyPass);
-        out.println("  Owner    : "+info.getOwner());
-        out.println("  StartTime: "+info.getStartTimeAsDate());
-        out.println("  EndTime  : "+info.getEndTimeAsDate());
-        long now = System.currentTimeMillis();
-        if (info.getEndTime() > now) {
-            out.println("  LifeTime : "+Util.formatTimeSec((info.getEndTime() - now)/1000));
-        } else {
-            out.println("  LifeTime : expired");
+        // --- CredentialInfo info = server.info(m_proxy, m_userId, m_myProxyPass);
+        InfoParams proxyParameters = new InfoParams();
+        CredentialInfo[] infos = server.info(m_proxy, proxyParameters);
+        out.println("Owner    : "+infos[0].getOwner());
+        for (int i=0;i<infos.length;i++) {
+        	CredentialInfo info = infos[i];
+            out.println ((info.getName() == null) ? "default:" : info.getName() +":");
+	        out.println("  StartTime: "+info.getStartTimeAsDate());
+	        out.println("  EndTime  : "+info.getEndTimeAsDate());
+	        long now = System.currentTimeMillis();
+	        if (info.getEndTime() > now) {
+	            out.println("  LifeTime : "+Util.formatTimeSec((info.getEndTime() - now)/1000));
+	        } else {
+	            out.println("  LifeTime : expired");
+	        }
+	        //if (info.getName() != null) {
+	        //    out.println("  Name     : "+info.getName());
+	        //}
+	        if (info.getRetrievers() != null) {
+	            out.println("  Retrievers : "+info.getRetrievers());
+	        }
+	        if (info.getRenewers() != null) {
+	            out.println("  Renewers : "+info.getRenewers());
+	        }
+	        if (info.getDescription() != null) {
+	            out.println("  Description : "+info.getDescription());
+	        }
         }
-        if (info.getName() != null) {
-            out.println("  Name     : "+info.getName());
-        }
-        if (info.getRetrievers() != null) {
-            out.println("  Retrievers : "+info.getRetrievers());
-        }
-        if (info.getRenewers() != null) {
-            out.println("  Renewers : "+info.getRenewers());
-        }
-        if (info.getDescription() != null) {
-            out.println("  Description : "+info.getDescription());
-        }
-
         // local info
         out.println("  Delegated LifeTime : "+format(m_proxy.getRemainingLifetime()));
     }
