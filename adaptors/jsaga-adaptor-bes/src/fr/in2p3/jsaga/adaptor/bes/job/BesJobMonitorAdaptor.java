c792bd7e43f5d252f9905b79076a67b57807975f
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2010-11-29 15:50:27 +0100

    implement ListableJobAdaptor

--- adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/BesJobMonitorAdaptor.java
+++ adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/BesJobMonitorAdaptor.java
@@ -2,20 +2,26 @@ package fr.in2p3.jsaga.adaptor.bes.job;
 
 import fr.in2p3.jsaga.adaptor.base.defaults.Default;
 import fr.in2p3.jsaga.adaptor.base.usage.Usage;
+import fr.in2p3.jsaga.adaptor.job.control.manage.ListableJobAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 import fr.in2p3.jsaga.adaptor.job.monitor.QueryIndividualJob;
 
+import org.ggf.schemas.bes.x2006.x08.besFactory.FactoryResourceAttributesDocumentType;
 import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusResponseType;
 import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusesResponseType;
 import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusesType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetFactoryAttributesDocumentResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetFactoryAttributesDocumentType;
 import org.ggf.schemas.bes.x2006.x08.besFactory.InvalidRequestMessageFaultType;
 import org.ogf.saga.error.IncorrectStateException;
 import org.ogf.saga.error.NoSuccessException;
+import org.ogf.saga.error.PermissionDeniedException;
 import org.ogf.saga.error.TimeoutException;
-import org.w3.x2005.x08.addressing.AttributedURIType;
 import org.w3.x2005.x08.addressing.EndpointReferenceType;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /* ***************************************************
@@ -27,7 +33,7 @@ import java.util.Map;
 * Date:   23 Nov. 2010
 * ***************************************************/
 
-public class BesJobMonitorAdaptor extends BesJobAdaptorAbstract implements QueryIndividualJob {
+public class BesJobMonitorAdaptor extends BesJobAdaptorAbstract implements QueryIndividualJob, ListableJobAdaptor {
         
     public Usage getUsage() {
     	return null;
@@ -39,10 +45,6 @@ public class BesJobMonitorAdaptor extends BesJobAdaptorAbstract implements Query
     
     public JobStatus getStatus(String nativeJobId) throws TimeoutException, NoSuccessException {
 		GetActivityStatusesType requestStatus = new GetActivityStatusesType();
-		//EndpointReferenceType epr[] = new EndpointReferenceType[1];
-        //EndpointReferenceType rep= new EndpointReferenceType();
-        //rep.setAddress(new AttributedURIType(nativeJobId));
-		//epr[0]= rep;
 		requestStatus.setActivityIdentifier(new BesJob(nativeJobId).getReferenceEndpoints());
 		GetActivityStatusesResponseType responseStatus;
 		try {
@@ -52,9 +54,25 @@ public class BesJobMonitorAdaptor extends BesJobAdaptorAbstract implements Query
 		} catch (RemoteException e) {
 			throw new NoSuccessException(e);
 		}
-		GetActivityStatusResponseType activityStatusArray = responseStatus.getResponse(0);
-		
-    	return new BesJobStatus(nativeJobId, activityStatusArray.getActivityStatus());
+		GetActivityStatusResponseType activityStatus = responseStatus.getResponse(0);
+    	return new BesJobStatus(nativeJobId, activityStatus.getActivityStatus());
+	}
+
+	public String[] list() throws PermissionDeniedException, TimeoutException,	NoSuccessException {
+		List<String> urls = new ArrayList<String>();
+		GetFactoryAttributesDocumentResponseType r;
+		try {
+			r = _bes_pt.getFactoryAttributesDocument(new GetFactoryAttributesDocumentType());
+		} catch (InvalidRequestMessageFaultType e) {
+			throw new NoSuccessException(e);
+		} catch (RemoteException e) {
+			throw new NoSuccessException(e);
+		}
+		FactoryResourceAttributesDocumentType attr = r.getFactoryResourceAttributesDocument();
+		for (EndpointReferenceType epr: attr.getActivityReference()) {
+			urls.add(epr.getAddress().get_value().toString());
+		}
+		return (String[])urls.toArray(new String[urls.size()]);
 	}
 
 }
