a67582acf20d78ed97fb759504f6987e56f4a2ad
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2010-11-29 11:22:26 +0100

    sub-state: "Staging-In" and "Staging-Out"

--- adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/BesJobStatus.java
+++ adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/BesJobStatus.java
@@ -1,5 +1,6 @@
 package fr.in2p3.jsaga.adaptor.bes.job;
 
+import org.apache.axis.message.MessageElement;
 import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStateEnumeration;
 import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStatusType;
 
@@ -10,7 +11,7 @@ import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
 * ***             http://cc.in2p3.fr/             ***
 * ***************************************************
-* File:   SHJobStatus
+* File:   BesJobStatus
 * Author: Lionel Schwarz (lionel.schwarz@in2p3.fr)
 * Date:   26 Nov 2010
 * ***************************************************/
@@ -27,10 +28,19 @@ public class BesJobStatus extends JobStatus {
 
     public SubState getSubState() {
     	ActivityStateEnumeration state = ((ActivityStatusType) m_nativeStateCode).getState();
+    	MessageElement any[] = ((ActivityStatusType) m_nativeStateCode).get_any();
+    	String substate = (any == null)?null:any[0].getName();
+    		
         if (ActivityStateEnumeration.Pending.equals(state)) {
             return SubState.RUNNING_SUBMITTED;
         } else if (ActivityStateEnumeration.Running.equals(state)) {
-            return SubState.RUNNING_ACTIVE;
+        	if ("Staging-In".equals(substate)) {
+        		return SubState.RUNNING_PRE_STAGING;
+        	} else if ("Staging-Out".equals(substate)) {
+        		return SubState.RUNNING_POST_STAGING;
+        	} else {
+        		return SubState.RUNNING_ACTIVE;
+        	}
         } else if (ActivityStateEnumeration.Finished.equals(state)) {
             return SubState.DONE;
         } else if (ActivityStateEnumeration.Cancelled.equals(state)) {
