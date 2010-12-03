1c03089cd4af2c2464a7f1897de27c52fd8d64d9
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2010-12-03 17:41:06 +0100

    New adaptor for a-rex

---
+++ adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/arex/BesArexJobControlAdaptor.java
@@ -0,0 +1,78 @@
+package fr.in2p3.jsaga.adaptor.bes.job.arex;
+
+import fr.in2p3.jsaga.adaptor.base.defaults.Default;
+import fr.in2p3.jsaga.adaptor.base.usage.*;
+import fr.in2p3.jsaga.adaptor.bes.job.BesJob;
+import fr.in2p3.jsaga.adaptor.bes.job.BesJobAdaptorAbstract;
+import fr.in2p3.jsaga.adaptor.job.BadResource;
+import fr.in2p3.jsaga.adaptor.job.control.JobControlAdaptor;
+import fr.in2p3.jsaga.adaptor.job.control.description.JobDescriptionTranslator;
+import fr.in2p3.jsaga.adaptor.job.control.description.JobDescriptionTranslatorJSDL;
+import fr.in2p3.jsaga.adaptor.job.monitor.JobMonitorAdaptor;
+
+import org.apache.axis.message.MessageElement;
+import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityDocumentType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.CreateActivityResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.CreateActivityType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.InvalidRequestMessageFaultType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.NotAcceptingNewActivitiesFaultType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.NotAuthorizedFaultType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.TerminateActivitiesResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.TerminateActivitiesType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.TerminateActivityResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.UnsupportedFeatureFaultType;
+import org.ggf.schemas.jsdl.x2005.x11.jsdl.JobDefinition_Type;
+import org.globus.wsrf.encoding.DeserializationException;
+import org.globus.wsrf.encoding.ObjectDeserializer;
+import org.globus.wsrf.encoding.ObjectSerializer;
+import org.globus.wsrf.encoding.SerializationException;
+import org.ogf.saga.error.*;
+
+import org.w3.x2005.x08.addressing.AttributedURIType;
+import org.w3.x2005.x08.addressing.EndpointReferenceType;
+import org.w3.x2005.x08.addressing.ReferenceParametersType;
+import org.xml.sax.InputSource;
+
+import java.io.*;
+import java.rmi.RemoteException;
+import java.util.*;
+
+import javax.xml.namespace.QName;
+
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   BesArexJobControlAdaptor
+* Author: Lionel Schwarz (lionel.schwarz@in2p3.fr)
+* Date:   23 Nov. 2010
+* ***************************************************/
+public class BesArexJobControlAdaptor extends BesJobAdaptorAbstract 
+		implements JobControlAdaptor {
+
+    public String getType() {
+        return "bes-arex";
+    }
+    
+	public int getDefaultPort() {
+		return 2010;
+	}
+
+	public Usage getUsage() {
+    	return null;
+    }
+	
+	protected Class getJobClass() {
+		return BesArexJob.class;
+	}
+
+	public Default[] getDefaults(Map attributes) throws IncorrectStateException {
+    	return new Default[]{};
+    }
+    
+    public JobMonitorAdaptor getDefaultJobMonitor() {
+        return new BesArexJobMonitorAdaptor();
+    }
+
+}
---
+++ adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/arex/BesArexJobMonitorAdaptor.java
@@ -0,0 +1,111 @@
+package fr.in2p3.jsaga.adaptor.bes.job.arex;
+
+import fr.in2p3.jsaga.adaptor.base.defaults.Default;
+import fr.in2p3.jsaga.adaptor.base.usage.Usage;
+import fr.in2p3.jsaga.adaptor.bes.job.BesJob;
+import fr.in2p3.jsaga.adaptor.bes.job.BesJobAdaptorAbstract;
+import fr.in2p3.jsaga.adaptor.bes.job.BesJobMonitorAdaptor;
+import fr.in2p3.jsaga.adaptor.bes.job.BesJobStatus;
+import fr.in2p3.jsaga.adaptor.job.control.manage.ListableJobAdaptor;
+import fr.in2p3.jsaga.adaptor.job.monitor.JobMonitorAdaptor;
+import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
+import fr.in2p3.jsaga.adaptor.job.monitor.QueryIndividualJob;
+
+import org.apache.axis.message.MessageElement;
+import org.apache.axis.message.Text;
+import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStatusType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.FactoryResourceAttributesDocumentType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusesResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusesType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetFactoryAttributesDocumentResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetFactoryAttributesDocumentType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.InvalidRequestMessageFaultType;
+import org.globus.wsrf.encoding.ObjectSerializer;
+import org.globus.wsrf.encoding.SerializationException;
+import org.ogf.saga.error.IncorrectStateException;
+import org.ogf.saga.error.NoSuccessException;
+import org.ogf.saga.error.PermissionDeniedException;
+import org.ogf.saga.error.TimeoutException;
+import org.w3.x2005.x08.addressing.EndpointReferenceType;
+
+import java.io.StringWriter;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   BesArexJobMonitorAdaptor
+* Author: Lionel Schwarz (lionel.schwarz@in2p3.fr)
+* Date:   23 Nov. 2010
+* ***************************************************/
+
+public class BesArexJobMonitorAdaptor extends BesJobMonitorAdaptor implements QueryIndividualJob, ListableJobAdaptor {
+        
+    public String getType() {
+        return "bes-arex";
+    }
+
+	public int getDefaultPort() {
+		return 2010;
+	}
+
+    public Usage getUsage() {
+    	return null;
+    }
+
+    public Default[] getDefaults(Map attributes) throws IncorrectStateException {
+    	return new Default[]{};
+    }
+    
+	protected Class getJobClass() {
+		return BesArexJob.class;
+	}
+
+	public JobMonitorAdaptor getDefaultJobMonitor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * 
+  <soapenv:Fault xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+   <soapenv:Code>
+    <soapenv:Value>soap-env:Sender</soapenv:Value>
+   </soapenv:Code>
+   <soapenv:Reason>
+    <soapenv:Text>Missing a-rex:JobID in ActivityIdentifier</soapenv:Text>
+   </soapenv:Reason>
+   <soapenv:Detail>
+    <ns1:UnknownActivityIdentifierFault>
+     <ns1:Message>Unrecognized EPR in ActivityIdentifier</ns1:Message>
+    </ns1:UnknownActivityIdentifierFault>
+   </soapenv:Detail>
+  </soapenv:Fault>
+	 */
+    protected ActivityStatusType getActivityStatus(GetActivityStatusesResponseType responseStatus) throws NoSuccessException{
+		MessageElement[] me_list = responseStatus.getResponse(0).get_any();
+		if (me_list != null) {
+			for (MessageElement me : responseStatus.getResponse(0).get_any()) {
+				if ("Fault".equals(me.getName())) {
+					String soap_ns = me.getNamespaceURI();
+					MessageElement soap_fault = me.getChildElement(new QName(soap_ns, "Reason"));
+					if (soap_fault != null) {
+						MessageElement me_text = soap_fault.getChildElement(new QName(soap_ns, "Text"));
+						if (me_text != null) {
+							Text text = (Text)me_text.getChildElements().next();
+							throw new NoSuccessException(text.getNodeValue());
+						}
+					}
+				}
+			}
+		}
+		return super.getActivityStatus(responseStatus);
+    }
+}
