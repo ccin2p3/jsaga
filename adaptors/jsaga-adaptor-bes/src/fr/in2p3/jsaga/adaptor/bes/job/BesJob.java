54361f4970a8bf93fd6590bab8877644b5b32ef3
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2010-12-03 17:49:40 +0100

    make it abstract

--- adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/BesJob.java
+++ adaptors/jsaga-adaptor-bes/src/fr/in2p3/jsaga/adaptor/bes/job/BesJob.java
@@ -1,6 +1,6 @@
 package fr.in2p3.jsaga.adaptor.bes.job;
 
-import org.w3.x2005.x08.addressing.AttributedURIType;
+import org.ogf.saga.error.NoSuccessException;
 import org.w3.x2005.x08.addressing.EndpointReferenceType;
 
 
@@ -13,18 +13,58 @@ import org.w3.x2005.x08.addressing.EndpointReferenceType;
 * Date:   26 Nov 2010
 * ***************************************************/
 
-public class BesJob {
+public abstract class BesJob {
 
-	protected EndpointReferenceType[] _job_endpoints;
+	protected EndpointReferenceType _job_endpoints = null;
+	protected String _job_nativeId = null;
 	
-	public BesJob(String nativeJobId) {
-		_job_endpoints = new EndpointReferenceType[1];
-        EndpointReferenceType rep= new EndpointReferenceType();
-        rep.setAddress(new AttributedURIType(nativeJobId));
-        _job_endpoints[0]= rep;
+	/**
+	 * convert the native job identifier (URL as String) into a BES activity identifier (EndpointReferenceType)
+	 * @return EndpointReferenceType the activityIdentifier
+	 */
+	protected abstract EndpointReferenceType toActivityIdentifier() throws NoSuccessException;
+	
+	/**
+	 * convert the BES activity identifier (EndpointReferenceType) into a native job identifier (URL as String)
+	 * @return String the native Job identifier
+	 */
+	protected abstract String toStringIdentifier() throws NoSuccessException;
+	
+	/**
+	 * Initialize the native job identifier
+	 * @param nativeJobId the native job identifier
+	 */
+	public void setNativeJobId(String nativeJobId) {
+		_job_nativeId = nativeJobId;
 	}
-
-	public EndpointReferenceType[] getReferenceEndpoints() {
-        return _job_endpoints;
-    }
+	
+	/**
+	 * Initialize the BES activity identifier (EndpointReferenceType)
+	 * @param epr the BES activity identifier
+	 */
+	public void setActivityIdentifier(EndpointReferenceType epr) {
+		_job_endpoints = epr;
+	}
+	
+	/**
+	 * returns the native job identifier
+	 * @return String the native job identifier
+	 */
+	public String getNativeJobID() throws NoSuccessException {
+		if (_job_nativeId != null) 
+			return _job_nativeId;
+		else
+			return toStringIdentifier();
+	}
+	
+	/**
+	 * returns the BES activity identifier (EndpointReferenceType)
+	 * @return EndpointReferenceType the BES activity identifier
+	 */
+	public EndpointReferenceType getActivityIdentifier() throws NoSuccessException {
+		if (_job_endpoints != null) 
+			return _job_endpoints;
+		else
+			return toActivityIdentifier();
+	}
 }
