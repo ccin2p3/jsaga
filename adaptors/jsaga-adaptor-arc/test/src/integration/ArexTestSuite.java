b7b1ba74f05db21579a3f1bc5a10e4017c080d0b
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-01-14 16:00:11 +0100

    add ArexJobRunSandboxTest

--- adaptors/jsaga-adaptor-arc/test/src/integration/ArexTestSuite.java
+++ adaptors/jsaga-adaptor-arc/test/src/integration/ArexTestSuite.java
@@ -55,13 +55,8 @@ public class ArexTestSuite extends JSAGATestSuite {
     }
 
  	// test cases
-    // TODO : implement HTTPS PUT
     public static class ArexJobRunSandboxTest extends JobRunSandboxTest {
         public ArexJobRunSandboxTest() throws Exception {super("arex");}
-        public void test_remote_input_explicit() { super.ignore("HTTPS PUT not supported"); }
-        public void test_remote_output_explicit() { super.ignore("HTTPS PUT not supported"); }
-        public void test_input_output_explicit() { super.ignore("HTTPS PUT not supported"); }
-        public void test_input_output_implicit() { super.ignore("HTTPS PUT not supported"); }
     }
     
     // test cases
