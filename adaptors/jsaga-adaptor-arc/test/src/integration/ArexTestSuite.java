fd1b3e5efb892b951390ef7bbf9fa2372a2b0106
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-01-28 09:19:20 +0100

    fix ArexJobRunDescriptionTest

--- adaptors/jsaga-adaptor-arc/test/src/integration/ArexTestSuite.java
+++ adaptors/jsaga-adaptor-arc/test/src/integration/ArexTestSuite.java
@@ -4,6 +4,7 @@ import junit.framework.Test;
 
 import org.ogf.saga.error.NoSuccessException;
 import org.ogf.saga.job.*;
+import org.ogf.saga.job.abstracts.Attribute;
 import org.ogf.saga.task.State;
 
 /* ***************************************************
@@ -54,15 +55,16 @@ public class ArexTestSuite extends JSAGATestSuite {
         public ArexJobRunDescriptionTest() throws Exception {super("arex");}
         public void test_run_inWorkingDirectory() { super.ignore("WorkingDirectory is not supported"); }
         
-        // FIXME: returned DONE instead of FAILED
-        public void test_run_cpuTimeRequirement() { super.ignore("walltimelimit is ignored and job is stuck"); }
-        /*
         public void test_run_cpuTimeRequirement() throws Exception {
-            
+        	super.ignore("TotalCPUTime is ignored");
+        }
+        
+        public void test_run_wallTimeRequirement() throws Exception {
+        	super.ignore("walltimelimit is ignored and job is stuck"); if (true) return;
         	// prepare a simple job
         	JobDescription desc = createSimpleJob();
         	// and inform the scheduler to the estimate time is 14 days
-        	desc.setAttribute(JobDescription.WALLTIMELIMIT, String.valueOf(3001));
+        	desc.setAttribute(JobDescription.WALLTIMELIMIT, String.valueOf(60*60*24*14));
         	
         	Job job = null;
         	try {
@@ -89,18 +91,35 @@ public class ArexTestSuite extends JSAGATestSuite {
             	}
             }
     	        
-        }*/
+        }
+        /**
+         * Runs simple job, requests 4096 Mo of memory and expects FAILED status
+         */
+        public void test_run_memoryRequirement() throws Exception {
+            
+        	// prepare a job witch requires 8GB of RAM and expect FAILED status
+        	Attribute[] attributes = new Attribute[1];
+        	attributes[0] = new Attribute(JobDescription.TOTALPHYSICALMEMORY, "8192");
+        	JobDescription desc =  createJob(SIMPLE_JOB_BINARY, attributes, null);
+        	
+        	// submit
+            Job job = runJob(desc);
+            
+            // wait for the end
+            job.waitFor();  
+            
+            // check job status
+            assertEquals(
+                    State.FAILED,
+                    job.getState());       
+        }
+            
     }
 
  	// test cases
     public static class ArexJobRunSandboxTest extends JobRunSandboxTest {
         public ArexJobRunSandboxTest() throws Exception {super("arex");}
     }
-    
-    // test cases
-    /*public static class ArexJobRunInteractiveTest extends JobRunInteractiveTest {
-        public ArexJobRunInteractiveTest() throws Exception {super("arex");}
-    }*/
     
     // test cases
     public static class ArexJobRunInfoTest extends JobRunInfoTest {
