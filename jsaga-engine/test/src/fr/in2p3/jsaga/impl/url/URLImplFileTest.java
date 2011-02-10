5cb86e99d0ee358576bd6ce855d7370149b1f411
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-02-10 16:28:49 +0100

    added test_redondantslashes()

--- jsaga-engine/test/src/fr/in2p3/jsaga/impl/url/URLImplFileTest.java
+++ jsaga-engine/test/src/fr/in2p3/jsaga/impl/url/URLImplFileTest.java
@@ -18,44 +18,24 @@ import org.ogf.saga.url.URLFactory;
  */
 public class URLImplFileTest extends URLImplTest {
     
-    protected void init() {
+	protected void init() {
     	super.init();
 	   	 _uri = "file:";
 		 _user = "";
 		 _host = "";
 		 _port = "";
+		 _file = "contains#init";
     	 _query = "";
     	 _fragment = "";
      }
 
-    /*
-    public void test_remove() throws Exception {
-        URL url;
-        
-        url = URLFactory.createURL(_url_simple);
-        url.setString();
-        assertEquals("", url.getString());
-
-        url = URLFactory.createURL(_url_simple);
-        url.setScheme();
-        assertEquals(_user+_host+_port+_abs_path+_query+_fragment, url.getString());
-
+    public void test_redondantslashes()  throws Exception {
+        URL url = URLFactory.createURL("//path/subdir");
+        // path should not be considered as HOST
+        assertTrue(url.getHost() == null);
     }
-    public void test_replace() throws Exception {
-        URL url;
 
-        url = URLFactory.createURL(_url_simple);
-        url.setString("NEW:///");
-        assertEquals("NEW:///", url.getString());
-
-        url = URLFactory.createURL(_url_simple);
-        url.setScheme("NEW");
-        assertEquals("NEW://" + _user+_host+_port+_abs_path+_query+_fragment, url.getString());
-
-        url = URLFactory.createURL(_url_simple);
-        url.setPath("/f:/NEW");
-        assertEquals(_uri+_user+_host+_port+"/f:/NEW"+_query+_fragment, url.getString());
-
-    }
-*/
+    /*public void test_specialchars() throws Exception {
+    	URL url = URLFactory.createURL();
+    }*/
 }
