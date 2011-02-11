d8a2f9998badc8df18dad213c6c33240fafa547a
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-02-11 17:04:31 +0100

    added test_relative and test_replace

--- jsaga-engine/test/src/fr/in2p3/jsaga/impl/url/URLImplWinTest.java
+++ jsaga-engine/test/src/fr/in2p3/jsaga/impl/url/URLImplWinTest.java
@@ -1,5 +1,6 @@
 package fr.in2p3.jsaga.impl.url;
 
+import org.ogf.saga.error.BadParameterException;
 import org.ogf.saga.url.URL;
 import org.ogf.saga.url.URLFactory;
 
@@ -21,13 +22,58 @@ public class URLImplWinTest extends URLImplFileTest {
     protected void init() {
     	super.init();
     	 _abs_path = "/c:/path/";
-    	 _path_with_space = "/c:/path with space";
+    	 _path_not_encoded = "/c:/path with#and?and{}and%and\"and[]";
+    	 _path_encoded = "/c:/path%20with%23and%3Fand%7B%7Dand%25and%22and%5B%5D";
     }
 
     public void test_antislash()  throws Exception {
     	
+    	/*URL zz = URLFactory.createURL("file:/c:/dir/file");
+    	System.out.println(zz.getScheme());
+    	System.out.println(zz.getHost());
+    	System.out.println(zz.getPath());
+    	zz.setString("file:/d:/newdir/newfile");
+    	System.out.println(zz.getScheme());
+    	System.out.println(zz.getHost());
+    	System.out.println(zz.getPath());
+    	zz.setPath("e:/newdir2/newfile2");
+    	System.out.println(zz.getScheme());
+    	System.out.println(zz.getHost());
+    	System.out.println(zz.getPath());*/
+    	
+    	System.out.println(new java.io.File("/tmp/lsz").toURI().toString());
+    	
+    	
+    	// Simulate Windows
+    	System.setProperty("file.separator", "\\");
         URL url = URLFactory.createURL("c:\\path");
         // backslash should be changed in slash
         assertEquals("c:/path", url.getString());
     }
+    
+    public void test_replace() throws Exception {
+    	super.test_replace();
+        URL url = URLFactory.createURL(_url_simple);
+        String newWinFile = "e:/data/file.txt";
+        url.setPath(newWinFile);
+        // a / should be added
+        assertEquals(_uri+_user+_host+_port+"/"+newWinFile+_query+_fragment, url.getString());
+    }
+    
+    public void test_relative() throws Exception {
+    	super.test_relative();
+    	URL url;
+    	
+    	url = URLFactory.createURL(_url_simple);
+    	try {
+    		url.setString("c:/data/file.txt");
+    		fail("BadParameterException was expected");
+    	} catch (BadParameterException bpe) {
+    	} catch (Exception e) {
+    		fail("NoSuccessException was expected");
+    	}
+    	
+ 
+    }
+    
 }
