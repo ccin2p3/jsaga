a479a36782bc646dca874315f8c618eb973fe61b
Sylvain Reynaud <sreynaud@in2p3.fr>
2011-02-17 15:57:08 +0100

    use log4j instead of java.util.logging

--- adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/data/GsiftpDataAdaptorAbstract.java
+++ adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/data/GsiftpDataAdaptorAbstract.java
@@ -9,6 +9,8 @@ import fr.in2p3.jsaga.adaptor.data.read.FileReaderStreamFactory;
 import fr.in2p3.jsaga.adaptor.data.write.FileWriterStreamFactory;
 import fr.in2p3.jsaga.adaptor.security.SecurityCredential;
 import fr.in2p3.jsaga.adaptor.security.impl.GSSCredentialSecurityCredential;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.globus.common.ChainedIOException;
 import org.globus.ftp.*;
 import org.globus.ftp.exception.*;
@@ -19,8 +21,6 @@ import org.ogf.saga.error.*;
 
 import java.io.*;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -148,7 +148,7 @@ public abstract class GsiftpDataAdaptorAbstract implements DataCopy, DataRename,
         } catch (Exception e) {
             try{tmpConnection.close();} catch (Exception e2) {
                 /*Ignore it to throw the primary execption*/
-                Logger.getLogger(GsiftpDataAdaptorAbstract.class.getName()).log(Level.FINE, "Exception ignored when closing connection.", e2);
+                Logger.getLogger(GsiftpDataAdaptorAbstract.class.getName()).log(Level.DEBUG, "Exception ignored when closing connection.", e2);
             }
             try {
                 throw rethrowExceptionFull(e);
