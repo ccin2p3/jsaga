f32171e72463a9c64b47ddbac54fc4eb09abea83
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-02-21 14:06:59 +0100

    rollback: call URLHelper.toDirectoryURL()

--- jsaga-engine/src/fr/in2p3/jsaga/impl/namespace/AbstractSyncNSDirectoryImpl.java
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/namespace/AbstractSyncNSDirectoryImpl.java
@@ -35,13 +35,13 @@ import java.util.regex.Pattern;
 public abstract class AbstractSyncNSDirectoryImpl extends AbstractNSEntryDirImpl implements SyncNSDirectory {
     /** constructor for factory */
     protected AbstractSyncNSDirectoryImpl(Session session, URL url, DataAdaptor adaptor, int flags) throws NotImplementedException, IncorrectURLException, AuthenticationFailedException, AuthorizationFailedException, PermissionDeniedException, BadParameterException, AlreadyExistsException, DoesNotExistException, TimeoutException, NoSuccessException {
-        super(session, url, adaptor, flags);
+        super(session, URLHelper.toDirectoryURL(url), adaptor, flags);
         this.init(flags);
     }
 
     /** constructor for NSDirectory.open() */
     protected AbstractSyncNSDirectoryImpl(AbstractNSDirectoryImpl dir, URL relativeUrl, int flags) throws NotImplementedException, IncorrectURLException, AuthenticationFailedException, AuthorizationFailedException, PermissionDeniedException, BadParameterException, AlreadyExistsException, DoesNotExistException, TimeoutException, NoSuccessException {
-        super(dir, relativeUrl, flags);
+        super(dir, URLHelper.toDirectoryURL(relativeUrl), flags);
         this.init(flags);
     }
 
