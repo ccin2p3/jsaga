b5fa664e902c9682ba2131107447e3a28db2ab99
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-03-07 17:35:04 +0100

    implement LinkAdapator

--- adaptors/jsaga-adaptor-linux/src/fr/in2p3/jsaga/adaptor/data/LinuxDataAdaptor.java
+++ adaptors/jsaga-adaptor-linux/src/fr/in2p3/jsaga/adaptor/data/LinuxDataAdaptor.java
@@ -3,11 +3,14 @@ package fr.in2p3.jsaga.adaptor.data;
 import fr.in2p3.commons.filesystem.FileStat;
 import fr.in2p3.commons.filesystem.FileSystem;
 import fr.in2p3.jsaga.adaptor.data.file.FileDataAdaptor;
+import fr.in2p3.jsaga.adaptor.data.link.LinkAdaptor;
+import fr.in2p3.jsaga.adaptor.data.link.NotLink;
 import fr.in2p3.jsaga.adaptor.data.read.FileAttributes;
 import org.ogf.saga.error.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /* ***************************************************
  * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -21,7 +24,7 @@ import java.io.FileNotFoundException;
 /**
  *
  */
-public class LinuxDataAdaptor extends FileDataAdaptor {
+public class LinuxDataAdaptor extends FileDataAdaptor implements LinkAdaptor{
     public String getType() {
         return "linux";
     }
@@ -51,4 +54,44 @@ public class LinuxDataAdaptor extends FileDataAdaptor {
         }
         return ret;
     }
+
+	public boolean isLink(String absolutePath)
+			throws PermissionDeniedException, DoesNotExistException,
+			TimeoutException, NoSuccessException {
+        return ((LinuxFileAttributes)getAttributes(absolutePath, null)).isLink();
+	}
+
+	public String readLink(String absolutePath) throws NotLink,
+			PermissionDeniedException, DoesNotExistException, TimeoutException,
+			NoSuccessException {
+		return ((LinuxFileAttributes)getAttributes(absolutePath,null)).readLink();
+	}
+
+	public void link(String sourceAbsolutePath, String linkAbsolutePath,
+			boolean overwrite) throws PermissionDeniedException,
+			DoesNotExistException, AlreadyExistsException, TimeoutException,
+			NoSuccessException {
+		// TODO link
+        File sourceEntry = super.newEntry(sourceAbsolutePath);
+		if (!sourceEntry.exists()) { throw new DoesNotExistException("File does not exist: " + sourceAbsolutePath);}
+		try {
+	        File linkEntry = super.newEntry(linkAbsolutePath);
+			if (!overwrite) { throw new AlreadyExistsException("File already exists: " + linkAbsolutePath);}
+			linkEntry.delete();
+		} catch (DoesNotExistException e) {
+			// Nothing to do
+		}
+		try {
+			new FileSystem().symlink(sourceEntry, linkAbsolutePath);
+		} catch (FileNotFoundException fnfe) {
+			throw new DoesNotExistException(fnfe);
+		} catch (fr.in2p3.commons.filesystem.FileAlreadyExistsException faee) {
+			throw new AlreadyExistsException("File already exists: " + linkAbsolutePath);
+		} catch (fr.in2p3.commons.filesystem.PermissionDeniedException pde) {
+			throw new PermissionDeniedException(pde);
+		} catch (IOException ioe) {
+			throw new NoSuccessException(ioe);
+		}
+	}
+	
 }
