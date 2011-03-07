0787d260e09dda0a8981b84e23023094113c14d1
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-03-07 17:29:07 +0100

    implement symbolic links

--- externals/file-system/src/file-system.cpp
+++ externals/file-system/src/file-system.cpp
@@ -2,6 +2,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <errno.h>
+#include <malloc.h>
 
 //WARNING: not compatible with -mno-cygwin
 #ifndef WIN32
@@ -15,6 +17,11 @@ using namespace std;
 #define DEFAULT_MODE 32768
 #define TRUE  1
 #define FALSE 0
+#define SYMLINK_ERRNO_PERMISSIONDENIED -1
+#define SYMLINK_ERRNO_DOESNOTEXIST -2
+#define SYMLINK_ERRNO_ALREADYEXISTS -3
+#define SYMLINK_ERRNO_INTERNALERROR -4
+
 
 JNIEXPORT jboolean JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_stat
   (JNIEnv *env, jobject, jstring jPath, jobject obj)
@@ -22,7 +29,6 @@ JNIEXPORT jboolean JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_stat
     const char *cPath = env->GetStringUTFChars(jPath, 0);
     struct stat buf;
     int ret = lstat(cPath, &buf);
-    env->ReleaseStringUTFChars(jPath, cPath);
 
     //file does not exist
     if (buf.st_mode==0 || buf.st_mode==7) {
@@ -51,6 +57,24 @@ JNIEXPORT jboolean JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_stat
     env->SetIntField(obj, env->GetFieldID(cls, "user_perms", "I"), user_perms);
     env->SetIntField(obj, env->GetFieldID(cls, "group_perms", "I"), group_perms);
     env->SetIntField(obj, env->GetFieldID(cls, "other_perms", "I"), other_perms);
+    if (islink == TRUE) {
+       int size = 100;
+       while (1) {
+           char *buffer = (char *) malloc (size);
+           int nchars = readlink (cPath, buffer, size);
+           if (nchars < 0) {
+               break;
+           }
+           if (nchars < size) {
+               buffer[nchars] = '\0';
+               env->SetObjectField(obj, env->GetFieldID(cls, "target", "Ljava/lang/String;"), env->NewStringUTF(buffer));
+               free (buffer);
+               break; /*return buffer;*/
+           }
+           free (buffer);
+           size *= 2;
+        }
+    }
 //WARNING: not compatible with -mno-cygwin
 #ifndef WIN32
     struct passwd *pws = getpwuid(buf.st_uid);
@@ -63,6 +87,8 @@ JNIEXPORT jboolean JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_stat
     env->SetLongField(obj, env->GetFieldID(cls, "atime", "J"), buf.st_atime);
     env->SetLongField(obj, env->GetFieldID(cls, "mtime", "J"), buf.st_mtime);
     env->SetLongField(obj, env->GetFieldID(cls, "ctime", "J"), buf.st_ctime);
+
+    env->ReleaseStringUTFChars(jPath, cPath);
     return TRUE;
 }
 
@@ -75,6 +101,35 @@ JNIEXPORT jboolean JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_chmod
     env->ReleaseStringUTFChars(jPath, cPath);
     return TRUE;
 }
+
+JNIEXPORT jint JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_symlink
+  (JNIEnv *env, jobject, jstring jOldPath, jstring jNewPath)
+{
+    const char *cOldPath = env->GetStringUTFChars(jOldPath, 0);
+    const char *cNewPath = env->GetStringUTFChars(jNewPath, 0);
+    int ret = symlink(cOldPath, cNewPath);
+    env->ReleaseStringUTFChars(jOldPath, cOldPath);
+    env->ReleaseStringUTFChars(jNewPath, cNewPath);
+    if (ret == 0) { return 0; }
+    switch (errno) {
+       case EACCES:
+       case EFAULT:
+       case EROFS:
+       case EPERM:
+         return SYMLINK_ERRNO_PERMISSIONDENIED;
+         break;
+       case ENOENT:
+         return SYMLINK_ERRNO_DOESNOTEXIST;
+         break;
+       case EEXIST:
+         return SYMLINK_ERRNO_ALREADYEXISTS;
+         break;
+       default:
+         return SYMLINK_ERRNO_INTERNALERROR;
+         break;
+    }
+}
+
 
 JNIEXPORT void JNICALL Java_fr_in2p3_commons_filesystem_FileSystem_intArray
   (JNIEnv *env, jobject, jintArray arr)
