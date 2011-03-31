c665f3b80b2540cfa23d1d75b70ce72a9305f932
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-03-31 16:55:29 +0200

    add header

--- jsaga-engine/src/fr/in2p3/jsaga/impl/context/ConfigurableContextFactory.java
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/context/ConfigurableContextFactory.java
@@ -10,6 +10,16 @@ import fr.in2p3.jsaga.engine.config.ConfigurationException;
 import fr.in2p3.jsaga.engine.session.SessionConfiguration;
 import fr.in2p3.jsaga.generated.session.Attribute;
 
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   ConfigurableContextFactory
+* Author: Lionel Schwarz (lionel.schwarz@in2p3.fr)
+* Date:   31/03/2011
+* ***************************************************
+* Description:                                      */
+
 public class ConfigurableContextFactory {
 	
     public static ConfiguredContext[] listConfiguredContext() throws ConfigurationException {
--- jsaga-engine/src/fr/in2p3/jsaga/impl/context/ConfiguredContext.java
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/context/ConfiguredContext.java
@@ -1,5 +1,15 @@
 package fr.in2p3.jsaga.impl.context;
 
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   ConfiguredContext
+* Author: Lionel Schwarz (lionel.schwarz@in2p3.fr)
+* Date:   31/03/2011
+* ***************************************************
+* Description:                                      */
+
 public class ConfiguredContext {
 	private String m_urlPrefix;
 	private String m_type;
