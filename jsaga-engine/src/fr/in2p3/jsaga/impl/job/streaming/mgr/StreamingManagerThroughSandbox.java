c1fca984fac2226f6ffe92be2b434d491801e415
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-04-11 10:46:14 +0200

    new interface StreamingManagerThroughSandbox

---
+++ jsaga-engine/src/fr/in2p3/jsaga/impl/job/streaming/mgr/StreamingManagerThroughSandbox.java
@@ -0,0 +1,15 @@
+package fr.in2p3.jsaga.impl.job.streaming.mgr;
+
+/* ***************************************************
+* *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+* ***             http://cc.in2p3.fr/             ***
+* ***************************************************
+* File:   StreamingManagerThroughSandbox
+* Author: Lionel Schwarz (lionel.schwarz@in2p3.fr)
+* Date:   11 avril 2011
+* ***************************************************
+* Description:                                      */
+
+public interface StreamingManagerThroughSandbox {
+
+}
