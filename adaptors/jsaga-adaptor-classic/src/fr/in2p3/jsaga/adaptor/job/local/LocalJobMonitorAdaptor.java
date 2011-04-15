c09263fb46b7780ea6163ed7f59382e9d796f6d6
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-04-15 09:07:58 +0200

    remove sessionMap and implement ListableJobAdaptor

--- adaptors/jsaga-adaptor-classic/src/fr/in2p3/jsaga/adaptor/job/local/LocalJobMonitorAdaptor.java
+++ adaptors/jsaga-adaptor-classic/src/fr/in2p3/jsaga/adaptor/job/local/LocalJobMonitorAdaptor.java
@@ -3,12 +3,21 @@ package fr.in2p3.jsaga.adaptor.job.local;
 
 import fr.in2p3.jsaga.adaptor.base.defaults.Default;
 import fr.in2p3.jsaga.adaptor.base.usage.Usage;
+import fr.in2p3.jsaga.adaptor.job.control.manage.ListableJobAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 import fr.in2p3.jsaga.adaptor.job.monitor.QueryIndividualJob;
 import org.ogf.saga.error.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.Exception;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -19,7 +28,7 @@ import java.util.Map;
 * Date:   29 avril 2008
 * ***************************************************/
 
-public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements QueryIndividualJob {
+public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements QueryIndividualJob, ListableJobAdaptor {
 
 	public Usage getUsage() {
     	return null;
@@ -30,17 +39,43 @@ public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements Quer
     }
 
     public JobStatus getStatus(String nativeJobId) throws TimeoutException, NoSuccessException {
-
-    	try {    		
-    		Process p = (Process) LocalAdaptorAbstract.sessionMap.get(nativeJobId);    		
-    		return new LocalJobStatus(nativeJobId, p.exitValue());						
-    	} catch (Exception e) {
-    		if (e instanceof IllegalThreadStateException)
-    			// return "process has not exited" message
-    			return new LocalJobStatus(nativeJobId, -1);
-    		else
-    			throw new NoSuccessException(e);
+    	// TODO: use LocalJobProcess.getStatus()
+    	File f = new File(getEndcodeFile(nativeJobId));
+    	FileInputStream fis;
+		try {
+			fis = new FileInputStream(f);
+		} catch (FileNotFoundException e) {
+			// PROCESS Is not finished
+			return new LocalJobStatus(nativeJobId, -1);
 		}
-    }        
+    	byte[] buf = new byte[(int)f.length()];
+    	try {
+			int len = fis.read(buf);
+        	fis.close();
+		} catch (IOException e) {
+			throw new NoSuccessException(e);
+		}
+		// Get return code reading xxx.endcode file minus last character (carriage return)
+		return new LocalJobStatus(nativeJobId, Integer.valueOf(new String(buf).substring(0, buf.length-1)));						
+    }
+
+	public String[] list() throws PermissionDeniedException, TimeoutException,	NoSuccessException {
+		// Find all local files named *.pid: running jobs
+		// Find all local files named *.endcode: finished jobs
+		// TODO: search Process Files *.process
+		String filter = "(\\w*).(pid|endcode)";
+		Pattern p = Pattern.compile(filter);
+		String[] files = new File(getRootDir()).list();
+		List<String> listeFichiers = new ArrayList<String>();
+		for (int i=0; i<files.length;i++) {
+			Matcher m = p.matcher(files[i]);
+			if ( m.matches()) {
+				listeFichiers.add(files[i].substring(0, files[i].lastIndexOf('.')));
+				//files[i] = files[i].substring(0, files[i].lastIndexOf('.'));
+			}
+		}
+		return (String[]) listeFichiers.toArray(new String[]{});
+		//return files;
+	}
 
 }
