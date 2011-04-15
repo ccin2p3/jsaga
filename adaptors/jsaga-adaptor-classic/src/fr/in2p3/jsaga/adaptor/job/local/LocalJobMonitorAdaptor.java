a23afefa18996f8076bfd014a3e17037942c64a4
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-04-15 14:59:50 +0200

    use LocalJobProcess and implement JobInfoAdaptor interface

--- adaptors/jsaga-adaptor-classic/src/fr/in2p3/jsaga/adaptor/job/local/LocalJobMonitorAdaptor.java
+++ adaptors/jsaga-adaptor-classic/src/fr/in2p3/jsaga/adaptor/job/local/LocalJobMonitorAdaptor.java
@@ -4,6 +4,7 @@ package fr.in2p3.jsaga.adaptor.job.local;
 import fr.in2p3.jsaga.adaptor.base.defaults.Default;
 import fr.in2p3.jsaga.adaptor.base.usage.Usage;
 import fr.in2p3.jsaga.adaptor.job.control.manage.ListableJobAdaptor;
+import fr.in2p3.jsaga.adaptor.job.monitor.JobInfoAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 import fr.in2p3.jsaga.adaptor.job.monitor.QueryIndividualJob;
 import org.ogf.saga.error.*;
@@ -14,6 +15,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.Exception;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -28,7 +30,7 @@ import java.util.regex.Pattern;
 * Date:   29 avril 2008
 * ***************************************************/
 
-public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements QueryIndividualJob, ListableJobAdaptor {
+public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements QueryIndividualJob, ListableJobAdaptor, JobInfoAdaptor {
 
 	public Usage getUsage() {
     	return null;
@@ -40,6 +42,15 @@ public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements Quer
 
     public JobStatus getStatus(String nativeJobId) throws TimeoutException, NoSuccessException {
     	// TODO: use LocalJobProcess.getStatus()
+    	try {
+			LocalJobProcess ljp = LocalAdaptorAbstract.restore(nativeJobId);
+			return ljp.getJobStatus();
+		} catch (IOException e) {
+			throw new NoSuccessException(e);
+		} catch (ClassNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
+    	/*
     	File f = new File(getEndcodeFile(nativeJobId));
     	FileInputStream fis;
 		try {
@@ -56,16 +67,17 @@ public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements Quer
 			throw new NoSuccessException(e);
 		}
 		// Get return code reading xxx.endcode file minus last character (carriage return)
-		return new LocalJobStatus(nativeJobId, Integer.valueOf(new String(buf).substring(0, buf.length-1)));						
+		return new LocalJobStatus(nativeJobId, Integer.valueOf(new String(buf).substring(0, buf.length-1)));	
+		*/					
     }
 
 	public String[] list() throws PermissionDeniedException, TimeoutException,	NoSuccessException {
 		// Find all local files named *.pid: running jobs
 		// Find all local files named *.endcode: finished jobs
 		// TODO: search Process Files *.process
-		String filter = "(\\w*).(pid|endcode)";
+		String filter = "(\\w*).process";
 		Pattern p = Pattern.compile(filter);
-		String[] files = new File(getRootDir()).list();
+		String[] files = new File(LocalJobProcess.getRootDir()).list();
 		List<String> listeFichiers = new ArrayList<String>();
 		for (int i=0; i<files.length;i++) {
 			Matcher m = p.matcher(files[i]);
@@ -76,6 +88,59 @@ public class LocalJobMonitorAdaptor extends LocalAdaptorAbstract implements Quer
 		}
 		return (String[]) listeFichiers.toArray(new String[]{});
 		//return files;
+	}
+
+	public Integer getExitCode(String nativeJobId)
+			throws NotImplementedException, NoSuccessException {
+    	try {
+			LocalJobProcess ljp = LocalAdaptorAbstract.restore(nativeJobId);
+			return ljp.getReturnCode();
+		} catch (IOException e) {
+			throw new NoSuccessException(e);
+		} catch (ClassNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
+	}
+
+	public Date getCreated(String nativeJobId) throws NotImplementedException,
+			NoSuccessException {
+    	try {
+			LocalJobProcess ljp = LocalAdaptorAbstract.restore(nativeJobId);
+			return ljp.getCreated();
+		} catch (IOException e) {
+			throw new NoSuccessException(e);
+		} catch (ClassNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
+	}
+
+	public Date getStarted(String nativeJobId) throws NotImplementedException,
+			NoSuccessException {
+    	try {
+			LocalJobProcess ljp = LocalAdaptorAbstract.restore(nativeJobId);
+			return ljp.getStarted();
+		} catch (IOException e) {
+			throw new NoSuccessException(e);
+		} catch (ClassNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
+	}
+
+	public Date getFinished(String nativeJobId) throws NotImplementedException,
+			NoSuccessException {
+    	try {
+			LocalJobProcess ljp = LocalAdaptorAbstract.restore(nativeJobId);
+			return ljp.getFinished();
+		} catch (IOException e) {
+			throw new NoSuccessException(e);
+		} catch (ClassNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
+	}
+
+	public String[] getExecutionHosts(String nativeJobId)
+			throws NotImplementedException, NoSuccessException {
+		return new String[] {"localhost"};
 	}
 
 }
