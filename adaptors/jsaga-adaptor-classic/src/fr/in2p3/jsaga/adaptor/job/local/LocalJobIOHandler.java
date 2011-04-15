9e11bae0e9a1cd90936369a49ddd8e5410fd6676
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-04-15 09:07:06 +0200

    remove Process

--- adaptors/jsaga-adaptor-classic/src/fr/in2p3/jsaga/adaptor/job/local/LocalJobIOHandler.java
+++ adaptors/jsaga-adaptor-classic/src/fr/in2p3/jsaga/adaptor/job/local/LocalJobIOHandler.java
@@ -6,6 +6,10 @@ import org.ogf.saga.error.NoSuccessException;
 import org.ogf.saga.error.PermissionDeniedException;
 import org.ogf.saga.error.TimeoutException;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -20,11 +24,11 @@ import java.io.OutputStream;
 * ***************************************************/
 public class LocalJobIOHandler implements JobIOGetterInteractive {
 
-	private Process p;
+	//private Process p;
 	private String jobId;
 
-	public LocalJobIOHandler(Process p, String jobId) {
-		this.p = p;
+	public LocalJobIOHandler(String jobId) {
+		//this.p = p;
 		this.jobId = jobId;
 	}
 
@@ -33,14 +37,29 @@ public class LocalJobIOHandler implements JobIOGetterInteractive {
 	}
 
 	public InputStream getStderr() throws PermissionDeniedException, TimeoutException, NoSuccessException {
-		return p.getErrorStream();
+		//return p.getErrorStream();
+		try {
+			return new FileInputStream(new File(/*LocalAdaptorAbstract.getStderrFile(jobId)*/""));
+		} catch (FileNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
 	}
 
 	public OutputStream getStdin() throws PermissionDeniedException, TimeoutException, NoSuccessException {
-		return p.getOutputStream();
+		//return p.getOutputStream();
+		try {
+			return new FileOutputStream(new File(/*LocalAdaptorAbstract.getStdinFile(jobId)*/""));
+		} catch (FileNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
 	}
 
 	public InputStream getStdout() throws PermissionDeniedException, TimeoutException, NoSuccessException {
-		return p.getInputStream();
+		//return p.getInputStream();
+		try {
+			return new FileInputStream(new File(/*LocalAdaptorAbstract.getStdoutFile(jobId)*/""));
+		} catch (FileNotFoundException e) {
+			throw new NoSuccessException(e);
+		}
 	}
 }
