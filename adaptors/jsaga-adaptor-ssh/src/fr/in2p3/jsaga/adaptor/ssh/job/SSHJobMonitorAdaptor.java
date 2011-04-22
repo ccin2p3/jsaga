c8c34bbe6ebe5f07a9fb6ffa2287950649c69644
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-04-22 18:30:49 +0200

    implement listableJobAdaptor

--- adaptors/jsaga-adaptor-ssh/src/fr/in2p3/jsaga/adaptor/ssh/job/SSHJobMonitorAdaptor.java
+++ adaptors/jsaga-adaptor-ssh/src/fr/in2p3/jsaga/adaptor/ssh/job/SSHJobMonitorAdaptor.java
@@ -2,7 +2,9 @@ package fr.in2p3.jsaga.adaptor.ssh.job;
 
 import java.io.InputStream;
 import java.util.Map;
+import java.util.Vector;
 
+import fr.in2p3.jsaga.adaptor.job.control.manage.ListableJobAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 import fr.in2p3.jsaga.adaptor.job.monitor.QueryIndividualJob;
 import fr.in2p3.jsaga.adaptor.ssh.SSHAdaptorAbstract;
@@ -12,10 +14,12 @@ import org.ogf.saga.error.AuthorizationFailedException;
 import org.ogf.saga.error.BadParameterException;
 import org.ogf.saga.error.NoSuccessException;
 import org.ogf.saga.error.NotImplementedException;
+import org.ogf.saga.error.PermissionDeniedException;
 import org.ogf.saga.error.TimeoutException;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.ChannelSftp.LsEntry;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.SftpException;
 
@@ -30,7 +34,7 @@ import com.jcraft.jsch.SftpException;
 * ***************************************************/
 // TODO: implement ListableJobAdaptor
 
-public class SSHJobMonitorAdaptor extends SSHAdaptorAbstract implements QueryIndividualJob {
+public class SSHJobMonitorAdaptor extends SSHAdaptorAbstract implements QueryIndividualJob, ListableJobAdaptor {
 
 	private ChannelSftp channelGet = null;
 	
@@ -56,7 +60,7 @@ public class SSHJobMonitorAdaptor extends SSHAdaptorAbstract implements QueryInd
 	    	byte[] buf = new byte[4];
 	    	int len = is.read(buf);
 	    	is.close();
-    		return new SSHJobStatus(nativeJobId, new Integer(new String(buf).substring(0, len-1)).intValue());
+    		return new SSHJobStatus(nativeJobId, new Integer(new String(buf).trim()));
 					
     	} catch (SftpException sftpe) {
 			// if endcode does not exist, SSHJobStatus(nativeJobId, true, null);
@@ -68,6 +72,22 @@ public class SSHJobMonitorAdaptor extends SSHAdaptorAbstract implements QueryInd
 
     public void disconnect() throws NoSuccessException {
 		channelGet.disconnect();
+		channelGet = null;
 		super.disconnect();
     }
+
+	public String[] list() throws PermissionDeniedException, TimeoutException,
+			NoSuccessException {
+		try {
+			Vector v = channelGet.ls(SSHJobProcess.getRootDir() + "/*.process");
+			String[] listOfJobs = new String[v.size()];
+			for (int i=0; i<v.size(); i++) {
+				String file = ((LsEntry)v.elementAt(i)).getFilename();
+				listOfJobs[i] = file.substring(0, file.length()-8);
+			}
+			return listOfJobs;
+		} catch (SftpException e) {
+    		throw new NoSuccessException(e);
+		}
+	}
 }
