e119b81b512ba3ab32d5beda2aee7d879b1dbe74
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-05-06 14:59:06 +0200

    manage CANCELED state

--- adaptors/jsaga-adaptor-batchssh/src/fr/in2p3/jsaga/adaptor/batchssh/job/BatchSSHJobStatus.java
+++ adaptors/jsaga-adaptor-batchssh/src/fr/in2p3/jsaga/adaptor/batchssh/job/BatchSSHJobStatus.java
@@ -11,16 +11,39 @@ import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
 
 public class BatchSSHJobStatus extends JobStatus {
    
+	private int m_retCode;
+	
     public BatchSSHJobStatus(String jobId, SubState status) {
         super(jobId,status, status.toString());
     }
 
+    public BatchSSHJobStatus(String jobId, String state, int returnCode) {
+    	super(jobId, state, state);
+    	m_retCode = returnCode;
+    }
+    
+    public BatchSSHJobStatus(String jobId, String state) {
+    	super(jobId, state, state);
+    }
+    
     public String getModel() {
-        return "pbs+ssh";
+        return "pbs-ssh";
     }
 
     public SubState getSubState() {
-            return (SubState) m_nativeStateCode;
+    	if (m_nativeStateCode.equals("C") || m_nativeStateCode.equals("E")) {
+    		if (m_retCode == 0) return SubState.DONE;
+    		else if (m_retCode == 271) return SubState.CANCELED;
+    		else return SubState.FAILED_ERROR;
+    	} else if (m_nativeStateCode.equals("H")) {
+            return SubState.SUSPENDED_ACTIVE;
+    	} else if (m_nativeStateCode.equals("Q")) {
+            return SubState.RUNNING_QUEUED;
+    	} else if (m_nativeStateCode.equals("S") || m_nativeStateCode.equals("W")) {
+            return SubState.SUSPENDED_QUEUED;
+    	} else  { // "R"
+            return SubState.RUNNING_ACTIVE;
+    	} 
     }
 
 }
