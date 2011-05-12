3bff88c1d473559bd4c43ce534add21106d3bcab
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-05-12 10:36:17 +0200

    implement StreamableJobBatch

--- adaptors/jsaga-adaptor-batchssh/src/fr/in2p3/jsaga/adaptor/batchssh/job/BatchSSHJobAdaptor.java
+++ adaptors/jsaga-adaptor-batchssh/src/fr/in2p3/jsaga/adaptor/batchssh/job/BatchSSHJobAdaptor.java
@@ -10,6 +10,10 @@ import fr.in2p3.jsaga.adaptor.job.control.advanced.HoldableJobAdaptor;
 import fr.in2p3.jsaga.adaptor.job.control.advanced.SuspendableJobAdaptor;
 import fr.in2p3.jsaga.adaptor.job.control.description.JobDescriptionTranslator;
 import fr.in2p3.jsaga.adaptor.job.control.description.JobDescriptionTranslatorXSLT;
+import fr.in2p3.jsaga.adaptor.job.control.interactive.JobIOGetterInteractive;
+import fr.in2p3.jsaga.adaptor.job.control.interactive.JobIOHandler;
+import fr.in2p3.jsaga.adaptor.job.control.interactive.StreamableJobBatch;
+import fr.in2p3.jsaga.adaptor.job.control.interactive.StreamableJobInteractiveGet;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobMonitorAdaptor;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,9 +25,11 @@ import org.ogf.saga.error.*;
 /******************************************************
  * File:   BatchSSHAdaptor
  * Author: Taha BENYEZZA & Yassine BACHAR
+ * Author: Lionel Schwarz
  * Date:   07 December 2010
  * ***************************************************/
-public class BatchSSHJobAdaptor extends BatchSSHAdaptorAbstract implements JobControlAdaptor, SuspendableJobAdaptor, HoldableJobAdaptor {
+public class BatchSSHJobAdaptor extends BatchSSHAdaptorAbstract implements JobControlAdaptor, StreamableJobBatch,
+	SuspendableJobAdaptor, HoldableJobAdaptor {
 
     public JobMonitorAdaptor getDefaultJobMonitor() {
         return new BatchSSHMonitorAdaptor();
@@ -33,9 +39,92 @@ public class BatchSSHJobAdaptor extends BatchSSHAdaptorAbstract implements JobCo
         return new JobDescriptionTranslatorXSLT("xsl/job/batchSSH.xsl");
     }
 
-    public String submit(String jobDesc, boolean checkMatch, String uniqId) throws PermissionDeniedException, TimeoutException, NoSuccessException, BadResource {
+	public JobIOGetterInteractive submitInteractive(String jobDesc,
+			boolean checkMatch) throws PermissionDeniedException,
+			TimeoutException, NoSuccessException {
+        String SubmitCommand = "qsub -I ";
+
+		// Openning a new session
+        try {
+            Session session = connexion.openSession();
+        	//session = this.sendCommand(SubmitCommand);
+        	session.execCommand(SubmitCommand);
+            return new BatchSSHJobIOHandler(session);
+        } catch (IOException ex) {
+			throw new NoSuccessException("Unable to submit job", ex);
+        }
+
+	}
+
+	public JobIOHandler submit(String jobDesc, boolean checkMatch,
+			String uniqId, InputStream stdin) throws PermissionDeniedException,
+			TimeoutException, NoSuccessException {
         // Creating the pbs script file's name using the randomUUID
-        String FileName = UUID.randomUUID().toString() + ".pbs";
+        String FileName = uniqId + ".pbs";
+        // the scpript's contenent
+//System.out.println(jobDesc);        
+        StringBuilder sb = new StringBuilder("#!/bin/bash\n");
+        sb.append(jobDesc);
+        // the submission command
+        String SubmitCommand = "qsub " + FileName;
+        String JobId = null;
+        Session session = null;
+        InputStream stdout;
+        BufferedReader br;
+
+        SFTPv3Client sftp;
+        try {
+			sftp = new SFTPv3Client(connexion);
+			SFTPv3FileHandle script = sftp.createFile(FileName);
+			sftp.write(script, 0, sb.toString().getBytes(), 0, sb.length());
+			int n;
+			int offset = sb.length();
+			byte[] buffer = new byte[1024];
+			while ((n = stdin.read(buffer)) != -1) {
+				sftp.write(script, offset, buffer, 0, n);
+				offset = offset+n;
+			}
+			sftp.closeFile(script);
+		} catch (IOException e1) {
+			throw new NoSuccessException("Unable to send script via SFTP", e1);
+		}
+
+		// Openning a new session
+        try {
+        	session = this.sendCommand(SubmitCommand);
+            // Retrieving the standard output
+            stdout = new StreamGobbler(session.getStdout());
+            br = new BufferedReader(new InputStreamReader(stdout));
+            
+            JobId = br.readLine();
+            if (JobId == null) {
+            	throw new IOException("qsub did not return a JobID");
+            }
+        } catch (IOException ex) {
+			throw new NoSuccessException("Unable to submit job", ex);
+        } catch (BatchSSHCommandFailedException e) {
+        	if (e.isErrorTypeOfBadResource()) {
+        		throw new BadResource("Error in Job description", e);
+        	}
+			throw new NoSuccessException("Unable to submit job", e);
+        } finally {
+            if (session != null) session.close();
+            // remove the script and close the SFTP
+            try {
+				sftp.rm(FileName);
+			} catch (IOException e) {
+				// Ignore
+			}
+            sftp.close();
+        }
+
+        return new BatchSSHJobIOHandler(session);
+		
+	}
+
+	public String submit(String jobDesc, boolean checkMatch, String uniqId) throws PermissionDeniedException, TimeoutException, NoSuccessException, BadResource {
+        // Creating the pbs script file's name using the randomUUID
+        String FileName = uniqId + ".pbs";
         // the scpript's contenent
 //System.out.println(jobDesc);        
         StringBuilder sb = new StringBuilder("#!/bin/bash\n");
@@ -165,4 +254,5 @@ public class BatchSSHJobAdaptor extends BatchSSHAdaptorAbstract implements JobCo
 			NoSuccessException {
 		return this.release(nativeJobId);
 	}
+
 }
