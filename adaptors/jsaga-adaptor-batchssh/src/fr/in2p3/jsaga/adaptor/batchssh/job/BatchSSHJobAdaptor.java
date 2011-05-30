bc0efbe13a07ac3d171063b4feea208b09aa3fa4
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-05-30 17:31:00 +0200

    fix data staging

--- adaptors/jsaga-adaptor-batchssh/src/fr/in2p3/jsaga/adaptor/batchssh/job/BatchSSHJobAdaptor.java
+++ adaptors/jsaga-adaptor-batchssh/src/fr/in2p3/jsaga/adaptor/batchssh/job/BatchSSHJobAdaptor.java
@@ -41,95 +41,14 @@ public class BatchSSHJobAdaptor extends BatchSSHAdaptorAbstract implements JobCo
         return new JobDescriptionTranslatorXSLT("xsl/job/batchSSH.xsl");
     }
 
-	/*public JobIOGetterInteractive submitInteractive(String jobDesc,
-			boolean checkMatch) throws PermissionDeniedException,
-			TimeoutException, NoSuccessException {
-        String SubmitCommand = "qsub -I ";
-
-		// Openning a new session
-        try {
-            Session session = connexion.openSession();
-        	//session = this.sendCommand(SubmitCommand);
-        	session.execCommand(SubmitCommand);
-            return new BatchSSHJobIOHandler(session);
-        } catch (IOException ex) {
-			throw new NoSuccessException("Unable to submit job", ex);
-        }
-
-	}*/
-
-	public JobIOHandler submit(String jobDesc, boolean checkMatch,
-			String uniqId, InputStream stdin) throws PermissionDeniedException,
-			TimeoutException, NoSuccessException {
-        // Creating the pbs script file's name using the randomUUID
-        //String FileName = uniqId + ".pbs";
-        // the scpript's contenent
-//System.out.println(jobDesc);        
-        StringBuilder sb = new StringBuilder("#!/bin/bash\n");
-        sb.append(jobDesc);
-        // the submission command
-        String SubmitCommand = "echo '" + sb + "' | qsub -I "; //;+ FileName;
-        //String JobId = null;
-        Session session = null;
-        InputStream stdout;
-        BufferedReader br;
-
-        /*
-        SFTPv3Client sftp;
-        try {
-			sftp = new SFTPv3Client(connexion);
-			SFTPv3FileHandle script = sftp.createFile(FileName);
-			sftp.write(script, 0, sb.toString().getBytes(), 0, sb.length());
-			int n;
-			int offset = sb.length();
-			byte[] buffer = new byte[1024];
-			while ((n = stdin.read(buffer)) != -1) {
-				sftp.write(script, offset, buffer, 0, n);
-				offset = offset+n;
-			}
-			sftp.closeFile(script);
-		} catch (IOException e1) {
-			throw new NoSuccessException("Unable to send script via SFTP", e1);
-		}
-		*/
-		// Openning a new session
-        try {
-        	session = connexion.openSession();//this.sendCommand(SubmitCommand);
-            // Retrieving the standard output
-            //stdout = new StreamGobbler(session.getStdout());
-            //br = new BufferedReader(new InputStreamReader(stdout));
-            
-            //JobId = br.readLine();
-            //if (JobId == null) {
-            //	throw new IOException("qsub did not return a JobID");
-            //}
-        	session.execCommand(SubmitCommand);
-        } catch (IOException ex) {
-			throw new NoSuccessException("Unable to submit job", ex);
-        /*} catch (BatchSSHCommandFailedException e) {
-        	if (e.isErrorTypeOfBadResource()) {
-        		throw new BadResource("Error in Job description", e);
-        	}
-			throw new NoSuccessException("Unable to submit job", e);*/
-        /*} finally {
-            if (session != null) session.close();
-            // remove the script and close the SFTP
-            try {
-				sftp.rm(FileName);
-			} catch (IOException e) {
-				// Ignore
-			}
-            sftp.close();*/
-        }
-
-        return new BatchSSHJobIOHandler(session);
-		
-	}
-
 	public String submit(String jobDesc, boolean checkMatch, String uniqId) throws PermissionDeniedException, TimeoutException, NoSuccessException, BadResource {
-        // Creating the pbs script file's name using the randomUUID
+        // Get executable name for chmod
+    	ArrayList<String[]> transfers = getCustomizedParams(jobDesc, BatchSSHJob.ATTR_VAR_JSAGA_EXECUTABLE);
+    	String executableFileName = (this.m_stagingDirectory != null)?this.m_stagingDirectory + "/":"";
+    	executableFileName += transfers.get(0)[0];
+		// Creating the pbs script file's name using the randomUUID
         String FileName = uniqId + ".pbs";
-        // the scpript's contenent
+        // the script's content
 System.out.println(jobDesc);        
         StringBuilder sb = new StringBuilder("#!/bin/bash\n");
         sb.append(jobDesc);
@@ -150,6 +69,13 @@ System.out.println(jobDesc);
 			throw new NoSuccessException("Unable to send script via SFTP", e1);
 		}
 
+		// chmod the executable
+		try {
+			session = this.sendCommand("/bin/chmod a+x " + executableFileName);
+		} catch (Exception e) {
+			// do nothing
+		}
+		
 		// Openning a new session
         try {
         	session = this.sendCommand(SubmitCommand);
@@ -280,19 +206,9 @@ System.out.println(jobDesc);
 
 	private StagingTransfer[] getStagingTransfers(String nativeJobId, boolean input) throws NoSuccessException {
         BatchSSHJob bj = this.getAttributes(new String[]{nativeJobId}).get(0);
-		String[][] path_pairs = bj.getStagingTransfers(input);
-    	StagingTransfer[] st = new StagingTransfer[path_pairs.length];
-		for (int i=0; i<path_pairs.length; i++) {
-	    	String to = path_pairs[i][0]; // local 
-	    	String from = this.makeTURL(path_pairs[i][1]); // remote sftp://
-			st[i] = new StagingTransfer(from, to, false);
-		}
-		return st;
+    	ArrayList<String[]> transfers = bj.getStagingTransfers(input);
+		return this.arrayListToStagingTransfers(transfers, input);
 	}
-
-	
-	
-	
 	
 	public String getStagingDirectory(String nativeJobDescription, String uniqId)
 			throws PermissionDeniedException, TimeoutException,
@@ -308,37 +224,43 @@ System.out.println(jobDesc);
 			String nativeJobDescription, String uniqId)
 			throws PermissionDeniedException, TimeoutException,
 			NoSuccessException {
-    	ArrayList transfers = new ArrayList();
-    	// TODO put something in it!
-//System.out.println(nativeJobDescription);
-		Scanner sc = new Scanner(nativeJobDescription);
-		String var_string=null;
-		String line;
-		try {
-			while (sc.hasNextLine()) {
-				line = sc.nextLine();
-				if (line.startsWith("#PBS -v")) {
-					line = line.substring("#PBS -v".length());
-					transfers = BatchSSHJob.getFilteredVars(line, BatchSSHJob.ATTR_VAR_JSAGA_STAGEIN);
-					break;
-				}
-			}
-		} catch (NoSuchElementException nsee) {
-			return new StagingTransfer[]{};
-		}
+    	ArrayList<String[]> transfers = getCustomizedParams(nativeJobDescription, BatchSSHJob.ATTR_VAR_JSAGA_STAGEIN);
+		return this.arrayListToStagingTransfers(transfers, true);
+	}
+
+	private StagingTransfer[] arrayListToStagingTransfers(ArrayList<String[]> transfers, boolean input) {
     	StagingTransfer[] st = new StagingTransfer[transfers.size()];
 		for (int i=0; i<transfers.size(); i++) {
 			String[] path_pair = (String[])transfers.get(i);
-	    	String to = this.makeTURL(path_pair[0]); // remote sftp:// 
-	    	String from = path_pair[1]; // local
+			String from, to;
+			if (input) {
+				to = this.makeTURL(path_pair[1]); // remote sftp:// 
+				from = path_pair[0]; // local
+			} else {
+				from = this.makeTURL(path_pair[1]); // remote sftp:// 
+				to = path_pair[0]; // local
+			}
 System.out.println("TR " + from + " => " + to);	    	
 			st[i] = new StagingTransfer(from, to, false);
 		}
-    	//return (StagingTransfer[]) transfers.toArray(st);		
 		return st;
 	}
 
+	private ArrayList<String[]> getCustomizedParams(String nativeJobDescription, String filter) throws NoSuchElementException {
+//    	ArrayList params = new ArrayList();
+		Scanner sc = new Scanner(nativeJobDescription);
+		String line;
+		while (sc.hasNextLine()) {
+			line = sc.nextLine();
+			if (line.startsWith("#PBS -v")) {
+				line = line.substring("#PBS -v".length());
+				return BatchSSHJob.getFilteredVars(line, filter);
+			}
+		}
+		return new ArrayList<String[]>();
+	}
+	
 	private String makeTURL(String filename) {
-    	return "sftp://cccetest01.in2p3.fr" + /*connexion.getHostname() +*/ ":" + connexion.getPort() + "/" + filename;
+    	return "sftp://" + connexion.getHostname() + ":" + connexion.getPort() + "/" + filename;
 	}
 }
