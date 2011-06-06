43b7576792554bbbd1b014d312656394645fb21e
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-06-06 11:37:38 +0200

    added

---
+++ adaptors/jsaga-adaptor-globus/test/src/integration/MyProxyContextInfo.java
@@ -0,0 +1,11 @@
+package integration;
+
+import org.ogf.saga.context.Context;
+import org.ogf.saga.context.ContextInfoTest;
+
+public class MyProxyContextInfo extends ContextInfoTest {
+    public MyProxyContextInfo() throws Exception {
+        super();
+    }
+
+}
