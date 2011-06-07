1ea529da0faccac489784583db44474b6bb9108e
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-06-07 13:40:40 +0200

    new test

---
+++ adaptors/jsaga-adaptor-bes-unicore6/test/src/integration/BesUnicoreContextInfo.java
@@ -0,0 +1,21 @@
+package integration;
+
+import org.ogf.saga.context.ContextInfoTest;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   U6ContextInfo
+ * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
+ * Date:   3 f�vr. 2010
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class BesUnicoreContextInfo extends ContextInfoTest {
+    public BesUnicoreContextInfo() throws Exception {
+        super();
+    }
+}
