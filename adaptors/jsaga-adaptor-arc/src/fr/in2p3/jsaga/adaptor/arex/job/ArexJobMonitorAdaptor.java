99f49ea3d7f391569a7aab0479022f71d60a11a7
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-09-26 16:03:17 +0200

    fix getinfo() because WSRF document does not contain job information anymore

--- adaptors/jsaga-adaptor-arc/src/fr/in2p3/jsaga/adaptor/arex/job/ArexJobMonitorAdaptor.java
+++ adaptors/jsaga-adaptor-arc/src/fr/in2p3/jsaga/adaptor/arex/job/ArexJobMonitorAdaptor.java
@@ -1,32 +1,41 @@
 package fr.in2p3.jsaga.adaptor.arex.job;
 
+import java.rmi.RemoteException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.rpc.ServiceException;
+import javax.xml.soap.SOAPException;
 
 import org.ogf.saga.error.AuthenticationFailedException;
 import org.ogf.saga.error.AuthorizationFailedException;
 import org.ogf.saga.error.BadParameterException;
 import org.ogf.saga.error.NoSuccessException;
 import org.ogf.saga.error.NotImplementedException;
+import org.ogf.saga.error.PermissionDeniedException;
 import org.ogf.saga.error.TimeoutException;
 
-import fr.in2p3.jsaga.adaptor.arex.data.ArexHttpsDataAdaptor;
 import fr.in2p3.jsaga.adaptor.bes.job.BesJobMonitorAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobInfoAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
+import fr.in2p3.jsaga.generated.org.w3.x2005.x08.addressing.EndpointReferenceType;
 
 import org.apache.axis.client.Stub;
 import org.apache.axis.message.MessageElement;
 import org.apache.axis.message.SOAPHeaderElement;
 import org.apache.axis.message.Text;
 import org.apache.axis.types.URI;
+import org.apache.log4j.Logger;
 import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStatusType;
 import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStateEnumeration;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusesResponseType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.GetActivityStatusesType;
+import org.ggf.schemas.bes.x2006.x08.besFactory.InvalidRequestMessageFaultType;
 import org.nordugrid.schemas.arex.ARex_PortType;
 import org.nordugrid.schemas.arex.ARex_ServiceLocator;
 
@@ -49,6 +58,7 @@ public class ArexJobMonitorAdaptor extends BesJobMonitorAdaptor implements JobIn
         
 	protected ARex_PortType _arex_pt = null;
 	private static final Integer NB_TRIES = 10;
+	private static final String AREX_CREATIONTIME = "CreationTime";
 	private static final String AREX_EXITCODE = "ExitCode";
 	private static final String AREX_SUBMISSIONTIME = "SubmissionTime";
 	private static final String AREX_ENDTIME = "EndTime";
@@ -89,6 +99,8 @@ public class ArexJobMonitorAdaptor extends BesJobMonitorAdaptor implements JobIn
 		} catch (ServiceException e) {
 			throw new NoSuccessException(e);
 		}
+		//System.out.println(getInfo("https://interop.grid.niif.hu:2010/arex-x509;93413170414131940927376;JobID;http://www.nordugrid.org/schemas/a-rex","ExitCode",1));
+		//throw new NoSuccessException("DUMMY");
     }
 
 	public void disconnect() throws NoSuccessException {
@@ -105,7 +117,8 @@ public class ArexJobMonitorAdaptor extends BesJobMonitorAdaptor implements JobIn
 	}
 	
 	public Date getCreated(String nativeJobId) throws NotImplementedException,	NoSuccessException {
-		throw new NotImplementedException();
+		//throw new NotImplementedException();
+		return getTime(nativeJobId, AREX_CREATIONTIME);
 	}
 
 	public Date getStarted(String nativeJobId) throws NotImplementedException,	NoSuccessException {
@@ -131,11 +144,68 @@ public class ArexJobMonitorAdaptor extends BesJobMonitorAdaptor implements JobIn
 		};
 	}
 
-	// Wrapper for getInfoXML or getInfoWSRF
+	// Wrapper for getInfoXML or getInfoWSRF or getInfoBES
 	private String getInfo(String nativeJobId, String infoName, Integer nbTries) throws NotImplementedException, NoSuccessException {
-		return getInfoWSRP( nativeJobId,  infoName, nbTries);
+		return getInfoBES( nativeJobId,  infoName, nbTries);
 	}
 	
+	private String getInfoBES(String nativeJobId, String infoName, Integer nbTries) throws NotImplementedException, NoSuccessException {
+		try {
+    		int loop = 0;
+    		while (loop < nbTries) {
+				GetActivityStatusesType requestStatus = new GetActivityStatusesType();
+				EndpointReferenceType[] refs = new EndpointReferenceType[1];
+				refs[0] = nativeId2ActivityId(nativeJobId);
+				requestStatus.setActivityIdentifier(refs);
+				// add AREX extension to get more attributes
+				MessageElement arexExtensions = new MessageElement("ActivityStatusVerbosity", "a-rex", ArexJobControlAdaptor.AREX_NAMESPACE_URI);
+				arexExtensions.addTextNode("Full");
+				requestStatus.set_any(new MessageElement[]{arexExtensions});
+				Logger.getLogger(ArexJobMonitorAdaptor.class).debug(fr.in2p3.jsaga.adaptor.bes.BesUtils.dumpBESMessage(requestStatus));
+				GetActivityStatusesResponseType responseStatus = _bes_pt.getActivityStatuses(requestStatus);
+				Logger.getLogger(ArexJobMonitorAdaptor.class).debug(fr.in2p3.jsaga.adaptor.bes.BesUtils.dumpBESMessage(responseStatus));
+				// Search for <ComputingActivity
+				for (MessageElement me : responseStatus.getResponse(0).getActivityStatus().get_any()) {
+					if (me.getName().equals("ComputingActivity")) {
+						// CreationTime is defined as an attribute of ComputingActivity node
+						if (AREX_CREATIONTIME.equals(infoName))
+							return me.getAttribute(AREX_CREATIONTIME);
+						// all other info are children nodes
+						java.util.Iterator iter = me.getChildElements();
+						while (iter.hasNext()) {
+							MessageElement jobinfo = (MessageElement)(iter.next());
+							if (jobinfo.getName().equals(infoName)) {
+								return jobinfo.getFirstChild().getNodeValue();
+							}
+						}
+					}
+				}
+				loop++;
+				// Do not sleep at last attempt
+	    		if (loop < nbTries) Thread.sleep(5000);
+    		}
+			throw new NotImplementedException("Could not get " + infoName);
+		} catch (InvalidRequestMessageFaultType e) {
+			throw new NoSuccessException(e);
+		} catch (RemoteException e) {
+			throw new NoSuccessException(e);
+		} catch (SOAPException e) {
+			throw new NoSuccessException(e);
+		} catch (InterruptedException e) {
+			throw new NoSuccessException(e);
+		}
+		
+	}
+	/**
+	 * get job information through the WSRP method
+	 * @param nativeJobId : the job id
+	 * @param infoName : the name of the information to get
+	 * @param nbTries : the number of tries before considering the query does not work
+	 * @return the job information
+	 * @throws NotImplementedException
+	 * @throws NoSuccessException
+	 * @deprecated
+	 */
 	private String getInfoWSRP(String nativeJobId, String infoName, Integer nbTries) throws NotImplementedException, NoSuccessException {
         try {
     		int loop = 0;
@@ -175,5 +245,4 @@ public class ArexJobMonitorAdaptor extends BesJobMonitorAdaptor implements JobIn
         	throw new NoSuccessException(e);
         }
 	}
-	
 }
