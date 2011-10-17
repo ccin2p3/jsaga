ba8cb49dfe9a771225f0481c947f2109be237e47
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-10-17 16:15:08 +0200

    list() not supported

--- adaptors/jsaga-adaptor-bes-genesis2/src/fr/in2p3/jsaga/adaptor/bes_genesis2/job/BesGenesisIIJobMonitorAdaptor.java
+++ adaptors/jsaga-adaptor-bes-genesis2/src/fr/in2p3/jsaga/adaptor/bes_genesis2/job/BesGenesisIIJobMonitorAdaptor.java
@@ -1,7 +1,11 @@
 package fr.in2p3.jsaga.adaptor.bes_genesis2.job;
 
+import java.util.List;
+
 import org.ggf.schemas.bes.x2006.x08.besFactory.ActivityStatusType;
 import org.ogf.saga.error.NoSuccessException;
+import org.ogf.saga.error.PermissionDeniedException;
+import org.ogf.saga.error.TimeoutException;
 
 import fr.in2p3.jsaga.adaptor.bes.job.BesJobMonitorAdaptor;
 import fr.in2p3.jsaga.adaptor.job.monitor.JobStatus;
@@ -28,6 +32,15 @@ public class BesGenesisIIJobMonitorAdaptor extends BesJobMonitorAdaptor  {
 	@Override
 	protected JobStatus getJobStatus(String nativeJobId, ActivityStatusType ast) throws NoSuccessException {
 		return new BesGenesisIIJobStatus(nativeJobId, ast);
+	}
+	
+	@Override
+	public String[] list() throws PermissionDeniedException, TimeoutException,	NoSuccessException {
+		// GenesisII does not provide job list in FactoryResourceAttributesDocument
+		String[] urls = super.list();
+		if (urls.length == 0)
+			throw new NoSuccessException("GenesisII servers do not provide the list of jobs");
+		return urls;
 	}
 
 }
