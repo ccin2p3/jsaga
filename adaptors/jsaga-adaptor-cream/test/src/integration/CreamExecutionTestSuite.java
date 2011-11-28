4c9b7487639310eda29f3d1ef2cce4879c85ec6f
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-11-28 18:37:50 +0100

    add tests for description

--- adaptors/jsaga-adaptor-cream/test/src/integration/CreamExecutionTestSuite.java
+++ adaptors/jsaga-adaptor-cream/test/src/integration/CreamExecutionTestSuite.java
@@ -2,6 +2,8 @@ package integration;
 
 import junit.framework.Test;
 
+import org.ogf.saga.job.JobDescriptionTest;
+import org.ogf.saga.job.JobRunDescriptionTest;
 import org.ogf.saga.job.JobRunInfoTest;
 import org.ogf.saga.job.JobRunInteractiveTest;
 import org.ogf.saga.job.JobRunMinimalTest;
@@ -28,6 +30,23 @@ public class CreamExecutionTestSuite extends JSAGATestSuite {
     /** test cases */
     public static class CreamJobRunMinimalTest extends JobRunMinimalTest {
         public CreamJobRunMinimalTest() throws Exception {super("cream");}
+    }
+
+    public static class CreamJobDescriptionTest extends JobDescriptionTest {
+        public CreamJobDescriptionTest() throws Exception {super("cream");}
+        public void test_totalCPUCount() throws Exception {super.ignore("Not supported"); }
+        public void test_workingDirectory() throws Exception {super.ignore("Not supported"); }
+        public void test_threadsPerProcess() throws Exception {super.ignore("Not supported"); }
+        public void test_cleanup() throws Exception {super.ignore("Not supported"); }
+        public void test_candidateHosts() throws Exception {super.ignore("Not supported"); }
+        public void test_queue() throws Exception {super.ignore("SAGA queue is override by Queue in URL"); }
+     }
+
+    public static class CreamJobRunDescriptionTest extends JobRunDescriptionTest {
+        public CreamJobRunDescriptionTest() throws Exception {super("cream");}
+        public void test_run_inWorkingDirectory() { super.ignore("WorkingDirectory is not supported"); }
+        public void test_run_queueRequirement() throws Exception {super.ignore("SAGA queue is override by Queue in URL"); }
+        //public void test_run_cpuTimeRequirement() throws Exception { super.ignore("TotalCPUTime is ignored");}
     }
 
     public static class CreamJobRunSandboxTest extends JobRunSandboxTest {
