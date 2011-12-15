cd30e76d2a544b5e1f4b189b7118cc49915849f1
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2011-12-15 18:00:38 +0100

    remove mlst(): it is useless

--- adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/data/GsiftpClient.java
+++ adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/data/GsiftpClient.java
@@ -25,7 +25,6 @@ import org.globus.ftp.vanilla.Reply;
  * ***************************************************
  * Description:                                      */
 /**
- * This class fixes the 'line.separator' bug in parsing MLST reply
  * This class uses the local FTPControlChannel class instead of Globus GridFTPControlChannel to get the welcome message
  */
 
@@ -57,52 +56,6 @@ public class GsiftpClient extends GridFTPClient {
 		}
 		return true;
 	}
-	
-    /**
-     * Get info of a certain remote file in Mlsx format.
-     * 
-     * Tokenize with "\n" instead of System.getProperty("line.separator")
-     * because the line separator is the server one not the client one.
-     * TODO: use command 'SYST' to know the operating system on the server
-     */
-	@Override
-    public MlsxEntry mlst(String fileName)
-        throws IOException, ServerException {
-        try {
-            Reply reply = controlChannel.execute(new Command("MLST", fileName));
-            String replyMessage = reply.getMessage();
-            StringTokenizer replyLines =
-                new StringTokenizer(
-                                    replyMessage,
-                                    "\n");
-            if (replyLines.hasMoreElements()) {
-                replyLines.nextElement();
-            } else {
-                throw new FTPException(FTPException.UNSPECIFIED,
-                                       "Expected multiline reply");
-            }
-            if (replyLines.hasMoreElements()) {
-                String line = (String) replyLines.nextElement();
-                return new MlsxEntry(line);
-            } else {
-                throw new FTPException(FTPException.UNSPECIFIED,
-                                       "Expected multiline reply");
-            }
-        } catch (FTPReplyParseException rpe) {
-            throw ServerException.embedFTPReplyParseException(rpe);
-        } catch (UnexpectedReplyCodeException urce) {
-            throw ServerException.embedUnexpectedReplyCodeException(
-                            urce,
-                            "Server refused MLST command");
-        } catch (FTPException e) {
-            ServerException ce =
-                new ServerException(
-                                    ClientException.UNSPECIFIED,
-                                    "Could not create MlsxEntry");
-            ce.setRootCause(e);
-            throw ce;
-        }
-    }
 	
 	@Override
 	public void close() throws ServerException, IOException {
