1b96516a9b0618471756c9d13ce58544dadbcb3b
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2012-08-16 10:53:24 +0200

    added

---
+++ adaptors/jsaga-adaptor-ourgrid/test/src/jar/data/OurGridDataAdaptorTest.java
@@ -0,0 +1,24 @@
+package jar.data;
+
+import fr.in2p3.jsaga.adaptor.ourgrid.job.OurGridAbstract;
+import junit.framework.TestCase;
+
+/* ***************************************************
+ * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
+ * ***             http://cc.in2p3.fr/             ***
+ * ***************************************************
+ * File:   MyProtocolDataAdaptorTest
+ * Author:
+ * Date:
+ * ***************************************************
+ * Description:                                      */
+/**
+ *
+ */
+public class OurGridDataAdaptorTest extends TestCase {
+    public void test_getScheme() {
+        assertEquals("ourgrid",new OurGridAbstract() 
+        {
+		});
+    }
+}
