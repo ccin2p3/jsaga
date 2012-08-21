cab873c214eb739b9f1950dce45b49fa0f6aa8cc
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2012-08-21 11:40:38 +0200

    new argument: content_type

--- externals/secret-service-api-java/src/org/freedesktop/Secret/Secret.java
+++ externals/secret-service-api-java/src/org/freedesktop/Secret/Secret.java
@@ -6,15 +6,18 @@ import org.freedesktop.dbus.Struct;
 public final class Secret extends Struct
 {
    @Position(0)
-   public final DBusInterface session;
+   public final DBusInterface session; // The session that was used to encode the secret.
    @Position(1)
-   public final List<Byte> parameters;
+   public final List<Byte> parameters; // Algorithm dependent parameters for secret value encoding.
    @Position(2)
-   public final List<Byte> value;
-  public Secret(DBusInterface a, List<Byte> b, List<Byte> c)
+   public final List<Byte> value; // Possibly encoded secret value
+   @Position(3)
+   public final String content_type;
+  public Secret(DBusInterface a, List<Byte> b, List<Byte> c, String d) // The content type of the secret. For example: 'text/plain; charset=utf8'
   {
    this.session = a;
    this.parameters = b;
    this.value = c;
+   this.content_type = d;
   }
 }
