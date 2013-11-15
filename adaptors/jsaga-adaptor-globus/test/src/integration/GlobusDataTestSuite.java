24ed3afb3bb35355e391ac2a55a95e5bb1317dc0
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2013-11-15 16:22:18 +0100

    Upgrade Data tests to JUnit4

--- adaptors/jsaga-adaptor-globus/test/src/integration/GlobusDataTestSuite.java
+++ adaptors/jsaga-adaptor-globus/test/src/integration/GlobusDataTestSuite.java
@@ -2,8 +2,10 @@ package integration;
 
 import java.util.Random;
 
-import junit.framework.Test;
-
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.ogf.saga.buffer.Buffer;
 import org.ogf.saga.buffer.BufferFactory;
 import org.ogf.saga.file.*;
@@ -14,7 +16,7 @@ import org.ogf.saga.url.URL;
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
 * ***             http://cc.in2p3.fr/             ***
 * ***************************************************
-* File:   GlobusIntegrationTestSuite
+* File:   GlobusDataTestSuite
 * Author: Sylvain Reynaud (sreynaud@in2p3.fr)
 * Date:   31 juil. 2007
 * ***************************************************
@@ -22,54 +24,52 @@ import org.ogf.saga.url.URL;
 /**
  *
  */
-public class GlobusDataTestSuite extends JSAGATestSuite {
-    /** create test suite */
-    public static Test suite() throws Exception {return new GlobusDataTestSuite();}
-    /** index of test cases */
-    public static class index extends IndexTest {public index(){super(GlobusDataTestSuite.class);}}
-
+@RunWith(Suite.class)
+@SuiteClasses({
+//    GlobusDataTestSuite.GsiftpNSSetUpTest.class,
+    GlobusDataTestSuite.GsiftpNSEntryTest.class,
+    GlobusDataTestSuite.GsiftpDirTest.class,
+    GlobusDataTestSuite.GsiftpDirectoryMakeTest.class,
+    GlobusDataTestSuite.GsiftpFileReadTest.class,
+    GlobusDataTestSuite.GsiftpFileWriteTest.class,
+    GlobusDataTestSuite.GsiftpDataMovementTest.class,
+    GlobusDataTestSuite.GsiftpEmulatorDataMovementTest.class
+})
+public class GlobusDataTestSuite {
     /** test cases */
-    public static class GsiftpNSSetUpTest extends NSSetUpTest {
-        public GsiftpNSSetUpTest() throws Exception {super("gsiftp");}
-    }
-    public static class GsiftpNSEntryTest extends NSEntryTest {
+//    public static class GsiftpNSSetUpTest extends NSSetUpTest {
+//        public GsiftpNSSetUpTest() throws Exception {super("gsiftp");}
+//    }
+    public static class GsiftpNSEntryTest extends EntryTest {
         public GsiftpNSEntryTest() throws Exception {super("gsiftp");}
     }
-    public static class GsiftpDirectoryListTest extends DirectoryListTest {
-        public GsiftpDirectoryListTest() throws Exception {super("gsiftp");}
+    public static class GsiftpDirTest extends DirTest {
+        public GsiftpDirTest() throws Exception {super("gsiftp");}
     }
-    public static class GsiftpDirectoryMakeTest extends DirectoryMakeTest {
+    public static class GsiftpDirectoryMakeTest extends MakeDirTest {
         public GsiftpDirectoryMakeTest() throws Exception {super("gsiftp");}
     }
-    public static class GsiftpDirectoryTest extends DirectoryTest {
-        public GsiftpDirectoryTest() throws Exception {super("gsiftp");}
-    }
-    public static class GsiftpFileReadTest extends FileReadTest {
+    public static class GsiftpFileReadTest extends ReadTest {
         public GsiftpFileReadTest() throws Exception {super("gsiftp");}
     }
-    public static class GsiftpFileWriteTest extends FileWriteTest {
+    public static class GsiftpFileWriteTest extends WriteTest {
     	// test_write_append: DPM ignores the append flag so the adaptor sends a BadParameter
     	// test_read_and_write: does not work on DPM servers for unknown reason
         public GsiftpFileWriteTest() throws Exception {super("gsiftp");}
     }
-    public static class GsiftpNSCopyTest extends NSCopyTest {
-        public GsiftpNSCopyTest() throws Exception {super("gsiftp", "gsiftp");}
+    public static class GsiftpDataMovementTest extends DataMovementTest {
+        public GsiftpDataMovementTest() throws Exception {super("gsiftp", "gsiftp");}
     }
-    public static class GsiftpNSCopyRecursiveTest extends NSCopyRecursiveTest {
-        public GsiftpNSCopyRecursiveTest() throws Exception {super("gsiftp", "gsiftp");}
-    }
-    public static class GsiftpNSMoveTest extends NSMoveTest {
-        public GsiftpNSMoveTest() throws Exception {super("gsiftp", "gsiftp");}
-    }
-    public static class Gsiftp_to_EmulatorNSCopyTest extends NSCopyTest {
-        public Gsiftp_to_EmulatorNSCopyTest() throws Exception {super("gsiftp", "test");}
+    public static class GsiftpEmulatorDataMovementTest extends DataMovementTest {
+        public GsiftpEmulatorDataMovementTest() throws Exception {super("gsiftp", "test");}
+        @Test
         public void test_copy_1MB() throws Exception {
-        	String File1MBName = "file1MB.txt";
-        	String bufferString = "01234567";
-        	for (int i=0; i<17; i++) {
-        		bufferString += bufferString;
-        	}
-        	NSEntry m_file_1MB = m_dir.open(createURL(m_subDirUrl, File1MBName), FLAGS_FILE);
+            String File1MBName = "file1MB.txt";
+            String bufferString = "01234567";
+            for (int i=0; i<17; i++) {
+                bufferString += bufferString;
+            }
+            NSEntry m_file_1MB = m_dir.open(createURL(m_subDirUrl, File1MBName), FLAGS_FILE);
             Buffer buffer = BufferFactory.createBuffer(bufferString.getBytes());
             ((File)m_file_1MB).write(buffer);
             m_file_1MB.close();
@@ -81,11 +81,4 @@ public class GlobusDataTestSuite extends JSAGATestSuite {
             
         }
     }
-    public static class Gsiftp_to_EmulatorNSCopyRecursiveTest extends NSCopyRecursiveTest {
-        public Gsiftp_to_EmulatorNSCopyRecursiveTest() throws Exception {super("gsiftp", "test");}
-    }
-    public static class Gsiftp_to_EmulatorNSMoveTest extends NSMoveTest {
-        public Gsiftp_to_EmulatorNSMoveTest() throws Exception {super("gsiftp", "test");}
-    }
-
 }
