db4884bf1a2ba8d117e08d5a00d06872bdb488f1
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2014-02-04 13:45:36 +0100

    deprecated

--- saga-api-test/src/org/ogf/saga/namespace/NSSetUpTest.java
+++ saga-api-test/src/org/ogf/saga/namespace/NSSetUpTest.java
@@ -14,6 +14,7 @@ import org.ogf.saga.namespace.abstracts.AbstractNSEntryTest;
 /**
  *
  */
+@Deprecated
 public abstract class NSSetUpTest extends AbstractNSEntryTest {
     protected NSSetUpTest(String protocol) throws Exception {
         super(protocol);
