79e6f05ba56baedf0338ea967d7c6819ded9ba7a
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2014-02-06 15:44:47 +0100

    add TODO

--- adaptors/jsaga-adaptor-cream/src/org/globus/gsi/bc/GlobusStyle.java
+++ adaptors/jsaga-adaptor-cream/src/org/globus/gsi/bc/GlobusStyle.java
@@ -17,6 +17,9 @@ import org.bouncycastle.asn1.x500.X500NameStyle;
 import org.bouncycastle.asn1.x500.style.BCStyle;
 import org.bouncycastle.asn1.x500.style.IETFUtils;
 
+/*
+ * TODO: remove this class when upgrading JGlobus
+ */
 public class GlobusStyle extends BCStyle {
 
     public static final X500NameStyle INSTANCE = new GlobusStyle();
