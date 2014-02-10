5481ad8cbb92cbd2aea3856b3c1deb65de96e2a0
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2014-02-10 10:59:16 +0100

    Upgrade to JUnit4

--- adaptors/jsaga-adaptor-unicore/test/src/integration/UnicoreDataTestSuite.java
+++ adaptors/jsaga-adaptor-unicore/test/src/integration/UnicoreDataTestSuite.java
@@ -1,10 +1,17 @@
 package integration;
 
-import junit.framework.Test;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.ogf.saga.file.DirTest;
+import org.ogf.saga.file.MakeDirTest;
+import org.ogf.saga.file.ReadTest;
+import org.ogf.saga.file.WriteTest;
+import org.ogf.saga.namespace.DataMovementTest;
+import org.ogf.saga.namespace.EntryTest;
 
-import org.ogf.saga.error.DoesNotExistException;
-import org.ogf.saga.file.*;
-import org.ogf.saga.namespace.*;
 
 /* ***************************************************
 * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
@@ -18,48 +25,40 @@ import org.ogf.saga.namespace.*;
 /**
  *
  */
-public class UnicoreDataTestSuite extends JSAGATestSuite {
-    /** create test suite */
-    public static Test suite() throws Exception {return new UnicoreDataTestSuite();}
-    /** index of test cases */
-    public static class index extends IndexTest {public index(){super(UnicoreDataTestSuite.class);}}
+@RunWith(Suite.class)
+@SuiteClasses({
+    UnicoreDataTestSuite.UnicoreNSEntryTest.class,
+    UnicoreDataTestSuite.UnicoreDirectoryTest.class,
+    UnicoreDataTestSuite.UnicoreDirectoryMakeTest.class,
+    UnicoreDataTestSuite.UnicoreFileReadTest.class,
+    UnicoreDataTestSuite.UnicoreFileWriteTest.class,
+    UnicoreDataTestSuite.UnicoreDataMovementTest.class,
+    UnicoreDataTestSuite.Unicore_to_EmulatorDataMovementTest.class
+})
+ public class UnicoreDataTestSuite {
 
     /** test cases */
-    public static class UnicoreNSEntryTest extends NSEntryTest {
+    public static class UnicoreNSEntryTest extends EntryTest {
         public UnicoreNSEntryTest() throws Exception {super("unicore");}
     }
-    public static class UnicoreDirectoryListTest extends DirectoryListTest {
-        public UnicoreDirectoryListTest() throws Exception {super("unicore");}
-    }
-    public static class UnicoreDirectoryMakeTest extends DirectoryMakeTest {
+    public static class UnicoreDirectoryMakeTest extends MakeDirTest {
         public UnicoreDirectoryMakeTest() throws Exception {super("unicore");}
     }
-    public static class UnicoreDirectoryTest extends DirectoryTest {
+    public static class UnicoreDirectoryTest extends DirTest {
         public UnicoreDirectoryTest() throws Exception {super("unicore");}
     }
-    public static class UnicoreFileReadTest extends FileReadTest {
+    public static class UnicoreFileReadTest extends ReadTest {
         public UnicoreFileReadTest() throws Exception {super("unicore");}
     }
-    public static class UnicoreFileWriteTest extends FileWriteTest {
+    public static class UnicoreFileWriteTest extends WriteTest {
         public UnicoreFileWriteTest() throws Exception {super("unicore");}
-        public void test_write_append() { super.ignore("not supported"); }
+        @Override @Test @Ignore("Not supported")
+        public void test_write_append() { }
     }
-    public static class UnicoreNSCopyTest extends NSCopyTest {
-        public UnicoreNSCopyTest() throws Exception {super("unicore", "unicore");}
+    public static class UnicoreDataMovementTest extends DataMovementTest {
+        public UnicoreDataMovementTest() throws Exception {super("unicore", "unicore");}
     }
-    public static class UnicoreNSCopyRecursiveTest extends NSCopyRecursiveTest {
-        public UnicoreNSCopyRecursiveTest() throws Exception {super("unicore", "unicore");}
-    }
-    public static class UnicoreNSMoveTest extends NSMoveTest {
-        public UnicoreNSMoveTest() throws Exception {super("unicore", "unicore");}
-    }
-    public static class Unicore_to_EmulatorNSCopyTest extends NSCopyTest {
-        public Unicore_to_EmulatorNSCopyTest() throws Exception {super("unicore", "test");}
-    }
-    public static class Unicore_to_EmulatorNSCopyRecursiveTest extends NSCopyRecursiveTest {
-        public Unicore_to_EmulatorNSCopyRecursiveTest() throws Exception {super("unicore", "test");}
-    }
-    public static class Unicore_to_EmulatorNSMoveTest extends NSMoveTest {
-        public Unicore_to_EmulatorNSMoveTest() throws Exception {super("unicore", "test");}
+    public static class Unicore_to_EmulatorDataMovementTest extends DataMovementTest {
+        public Unicore_to_EmulatorDataMovementTest() throws Exception {super("unicore", "test");}
     }
 }
