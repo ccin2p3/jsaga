b72f5d1a8aaab9357bfcfe014cdf7e64b2072af1
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2014-05-19 16:18:48 +0200

    fix https://forge.in2p3.fr/issues/7195

--- adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/data/GsiftpDPMDataAdaptor.java
+++ adaptors/jsaga-adaptor-globus/src/fr/in2p3/jsaga/adaptor/data/GsiftpDPMDataAdaptor.java
@@ -1,5 +1,14 @@
 package fr.in2p3.jsaga.adaptor.data;
 
+import org.ogf.saga.error.AlreadyExistsException;
+import org.ogf.saga.error.BadParameterException;
+import org.ogf.saga.error.DoesNotExistException;
+import org.ogf.saga.error.NoSuccessException;
+import org.ogf.saga.error.PermissionDeniedException;
+import org.ogf.saga.error.TimeoutException;
+
+import fr.in2p3.jsaga.adaptor.data.read.FileAttributes;
+
 /* ***************************************************
  * *** Centre de Calcul de l'IN2P3 - Lyon (France) ***
  * ***             http://cc.in2p3.fr/             ***
@@ -17,5 +26,28 @@ public class GsiftpDPMDataAdaptor extends Gsiftp2DataAdaptor {
     public String getType() {
         return "gsiftp-dpm";
     }
+
+    
+    // DPM from version 1.8.8 writes an empty file in GridFTP probably at srmPrepareToPut, so if exclusive flag is
+    // used, the parent class will throw a "AlreadyExistException...
+    @Override
+    protected void checkExists(String absolutePath) throws AlreadyExistsException, NoSuccessException, PermissionDeniedException, BadParameterException, TimeoutException, ParentDoesNotExist {
+        try {
+            super.checkExists(absolutePath);
+        } catch (AlreadyExistsException aee) {
+            // The file has just been written by srmPrepareToPut and has a size of 0
+            try {
+                FileAttributes fa = this.getAttributes(absolutePath, null);
+                if (fa.getType() == FileAttributes.TYPE_FILE && fa.getSize() == 0) {
+                    return;
+                } else {
+                    throw aee;
+                }
+            } catch (DoesNotExistException e) {
+                throw new ParentDoesNotExist(e);
+            }
+        }
+    }
+    
 
 }
