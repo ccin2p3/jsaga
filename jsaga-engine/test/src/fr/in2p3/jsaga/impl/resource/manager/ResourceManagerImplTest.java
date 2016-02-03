084ba752635e538295de4660c005f70c3c313613
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2016-02-03 09:49:30 +0100

    fix tests

--- jsaga-engine/test/src/fr/in2p3/jsaga/impl/resource/manager/ResourceManagerImplTest.java
+++ jsaga-engine/test/src/fr/in2p3/jsaga/impl/resource/manager/ResourceManagerImplTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.ogf.saga.error.AuthenticationFailedException;
 import org.ogf.saga.error.AuthorizationFailedException;
@@ -21,6 +22,7 @@ import org.ogf.saga.url.URLFactory;
 import fr.in2p3.jsaga.adaptor.resource.ComputeResourceAdaptor;
 import fr.in2p3.jsaga.adaptor.resource.NetworkResourceAdaptor;
 import fr.in2p3.jsaga.adaptor.resource.ResourceAdaptor;
+import fr.in2p3.jsaga.adaptor.resource.StorageResourceAdaptor;
 
 public class ResourceManagerImplTest {
 
@@ -52,8 +54,29 @@ public class ResourceManagerImplTest {
         assertEquals(2, rm.listResources(null).size());
     }
 
-    // TODO: test with 2 implements
-    
+    @Test @Ignore("this test does not work: is not an interface")
+    // TODO fix this test
+    public void listComputeAndStorageResources() throws TimeoutException, NoSuccessException, 
+            NotImplementedException, AuthenticationFailedException, AuthorizationFailedException {
+        final ResourceAdaptor adaptor;
+        final String[] compute_resources = new String[2];
+        compute_resources[0] = "cres0";
+        compute_resources[1] = "cres1";
+        final String[] storage_resources = new String[2];
+        storage_resources[0] = "sres0";
+        storage_resources[1] = "sres1";
+        
+        adaptor = m_mockery.mock(ComputeAndStorageResourceAdaptor.class);
+        m_mockery.checking(new Expectations() {{
+            ((ComputeResourceAdaptor) allowing(adaptor)).listComputeResources(); will(returnValue(compute_resources));
+            ((StorageResourceAdaptor) allowing(adaptor)).listStorageResources(); will(returnValue(storage_resources));
+        }});
+        ResourceManagerImpl rm = new ResourceManagerImpl(m_session, m_url, adaptor);
+        assertEquals(2, rm.listResources(Type.COMPUTE).size());
+        assertEquals(2, rm.listResources(Type.STORAGE).size());
+        assertEquals(4, rm.listResources(null).size());
+    }
+
     @Test(expected = NotImplementedException.class)
     public void listResourcesNotImplemented() throws TimeoutException, NoSuccessException, 
             NotImplementedException, AuthenticationFailedException, AuthorizationFailedException {
@@ -61,4 +84,6 @@ public class ResourceManagerImplTest {
         adaptor = m_mockery.mock(NetworkResourceAdaptor.class);
         new ResourceManagerImpl(m_session, m_url, adaptor).listResources(Type.COMPUTE);
     }
+    
+    private abstract class ComputeAndStorageResourceAdaptor implements ComputeResourceAdaptor, StorageResourceAdaptor {};
 }
