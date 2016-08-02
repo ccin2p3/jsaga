81efbe6e5d5b7fdf12abcf015dbe6c477cdee86f
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2016-08-02 15:55:03 +0200

    test submit

--- jsaga-engine/test/src/fr/in2p3/jsaga/impl/job/streaming/GenericStreamableJobAdaptorTest.java
+++ jsaga-engine/test/src/fr/in2p3/jsaga/impl/job/streaming/GenericStreamableJobAdaptorTest.java
@@ -2,7 +2,9 @@ package fr.in2p3.jsaga.impl.job.streaming;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -29,14 +31,25 @@ public class GenericStreamableJobAdaptorTest {
     }
     
     @Test 
-    public void submit() {
+    public void submit() throws Exception {
         m_mockery = new Mockery();
         final StagingJobAdaptor sja = m_mockery.mock(StagingJobAdaptor.class);
-//        m_mockery.checking(new Expectations() {{
-//            allowing(sja).submit(jobDesc, checkMatch, uniqId); will(returnValue("123")); 
-//        }});
+        m_mockery.checking(new Expectations() {{
+            allowing(sja).submit(
+                    with(any(String.class)), 
+                    with(any(Boolean.class)), 
+                    with(any(String.class))); will(returnValue("123")); 
+        }});
 
         GenericStreamableJobAdaptor adaptor = new GenericStreamableJobAdaptor(sja);
-//        adaptor.submit(jobDesc, checkMatch, uniqId, stdin);
+        GenericJobIOHandler gjioh = (GenericJobIOHandler) adaptor.submit("", false, "123", new ByteArrayInputStream("INPUT_STRING".getBytes()));
+        assertEquals(gjioh.getJobId(), "123");
+        // check that input file contains "INPUT_STRING"
+        FileInputStream fis = new FileInputStream(LocalFileFactory.getLocalInputFile("123"));
+        byte[] content = new byte[1024];
+        int bytesRead = fis.read(content);
+        assertEquals("INPUT_STRING", new String(content, 0, bytesRead));
+        // delete input file
+        LocalFileFactory.getLocalInputFile("123").delete();
     }
 }
