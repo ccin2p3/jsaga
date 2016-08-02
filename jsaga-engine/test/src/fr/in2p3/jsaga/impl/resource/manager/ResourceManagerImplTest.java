951d83b0172407b81236fdecbb989b45b187dfaa
Lionel Schwarz <lionel.schwarz@in2p3.fr>
2016-08-02 17:41:46 +0200

    SQA for INDIGO

--- jsaga-engine/test/src/fr/in2p3/jsaga/impl/resource/manager/ResourceManagerImplTest.java
+++ jsaga-engine/test/src/fr/in2p3/jsaga/impl/resource/manager/ResourceManagerImplTest.java
@@ -1,6 +1,9 @@
 package fr.in2p3.jsaga.impl.resource.manager;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Properties;
 
 import org.jmock.Expectations;
 import org.jmock.Mockery;
@@ -15,6 +18,12 @@ import org.ogf.saga.error.NoSuccessException;
 import org.ogf.saga.error.NotImplementedException;
 import org.ogf.saga.error.TimeoutException;
 import org.ogf.saga.resource.Type;
+import org.ogf.saga.resource.description.ComputeDescription;
+import org.ogf.saga.resource.description.ResourceDescription;
+import org.ogf.saga.resource.instance.Compute;
+import org.ogf.saga.resource.instance.Network;
+import org.ogf.saga.resource.instance.Resource;
+import org.ogf.saga.resource.instance.Storage;
 import org.ogf.saga.session.Session;
 import org.ogf.saga.session.SessionFactory;
 import org.ogf.saga.url.URL;
@@ -24,6 +33,9 @@ import fr.in2p3.jsaga.adaptor.resource.ResourceAdaptor;
 import fr.in2p3.jsaga.adaptor.resource.compute.ComputeResourceAdaptor;
 import fr.in2p3.jsaga.adaptor.resource.network.NetworkResourceAdaptor;
 import fr.in2p3.jsaga.adaptor.resource.storage.StorageResourceAdaptor;
+import fr.in2p3.jsaga.impl.resource.description.ComputeDescriptionImpl;
+import fr.in2p3.jsaga.impl.resource.description.NetworkDescriptionImpl;
+import fr.in2p3.jsaga.impl.resource.description.StorageDescriptionImpl;
 
 public class ResourceManagerImplTest {
 
@@ -59,8 +71,7 @@ public class ResourceManagerImplTest {
         assertEquals(2, rm.listResources(null).size());
     }
 
-    @Test @Ignore("this test does not work: is not an interface")
-    // TODO fix this test
+    @Test
     public void listComputeAndStorageResources() throws TimeoutException, NoSuccessException, 
             NotImplementedException, AuthenticationFailedException, AuthorizationFailedException {
         final ResourceAdaptor adaptor;
@@ -98,5 +109,118 @@ public class ResourceManagerImplTest {
         new ResourceManagerImpl(m_session, m_url, adaptor).listTemplates(Type.COMPUTE);
     }
     
-    private abstract class ComputeAndStorageResourceAdaptor implements ComputeResourceAdaptor, StorageResourceAdaptor {};
+    @Test
+    public void getTemplate() throws Exception {
+        final ResourceAdaptor adaptor;
+        adaptor = m_mockery.mock(ResourceAdaptor.class);
+        ResourceManagerImpl rm = new ResourceManagerImpl(m_session, m_url, adaptor);
+        ResourceDescription rd;
+        final Properties desc = new Properties();
+        desc.put("attr", "value");
+        
+        // COMPUTE
+        desc.put(Resource.RESOURCE_TYPE, Type.COMPUTE.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(adaptor).getTemplate(with(any(String.class))); will(returnValue(desc));
+        }});
+        rd = rm.getTemplate("[url]-[id]");
+        assertTrue(rd instanceof ComputeDescriptionImpl);
+        assertEquals("value", rd.getAttribute("attr"));
+
+        // STORAGE
+        desc.put(Resource.RESOURCE_TYPE, Type.STORAGE.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(adaptor).getTemplate(with(any(String.class))); will(returnValue(desc));
+        }});
+        rd = rm.getTemplate("[url]-[id]");
+        assertTrue(rd instanceof StorageDescriptionImpl);
+        assertEquals("value", rd.getAttribute("attr"));
+
+        // NETWORK
+        desc.put(Resource.RESOURCE_TYPE, Type.NETWORK.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(adaptor).getTemplate(with(any(String.class))); will(returnValue(desc));
+        }});
+        rd = rm.getTemplate("[url]-[id]");
+        assertTrue(rd instanceof NetworkDescriptionImpl);
+        assertEquals("value", rd.getAttribute("attr"));
+    }
+    
+    @Test
+    public void acquire() throws Exception {
+        final Properties desc = new Properties();
+        desc.put("attr", "value");
+        ResourceManagerImpl rm;
+        
+        // COMPUTE
+        final ResourceAdaptor c_adaptor = m_mockery.mock(ComputeResourceAdaptor.class);
+        desc.put(Resource.RESOURCE_TYPE, Type.COMPUTE.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(c_adaptor).getDescription(with(any(String.class))); will(returnValue(desc));
+        }});
+        rm = new ResourceManagerImpl(m_session, m_url, c_adaptor);
+        Compute server = rm.acquireCompute("[url]-[server1]");
+        assertEquals("[url]-[server1]", server.getId());
+        assertEquals("value", server.getDescription().getAttribute("attr"));
+
+        // STORAGE
+        final ResourceAdaptor s_adaptor = m_mockery.mock(StorageResourceAdaptor.class);
+        desc.put(Resource.RESOURCE_TYPE, Type.STORAGE.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(s_adaptor).getDescription(with(any(String.class))); will(returnValue(desc));
+        }});
+        rm = new ResourceManagerImpl(m_session, m_url, s_adaptor);
+        Storage repo = rm.acquireStorage("[url]-[repo1]");
+        assertEquals("[url]-[repo1]", repo.getId());
+        assertEquals("value", repo.getDescription().getAttribute("attr"));
+
+        // NETWORK
+        final ResourceAdaptor n_adaptor = m_mockery.mock(NetworkResourceAdaptor.class);
+        desc.put(Resource.RESOURCE_TYPE, Type.NETWORK.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(n_adaptor).getDescription(with(any(String.class))); will(returnValue(desc));
+        }});
+        rm = new ResourceManagerImpl(m_session, m_url, n_adaptor);
+        Network net = rm.acquireNetwork("[url]-[net1]");
+        assertEquals("[url]-[net1]", net.getId());
+        assertEquals("value", net.getDescription().getAttribute("attr"));
+    }
+
+    @Test
+    public void release() throws Exception {
+        final Properties desc = new Properties();
+        desc.put("attr", "value");
+        ResourceManagerImpl rm;
+        
+        // COMPUTE
+        final ComputeResourceAdaptor c_adaptor = m_mockery.mock(ComputeResourceAdaptor.class);
+        desc.put(Resource.RESOURCE_TYPE, Type.COMPUTE.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(c_adaptor).release(with(any(String.class)));
+            allowing(c_adaptor).release(with(any(String.class)), with(any(Boolean.class)));
+        }});
+        rm = new ResourceManagerImpl(m_session, m_url, c_adaptor);
+        rm.releaseCompute("[url]-[server1]");
+        rm.releaseCompute("[url]-[server1]", false);
+
+        // STORAGE
+        final ResourceAdaptor s_adaptor = m_mockery.mock(StorageResourceAdaptor.class);
+        desc.put(Resource.RESOURCE_TYPE, Type.STORAGE.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(s_adaptor).release(with(any(String.class)));
+        }});
+        rm = new ResourceManagerImpl(m_session, m_url, s_adaptor);
+        rm.releaseStorage("[url]-[repo1]");
+
+        // NETWORK
+        final ResourceAdaptor n_adaptor = m_mockery.mock(NetworkResourceAdaptor.class);
+        desc.put(Resource.RESOURCE_TYPE, Type.NETWORK.name());
+        m_mockery.checking(new Expectations() {{
+            allowing(n_adaptor).release(with(any(String.class)));
+        }});
+        rm = new ResourceManagerImpl(m_session, m_url, n_adaptor);
+        rm.releaseStorage("[url]-[net1]");
+    }
+
+    private interface ComputeAndStorageResourceAdaptor extends ComputeResourceAdaptor, StorageResourceAdaptor {};
 }
