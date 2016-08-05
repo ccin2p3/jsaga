a7b3123c346c9b29da0e2c6b07ecc2465f80a856
Sylvain Reynaud <Sylvain.Reynaud@in2p3.fr>
2016-08-05 16:01:51 +0200

    set integration test directory to : it/

---
+++ jsaga-engine/it/src/org/ogf/saga/SampleIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.ogf.saga;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SampleIntegrationTest extends Assert {
+    @Test
+    public void test_foo() {
+        assertTrue(true);
+    }
+}
